56b26172271f4fade661247bdfc183e8170275f4
Ryosuke Niwa <rniwa@webkit.org>
2025-02-18 11:37:52 -0800

    Deploy the latest static analyzer https://bugs.webkit.org/show_bug.cgi?id=287743
    Reviewed by Brianna Fan.
    
    Deploy clang static analyzer as of 09f00b8fc177f88db418d73022897fb8374237ea.
    
    * Tools/CISupport/Shared/steps.py:
    
    Canonical link: https://commits.webkit.org/290540@main

--- Tools/CISupport/Shared/steps.py
+++ Tools/CISupport/Shared/steps.py
@@ -43,7 +43,7 @@ CURRENT_HOSTNAME = socket.gethostname().strip()
 GITHUB_URL = 'https://github.com/'
 SCAN_BUILD_OUTPUT_DIR = 'scan-build-output'
 LLVM_DIR = 'llvm-project'
-LLVM_REVISION = 'f4bc6d37ec377f3f7f4ab3407e375d0c62580b62'
+LLVM_REVISION = '09f00b8fc177f88db418d73022897fb8374237ea'
 
 
 class ShellMixin(object):
