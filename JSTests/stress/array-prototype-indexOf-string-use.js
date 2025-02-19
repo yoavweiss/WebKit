d001c718d5f1933fce3c8816fefe28b962eef492
Yijia Huang <yijia_huang@apple.com>
2025-02-19 15:14:29 -0800

    [JSC] Remove print in array-prototype-indexOf-string-use.js https://bugs.webkit.org/show_bug.cgi?id=288010 rdar://145170761
    Reviewed by Keith Miller.
    
    * JSTests/stress/array-prototype-indexOf-string-use.js:
    
    Canonical link: https://commits.webkit.org/290652@main

--- JSTests/stress/array-prototype-indexOf-string-use.js
+++ JSTests/stress/array-prototype-indexOf-string-use.js
@@ -8,8 +8,6 @@ function test(array, value) {
 }
 noInline(test);
 
-print(testLoopCount);
-
 var array = [1, 2, 3.4, "foo", 5, 6, 6, 4];
 for (let i = 0; i < testLoopCount; i++) {
     sameValue(test(array, "foo"), 3);
