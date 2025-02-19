yoavweiss/WebKit

commit 392e377b4d137913ee367941bd47697244696ba5

Simplify socket ownership in ConnectionUnix.cpp https://bugs.webkit.org/show_bug.cgi?id=281616
Reviewed by Carlos Garcia Campos.

Currently the file descriptor m_socketDescriptor exists on all platforms
but is owned only on non-GLib platforms. On GLib platforms, this file
descriptor is owned by m_socket instead. This is slightly confusing, and
entirely unnecessary: we can simply get rid of it when using GLib.

* Source/WebKit/Platform/IPC/Connection.h:
* Source/WebKit/Platform/IPC/unix/ConnectionUnix.cpp:
(IPC::Connection::socketDescriptor const):
(IPC::Connection::platformInitialize):
(IPC::Connection::platformInvalidate):
(IPC::Connection::readyReadHandler):
(IPC::Connection::platformPrepareForOpen):
(IPC::Connection::platformOpen):
(IPC::Connection::sendOutputMessage):

Canonical link: https://commits.webkit.org/285337@main

Source/WebKit/Platform/IPC/unix/ConnectionUnix.cpp
@@ -98,11 +98,27 @@ private:
 
 static_assert(sizeof(MessageInfo) + sizeof(AttachmentInfo) * attachmentMaxAmount <= messageMaxSize, "messageMaxSize is too small.");
 
+int Connection::socketDescriptor() const
+{
+#if USE(GLIB)
+    return g_socket_get_fd(m_socket.get());
+#else
+    return m_socketDescriptor;
+#endif
+}
+
 void Connection::platformInitialize(Identifier identifier)
 {
-    m_socketDescriptor = identifier.handle;
 #if USE(GLIB)
-    m_socket = adoptGRef(g_socket_new_from_fd(m_socketDescriptor, nullptr));
+    GUniqueOutPtr<GError> error;
+    m_socket = adoptGRef(g_socket_new_from_fd(identifier.handle, &error.outPtr()));
+    if (!m_socket) {
+        // Note: g_socket_new_from_fd() takes ownership of the fd only on success, so if this error
+        // were not fatal, we would need to close it here.
+        g_error("Failed to adopt IPC::Connection socket: %s", error->message);
+    }
+#else
+    m_socketDescriptor = identifier.handle;
 #endif
     m_readBuffer.reserveInitialCapacity(messageMaxSize);
     m_fileDescriptors.reserveInitialCapacity(attachmentMaxAmount);
@@ -116,6 +132,7 @@ void Connection::platformInvalidate()
 #else
     if (m_socketDescriptor != -1)
         closeWithRetry(m_socketDescriptor);
+    m_socketDescriptor = -1;
 #endif
 
     if (!m_isConnected)
@@ -133,7 +150,6 @@ void Connection::platformInvalidate()
     }
 #endif
 
-    m_socketDescriptor = -1;
     m_isConnected = false;
 }
 
@@ -305,7 +321,7 @@ static ssize_t readBytesFromSocket(int socketDescriptor, Vector<uint8_t>& buffer
 void Connection::readyReadHandler()
 {
     while (true) {
-        ssize_t bytesRead = readBytesFromSocket(m_socketDescriptor, m_readBuffer, m_fileDescriptors);
+        ssize_t bytesRead = readBytesFromSocket(socketDescriptor(), m_readBuffer, m_fileDescriptors);
 
         if (bytesRead < 0) {
             // EINTR was already handled by readBytesFromSocket.
@@ -318,7 +334,7 @@ void Connection::readyReadHandler()
             }
 
             if (m_isConnected) {
-                WTFLogAlways("Error receiving IPC message on socket %d in process %d: %s", m_socketDescriptor, getpid(), safeStrerror(errno).data());
+                WTFLogAlways("Error receiving IPC message on socket %d in process %d: %s", socketDescriptor(), getpid(), safeStrerror(errno).data());
                 connectionDidClose();
             }
             return;
@@ -339,7 +355,7 @@ void Connection::readyReadHandler()
 
 bool Connection::platformPrepareForOpen()
 {
-    if (setNonBlock(m_socketDescriptor))
+    if (setNonBlock(socketDescriptor()))
         return true;
     ASSERT_NOT_REACHED();
     return false;
@@ -370,7 +386,7 @@ void Connection::platformOpen()
     m_socketMonitor = Thread::create("SocketMonitor"_s, [protectedThis] {
         {
             int fd;
-            while ((fd = protectedThis->m_socketDescriptor) != -1) {
+            while ((fd = protectedThis->socketDescriptor()) != -1) {
                 int maxFd = fd;
                 fd_set fdSet;
                 FD_ZERO(&fdSet);
@@ -495,7 +511,7 @@ bool Connection::sendOutputMessage(UnixMessage& outputMessage)
 
     message.msg_iovlen = iovLength;
 
-    while (sendmsg(m_socketDescriptor, &message, MSG_NOSIGNAL) == -1) {
+    while (sendmsg(socketDescriptor(), &message, MSG_NOSIGNAL) == -1) {
         if (errno == EINTR)
             continue;
         if (errno == EAGAIN || errno == EWOULDBLOCK) {
@@ -520,7 +536,7 @@ bool Connection::sendOutputMessage(UnixMessage& outputMessage)
 #else
             struct pollfd pollfd;
 
-            pollfd.fd = m_socketDescriptor;
+            pollfd.fd = socketDescriptor();
             pollfd.events = POLLOUT;
             pollfd.revents = 0;
             poll(&pollfd, 1, -1);

Source/WebKit/Platform/IPC/Connection.h
@@ -646,15 +646,17 @@ private:
     void readyReadHandler();
     bool processMessage();
     bool sendOutputMessage(UnixMessage&);
+    int socketDescriptor() const;
 
     Vector<uint8_t> m_readBuffer;
     Vector<int> m_fileDescriptors;
-    int m_socketDescriptor;
     std::unique_ptr<UnixMessage> m_pendingOutputMessage;
 #if USE(GLIB)
     GRefPtr<GSocket> m_socket;
     GSocketMonitor m_readSocketMonitor;
     GSocketMonitor m_writeSocketMonitor;
+#else
+    int m_socketDescriptor;
 #endif
 #if PLATFORM(PLAYSTATION)
     RefPtr<WTF::Thread> m_socketMonitor;

commit a7040fb63290dda27ae1a4810dccf0ee180a1f4c

REGRESSION(285337@main) [GTK] EWS layout test bots getting out of space due to sequence of assertions in IPC usage of g_socket_get_fd https://bugs.webkit.org/show_bug.cgi?id=287728
Reviewed by Philippe Normand.

I think this actually regressed in 285337@main, where socketDescriptor()
was introduced. Before, if readyReadHandler was called after the
connection was closed, the read operation failed because the fd was -1,
but now the socket is used and it's already nullptr. I think the safest
approach would be to close the socket instead of setting it to nullptr
and rely on the destructor to close it. That way the socket won't be
nullptr, but -1 will be returned from g_socket_get_fd() when called
after closed.

* Source/WebKit/Platform/IPC/unix/ConnectionUnix.cpp:
(IPC::Connection::platformInvalidate):

Canonical link: https://commits.webkit.org/290602@main

Source/WebKit/Platform/IPC/unix/ConnectionUnix.cpp
@@ -129,7 +129,7 @@ void Connection::platformInitialize(Identifier&& identifier)
 void Connection::platformInvalidate()
 {
 #if USE(GLIB)
-    m_socket = nullptr;
+    g_socket_close(m_socket.get(), nullptr);
 #else
     if (m_socketDescriptor.value() != -1)
         closeWithRetry(m_socketDescriptor.release());