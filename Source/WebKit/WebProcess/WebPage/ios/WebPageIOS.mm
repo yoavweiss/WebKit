yoavweiss/WebKit

commit 2689d158dbf1cd53cb306f6732a9f1fc1de90de7

[UnifiedPDF] [iOS] REGRESSION(290293@main): Main frame PDF content is not sharp on pinch zoom https://bugs.webkit.org/show_bug.cgi?id=287511 rdar://144640298
Reviewed by Wenson Hsieh.

In 290293@main, we opted out of applying page scale factor updates on
main frame plugins in the iOS family. However, we did not adequately
replace this with scale factor application on the core page instead,
which resulted in entirely dropping page scale factor updates when a
main frame plugin is displayed. The most notable symptom of this root
cause is PDF content that is not sharp.

We address the issue in this patch as such:

1. During visible content rect updates, if the page has a main frame
   plugin that does not handle page scale factors, actually apply this
   factor on the core page instead.
2. ... with the caveat that we want to gate this application behind a
   stable state, i.e. not intermediate pinch zoom updates.
3. Ignore the root view to plugin transformation for full main frame
   plugins that do not handle scale factor updates. Without this, text
   selections (and likely annotations had we supported them fully) look
   out of place during pinch gestures, almost as if they were double
   zoomed. This approach likely papers over the real transformation
   issue at hand and would probably not fly if we supported text
   selections on embedded PDFs in iOS, but that is a bridge to cross for
   later.

* Source/WebCore/platform/graphics/IntPoint.h:
(WebCore::IntPoint::scaled const):

Introduce a constexpr const method of receiving a scaled IntPoint
instance, then adopt said methods in PluginView::viewGeometryDidChange.

* Source/WebKit/WebProcess/Plugins/PluginView.cpp:
(WebKit::PluginView::viewGeometryDidChange):
* Source/WebKit/WebProcess/WebPage/WebPage.cpp:
(WebKit::WebPage::didScalePage):

Drive-by fix: Make sure to call into platformDidScalePage() whenever
scale factor is changed, so as to not mess up layer tree transaction
bookkeeping.

Source/WebKit/WebProcess/WebPage/ios/WebPageIOS.mm
@@ -4963,6 +4963,18 @@ void WebPage::updateVisibleContentRects(const VisibleContentRectUpdateInfo& visi
     })();
 
     if (!pageHasBeenScaledSinceLastLayerTreeCommitThatChangedPageScale) {
+        bool shouldSetCorePageScale = [this, protectedThis = Ref { *this }] {
+#if ENABLE(PDF_PLUGIN)
+            RefPtr pluginView = mainFramePlugIn();
+            if (!pluginView)
+                return true;
+            return pluginView->pluginHandlesPageScaleFactor() ? false : m_isInStableState;
+#else
+            UNUSED_PARAM(this);
+            return true;
+#endif
+        }();
+
         bool hasSetPageScale = false;
         if (scaleFromUIProcess) {
             m_scaleWasSetByUIProcess = true;
@@ -4970,24 +4982,14 @@ void WebPage::updateVisibleContentRects(const VisibleContentRectUpdateInfo& visi
 
             m_dynamicSizeUpdateHistory.clear();
 
-            // FIXME: <webkit.org/b/287511> Also call Page::setPageScaleFactor() for main frame plugins that do not handle page scale factor.
-            bool pluginHandlesPageScaleFactor = [this] {
-#if ENABLE(PDF_PLUGIN)
-                return this->mainFramePlugIn();
-#else
-                UNUSED_PARAM(this);
-                return false;
-#endif
-            }();
-
-            if (!pluginHandlesPageScaleFactor)
+            if (shouldSetCorePageScale)
                 m_page->setPageScaleFactor(scaleFromUIProcess.value(), scrollPosition, m_isInStableState);
 
             hasSetPageScale = true;
             send(Messages::WebPageProxy::PageScaleFactorDidChange(scaleFromUIProcess.value()));
         }
 
-        if (!hasSetPageScale && m_isInStableState)
+        if (!hasSetPageScale && m_isInStableState && shouldSetCorePageScale)
             m_page->setPageScaleFactor(scaleToUse, scrollPosition, true);
     }
 

Source/WebCore/platform/graphics/IntPoint.h
@@ -85,6 +85,16 @@ public:
     {
         this->scale(scale, scale);
     }
+
+    constexpr IntPoint scaled(float scale) const
+    {
+        return { static_cast<int>(std::lround(m_x * scale)), static_cast<int>(std::lround(m_y * scale)) };
+    }
+
+    constexpr IntPoint scaled(float scaleX, float scaleY) const
+    {
+        return { static_cast<int>(std::lround(m_x * scaleX)), static_cast<int>(std::lround(m_y * scaleY)) };
+    }
     
     constexpr IntPoint expandedTo(const IntPoint& other) const
     {

Source/WebKit/WebProcess/Plugins/PluginView.cpp
@@ -882,17 +882,27 @@ void PluginView::viewGeometryDidChange()
         return;
 
     ASSERT(frame());
-    float pageScaleFactor = frame()->page() ? frame()->page()->pageScaleFactor() : 1;
-
-    IntPoint scaledFrameRectLocation(frameRect().location().x() * pageScaleFactor, frameRect().location().y() * pageScaleFactor);
-    IntPoint scaledLocationInRootViewCoordinates(protectedParent()->contentsToRootView(scaledFrameRectLocation));
 
     // FIXME: We still don't get the right coordinates for transformed plugins.
-    AffineTransform transform;
-    transform.translate(scaledLocationInRootViewCoordinates.x(), scaledLocationInRootViewCoordinates.y());
-    transform.scale(pageScaleFactor);
+    auto pluginToRootViewTransform = [this, protectedThis = Ref { *this }] {
+        AffineTransform transform;
 
-    protectedPlugin()->geometryDidChange(size(), transform);
+        RefPtr plugin = protectedPlugin();
+        RefPtr frame = this->frame();
+        if (frame->isMainFrame() && plugin->isFullFramePlugin() && !plugin->handlesPageScaleFactor())
+            return transform;
+
+        float pageScaleFactor = frame->page() ? frame->page()->pageScaleFactor() : 1;
+        IntPoint scaledFrameRectLocation { frameRect().location().scaled(pageScaleFactor) };
+        IntPoint scaledLocationInRootViewCoordinates { protectedParent()->contentsToRootView(scaledFrameRectLocation) };
+
+        transform.translate(scaledLocationInRootViewCoordinates);
+        transform.scale(pageScaleFactor);
+
+        return transform;
+    }();
+
+    protectedPlugin()->geometryDidChange(size(), pluginToRootViewTransform);
 }
 
 void PluginView::viewVisibilityDidChange()

Source/WebKit/WebProcess/WebPage/WebPage.cpp
@@ -2554,6 +2554,10 @@ void WebPage::didScalePage(double scale, const IntPoint& origin)
 {
     double totalScale = scale * viewScaleFactor();
     bool willChangeScaleFactor = totalScale != totalScaleFactor();
+    auto platformDidScalePageIfNeeded = makeScopeExit([willChangeScaleFactor, this, protectedThis = Ref { *this }] {
+        if (willChangeScaleFactor)
+            platformDidScalePage();
+    });
 
 #if PLATFORM(IOS_FAMILY)
     if (willChangeScaleFactor) {
@@ -2566,7 +2570,7 @@ void WebPage::didScalePage(double scale, const IntPoint& origin)
     RefPtr page = m_page;
 #if ENABLE(PDF_PLUGIN)
     if (RefPtr pluginView = mainFramePlugIn(); pluginView && pluginView->pluginHandlesPageScaleFactor()) {
-        // Since the main-frame PDF plug-in handles the page scale factor, make sure to reset WebCore's page scale.
+        // Whenever the PDF plug-in handles the page scale factor, make sure to reset WebCore's page scale.
         // Otherwise, we can end up with an immutable but non-1 page scale applied by WebCore on top of whatever the plugin does.
         if (page->pageScaleFactor() != 1)
             page->setPageScaleFactor(1, origin);
@@ -2587,8 +2591,6 @@ void WebPage::didScalePage(double scale, const IntPoint& origin)
             pluginView->setPageScaleFactor(totalScale, { origin });
     }
 #endif
-
-    platformDidScalePage();
 }
 
 void WebPage::didScalePageInViewCoordinates(double scale, const IntPoint& origin)

commit 9b7724366badac916ce7a47ed0b0952d166977b7

REGRESSION(290563@main): [UnifiedPDF] [iOS] Web content process jetsams while zooming when find-in-page results are presented https://bugs.webkit.org/show_bug.cgi?id=288702 rdar://145686833
Reviewed by Aditya Keerthi.

In 290563@main, we introduced a behavior change where, for main frame
plugins on iOS, we would only apply the new page scale factor from a
visible content rects update if we were in steady state, i.e. at the end
of pinch gestures.

This change meant that when we would pinch out, since the new page scale
factor was not applied, there was an unnecessarily large number of tiles
for the find overlay layer - all of which were full resolution too. This
caused unnecessary memory spikes in the web content process and lead to
crossing the memory watermark on said process when pinch zooming around
while the find overlay was up.

This patch addresses the bug by partially reverting the behavior change
introduced in 290563@main. We do not require being in steady state to
apply page scale factor anymore. This also means that we align better
with pinch zooming behavior on general web content in iOS, in that the
content remains sharp throughout the gesture.

* Source/WebKit/WebProcess/WebPage/ios/WebPageIOS.mm:
(WebKit::WebPage::updateVisibleContentRects):

Canonical link: https://commits.webkit.org/291230@main

Source/WebKit/WebProcess/WebPage/ios/WebPageIOS.mm
@@ -4993,7 +4993,7 @@ void WebPage::updateVisibleContentRects(const VisibleContentRectUpdateInfo& visi
             RefPtr pluginView = mainFramePlugIn();
             if (!pluginView)
                 return true;
-            return pluginView->pluginHandlesPageScaleFactor() ? false : m_isInStableState;
+            return !pluginView->pluginHandlesPageScaleFactor();
 #else
             UNUSED_PARAM(this);
             return true;