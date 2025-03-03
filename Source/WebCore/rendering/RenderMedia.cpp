bcd068d061be6bc8bb951185b873fcfdd772edf5
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2025-03-03 00:25:38 -0800

    Remove unused constructor from RenderMedia https://bugs.webkit.org/show_bug.cgi?id=288954 rdar://problem/145993769
    Reviewed by Anne van Kesteren.
    
    This patch removes unused constructor from RenderMedia, it was
    added in 22046@main.
    
    * Source/WebCore/rendering/RenderMedia.cpp:
    * Source/WebCore/rendering/RenderMedia.h:
    
    Canonical link: https://commits.webkit.org/291481@main

--- Source/WebCore/rendering/RenderMedia.cpp
+++ Source/WebCore/rendering/RenderMedia.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007, 2008, 2009, 2010 Apple Inc. All rights reserved.
+ * Copyright (C) 2007-2025 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -42,13 +42,6 @@ WTF_MAKE_TZONE_OR_ISO_ALLOCATED_IMPL(RenderMedia);
 RenderMedia::RenderMedia(Type type, HTMLMediaElement& element, RenderStyle&& style)
     : RenderImage(type, element, WTFMove(style), ReplacedFlag::IsMedia)
 {
-    setHasShadowControls(true);
-}
-
-RenderMedia::RenderMedia(Type type, HTMLMediaElement& element, RenderStyle&& style, const IntSize& intrinsicSize)
-    : RenderImage(type, element, WTFMove(style), ReplacedFlag::IsMedia)
-{
-    setIntrinsicSize(intrinsicSize);
     setHasShadowControls(true);
 }
 
