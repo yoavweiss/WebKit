95f8857209a76c1defa10bfce15fa8578b60b850
Tim Nguyen <ntim@apple.com>
2025-03-13 06:33:49 -0700

    Free up 16 bits from `RuleData` using `CompactRefPtrTuple` https://bugs.webkit.org/show_bug.cgi?id=289595 rdar://146823471
    Reviewed by Antti Koivisto.
    
    * Source/WebCore/css/StyleRule.h:
    * Source/WebCore/style/RuleData.cpp:
    (WebCore::Style::RuleData::RuleData):
    * Source/WebCore/style/RuleData.h:
    (WebCore::Style::RuleData::styleRule const):
    (WebCore::Style::RuleData::selector const):
    (WebCore::Style::RuleData::compiledSelector const):
    (WebCore::Style::RuleData::selectorIndex const):
    
    Canonical link: https://commits.webkit.org/292085@main

--- Source/WebCore/css/StyleRule.h
+++ Source/WebCore/css/StyleRule.h
@@ -53,7 +53,7 @@ using CascadeLayerName = Vector<AtomString>;
     
 DECLARE_ALLOCATOR_WITH_HEAP_IDENTIFIER(StyleRuleBase);
 class StyleRuleBase : public RefCounted<StyleRuleBase> {
-    WTF_MAKE_STRUCT_FAST_ALLOCATED_WITH_HEAP_IDENTIFIER(StyleRuleBase);
+    WTF_MAKE_STRUCT_FAST_COMPACT_ALLOCATED_WITH_HEAP_IDENTIFIER(StyleRuleBase);
 public:
     StyleRuleType type() const { return static_cast<StyleRuleType>(m_type); }
 
@@ -116,7 +116,7 @@ private:
 
 DECLARE_ALLOCATOR_WITH_HEAP_IDENTIFIER(StyleRule);
 class StyleRule : public StyleRuleBase {
-    WTF_MAKE_STRUCT_FAST_ALLOCATED_WITH_HEAP_IDENTIFIER(StyleRule);
+    WTF_MAKE_STRUCT_FAST_COMPACT_ALLOCATED_WITH_HEAP_IDENTIFIER(StyleRule);
 public:
     static Ref<StyleRule> create(Ref<StyleProperties>&&, bool hasDocumentSecurityOrigin, CSSSelectorList&&);
     Ref<StyleRule> copy() const;
@@ -166,7 +166,7 @@ private:
 
 DECLARE_ALLOCATOR_WITH_HEAP_IDENTIFIER(StyleRuleWithNesting);
 class StyleRuleWithNesting final : public StyleRule {
-    WTF_MAKE_STRUCT_FAST_ALLOCATED_WITH_HEAP_IDENTIFIER(StyleRuleWithNesting);
+    WTF_MAKE_STRUCT_FAST_COMPACT_ALLOCATED_WITH_HEAP_IDENTIFIER(StyleRuleWithNesting);
 public:
     static Ref<StyleRuleWithNesting> create(Ref<StyleProperties>&&, bool hasDocumentSecurityOrigin, CSSSelectorList&&, Vector<Ref<StyleRuleBase>>&& nestedRules);
     static Ref<StyleRuleWithNesting> create(StyleRule&&);
@@ -190,7 +190,9 @@ private:
     CSSSelectorList m_originalSelectorList;
 };
 
+DECLARE_ALLOCATOR_WITH_HEAP_IDENTIFIER(StyleRuleNestedDeclarations);
 class StyleRuleNestedDeclarations final : public StyleRule {
+    WTF_MAKE_STRUCT_FAST_COMPACT_ALLOCATED_WITH_HEAP_IDENTIFIER(StyleRuleNestedDeclarations);
 public:
     static Ref<StyleRuleNestedDeclarations> create(Ref<StyleProperties>&& properties) { return adoptRef(*new StyleRuleNestedDeclarations(WTFMove(properties))); }
     ~StyleRuleNestedDeclarations() = default;
