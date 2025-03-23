0ecadc10b54efff6ed6a4626a815dc5563f56b45
Chris Dumez <cdumez@apple.com>
2025-03-23 12:30:21 -0700

    Address safer CPP failures in DisplayLink.h https://bugs.webkit.org/show_bug.cgi?id=290238
    Reviewed by Geoffrey Garen.
    
    * Source/WebKit/SaferCPPExpectations/NoUnretainedMemberCheckerExpectations:
    * Source/WebKit/UIProcess/DisplayLink.h:
    (WTF::DefaultRefDerefTraits<__CVDisplayLink>::refIfNotNull):
    (WTF::DefaultRefDerefTraits<__CVDisplayLink>::derefIfNotNull):
    * Source/WebKit/UIProcess/mac/DisplayLinkMac.cpp:
    (WebKit::createDisplayLinkWithDisplay):
    (WebKit::DisplayLink::platformInitialize):
    (WebKit::DisplayLink::platformFinalize):
    (WebKit::DisplayLink::platformIsRunning const):
    (WebKit::DisplayLink::platformStart):
    (WebKit::DisplayLink::platformStop):
    
    Canonical link: https://commits.webkit.org/292565@main

--- Source/WebKit/UIProcess/DisplayLink.h
+++ Source/WebKit/UIProcess/DisplayLink.h
@@ -48,6 +48,17 @@
 struct wpe_playstation_display;
 #endif
 
+namespace WTF {
+#if PLATFORM(MAC)
+ALLOW_DEPRECATED_DECLARATIONS_BEGIN
+template<> struct DefaultRefDerefTraits<__CVDisplayLink> {
+    static CVDisplayLinkRef refIfNotNull(CVDisplayLinkRef displayLink) { return CVDisplayLinkRetain(displayLink); }
+    static void derefIfNotNull(CVDisplayLinkRef displayLink) { CVDisplayLinkRelease(displayLink); }
+};
+ALLOW_DEPRECATED_DECLARATIONS_END
+#endif // PLATFORM(MAC)
+}
+
 namespace WebKit {
 
 class DisplayLink {
@@ -113,7 +124,7 @@ private:
     };
 
 #if PLATFORM(MAC)
-    CVDisplayLinkRef m_displayLink { nullptr };
+    RefPtr<__CVDisplayLink> m_displayLink;
 #endif
 #if PLATFORM(GTK) || PLATFORM(WPE)
     std::unique_ptr<DisplayVBlankMonitor> m_vblankMonitor;
