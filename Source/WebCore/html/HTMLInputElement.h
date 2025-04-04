fdc846a6dcfb6f978308992990890ad587c43795
Matthew Finkel <m_finkel@apple.com>
2025-04-04 11:46:55 -0700

    Recognize autofill as a user interaction https://bugs.webkit.org/show_bug.cgi?id=290788 rdar://148268327
    Reviewed by Aditya Keerthi.
    
    Currently autofill is not logged as a user interaction, despite it requiring
    explicit user consent. This patch resolves that problem by logging a user
    interaction on form autofill.
    
    * LayoutTests/http/tests/resourceLoadStatistics/input-autofilled-and-obscured-user-interaction-expected.txt: Added.
    * LayoutTests/http/tests/resourceLoadStatistics/input-autofilled-and-obscured-user-interaction.html: Added.
    * LayoutTests/http/tests/resourceLoadStatistics/input-autofilled-and-viewable-user-interaction-expected.txt: Added.
    * LayoutTests/http/tests/resourceLoadStatistics/input-autofilled-and-viewable-user-interaction.html: Added.
    * LayoutTests/http/tests/resourceLoadStatistics/input-autofilled-user-interaction-expected.txt: Added.
    * LayoutTests/http/tests/resourceLoadStatistics/input-autofilled-user-interaction.html: Added.
    * Source/WebCore/html/HTMLInputElement.cpp:
    (WebCore::HTMLInputElement::logUserInteraction):
    (WebCore::HTMLInputElement::setAutofilled):
    (WebCore::HTMLInputElement::setAutofilledAndViewable):
    (WebCore::HTMLInputElement::setAutofilledAndObscured):
    * Source/WebCore/html/HTMLInputElement.h:
    
    Canonical link: https://commits.webkit.org/293263@main

--- Source/WebCore/html/HTMLInputElement.h
+++ Source/WebCore/html/HTMLInputElement.h
@@ -445,6 +445,8 @@ private:
     bool computeWillValidate() const final;
     void requiredStateChanged() final;
 
+    void logUserInteraction();
+
     void updateType(const AtomString& typeAttributeValue);
     void runPostTypeUpdateTasks();
 
