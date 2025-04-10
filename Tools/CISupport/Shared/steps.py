50d53e938f627d4c7bf854fe52d4cb7113bd6944
Ryosuke Niwa <rniwa@webkit.org>
2025-04-10 08:07:36 -0700

    Deploy the latest WebKit branch of LLVM https://bugs.webkit.org/show_bug.cgi?id=291338
    Reviewed by Brianna Fan.
    
    Deploy LLVM as of 91d64ea419b80431460f6846005348d802b3df1f.
    
    * Tools/CISupport/Shared/steps.py:
    
    Canonical link: https://commits.webkit.org/293520@main

--- Tools/CISupport/Shared/steps.py
+++ Tools/CISupport/Shared/steps.py
@@ -43,7 +43,7 @@ CURRENT_HOSTNAME = socket.gethostname().strip()
 GITHUB_URL = 'https://github.com/'
 SCAN_BUILD_OUTPUT_DIR = 'scan-build-output'
 LLVM_DIR = 'llvm-project'
-LLVM_REVISION = '326077317d7500a1f47afe5664e76894caf1b04a'
+LLVM_REVISION = '91d64ea419b80431460f6846005348d802b3df1f'
 
 
 class ShellMixin(object):
