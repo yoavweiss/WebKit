db4829fe377269d317f3b16251a15875a09f076e
Alex Christensen <achristensen@apple.com>
2025-04-11 11:14:58 -0700

    WebTransport should filter outgoing connections in the same way WebSockets does https://bugs.webkit.org/show_bug.cgi?id=291428 rdar://140995977
    Reviewed by David Kilzer.
    
    WebSocket checks portAllowed and isIPAddressDisallowed in WebSocket::connect.
    WebTransport should have similar restrictions.
    
    * Source/WebKit/NetworkProcess/NetworkConnectionToWebProcess.cpp:
    (WebKit::NetworkConnectionToWebProcess::initializeWebTransportSession):
    
    Canonical link: https://commits.webkit.org/293586@main

--- Source/WebKit/NetworkProcess/NetworkConnectionToWebProcess.cpp
+++ Source/WebKit/NetworkProcess/NetworkConnectionToWebProcess.cpp
@@ -1751,7 +1751,7 @@ void NetworkConnectionToWebProcess::navigatorGetPushPermissionState(URL&& scopeU
 
 void NetworkConnectionToWebProcess::initializeWebTransportSession(URL&& url, WebPageProxyIdentifier&& pageID, WebCore::ClientOrigin&& clientOrigin, CompletionHandler<void(std::optional<WebTransportSessionIdentifier>)>&& completionHandler)
 {
-    if (!url.isValid())
+    if (!url.isValid() || !portAllowed(url) || isIPAddressDisallowed(url))
         return completionHandler(std::nullopt);
 
     NetworkTransportSession::initialize(*this, WTFMove(url), WTFMove(pageID), WTFMove(clientOrigin), [weakThis = WeakPtr { *this }, completionHandler = WTFMove(completionHandler)] (RefPtr<NetworkTransportSession>&& session) mutable {
