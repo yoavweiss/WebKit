26eadedcaa761b153ddbbe473d932c240d95cbfe
Adrian Perez de Castro <aperez@igalia.com>
2025-04-11 09:07:56 -0700

    [WPE] Use ASharedMemory when targeting Android https://bugs.webkit.org/show_bug.cgi?id=290210
    Reviewed by Carlos Garcia Campos.
    
    When targeting Android use the ASharedMemory API to create shared memory
    buffers and to adjust their protection bits. OS(ANDROID) guards ensure
    that only the new implementation gets built.
    
    * Source/WebCore/SourcesWPE.txt: List new SharedMemoryAndroid.cpp source.
    * Source/WebCore/platform/android/SharedMemoryAndroid.cpp: Added, based
    on the code from SharedMemoryUnix.cpp but using ASharedMemory instead.
    (WebCore::SharedMemoryHandle::SharedMemoryHandle):
    (WebCore::SharedMemoryHandle::releaseHandle):
    (WebCore::accessModeMMap):
    (WebCore::createSharedMemory):
    (WebCore::SharedMemory::allocate):
    (WebCore::SharedMemory::map):
    (WebCore::SharedMemory::wrapMap):
    (WebCore::SharedMemory::~SharedMemory):
    (WebCore::SharedMemory::createHandle):
    * Source/WebCore/platform/unix/SharedMemoryUnix.cpp:
    
    Canonical link: https://commits.webkit.org/293579@main

--- Source/WebCore/platform/android/SharedMemoryAndroid.cpp
+++ Source/WebCore/platform/android/SharedMemoryAndroid.cpp
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2025 Igalia S.L.
  * Copyright (C) 2010 Apple Inc. All rights reserved.
  * Copyright (c) 2010 University of Szeged
  * Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies)
@@ -28,8 +29,9 @@
 #include "config.h"
 #include "SharedMemory.h"
 
-#if USE(UNIX_DOMAIN_SOCKETS) && !OS(ANDROID)
+#if OS(ANDROID)
 
+#include <android/sharedmem.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdlib.h>
@@ -44,11 +46,6 @@
 #include <wtf/text/CString.h>
 #include <wtf/text/MakeString.h>
 #include <wtf/text/WTFString.h>
-
-#if HAVE(LINUX_MEMFD_H)
-#include <linux/memfd.h>
-#include <sys/syscall.h>
-#endif
 
 namespace WebCore {
 
@@ -76,60 +73,24 @@ static inline int accessModeMMap(SharedMemory::Protection protection)
     return PROT_READ | PROT_WRITE;
 }
 
-static UnixFileDescriptor createSharedMemory()
+static UnixFileDescriptor createSharedMemory(size_t size)
 {
-    int fileDescriptor = -1;
-
-#if HAVE(LINUX_MEMFD_H)
-    static bool isMemFdAvailable = true;
-    if (isMemFdAvailable) {
-        do {
-            fileDescriptor = syscall(__NR_memfd_create, "WebKitSharedMemory", MFD_CLOEXEC);
-        } while (fileDescriptor == -1 && errno == EINTR);
-
-        if (fileDescriptor != -1)
-            return UnixFileDescriptor { fileDescriptor, UnixFileDescriptor::Adopt };
-
-        if (errno != ENOSYS)
-            return { };
-
-        isMemFdAvailable = false;
-    }
-#endif
-
-#if HAVE(SHM_ANON)
-    do {
-        fileDescriptor = shm_open(SHM_ANON, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-    } while (fileDescriptor == -1 && errno == EINTR);
-#else
-    CString tempName;
-    for (int tries = 0; fileDescriptor == -1 && tries < 10; ++tries) {
-        auto name = makeString("/WK2SharedMemory."_s, cryptographicallyRandomNumber<unsigned>());
-        tempName = name.utf8();
-
-        do {
-            fileDescriptor = shm_open(tempName.data(), O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-        } while (fileDescriptor == -1 && errno == EINTR);
-    }
-
-    if (fileDescriptor != -1)
-        shm_unlink(tempName.data());
-#endif
-
+    const auto name = makeString("/WK2SharedMemory."_s, cryptographicallyRandomNumber<unsigned>());
+    int fileDescriptor = ASharedMemory_create(name.utf8().data(), size);
     return UnixFileDescriptor { fileDescriptor, UnixFileDescriptor::Adopt };
 }
 
 RefPtr<SharedMemory> SharedMemory::allocate(size_t size)
 {
-    auto fileDescriptor = createSharedMemory();
+    auto fileDescriptor = createSharedMemory(size);
     if (!fileDescriptor) {
         WTFLogAlways("Failed to create shared memory: %s", safeStrerror(errno).data());
         return nullptr;
     }
 
-    while (ftruncate(fileDescriptor.value(), size) == -1) {
-        if (errno != EINTR)
-            return nullptr;
+    if (ASharedMemory_setProt(fileDescriptor.value(), PROT_READ | PROT_WRITE) == -1) {
+        WTFLogAlways("Failed to set ASharedMemory protection: %s", safeStrerror(errno).data());
+        return nullptr;
     }
 
     void* data = mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_SHARED, fileDescriptor.value(), 0);
@@ -176,19 +137,32 @@ SharedMemory::~SharedMemory()
     munmap(m_data, m_size);
 }
 
-auto SharedMemory::createHandle(Protection) -> std::optional<Handle>
+auto SharedMemory::createHandle([[maybe_unused]] Protection protection) -> std::optional<Handle>
 {
-    // FIXME: Handle the case where the passed Protection is ReadOnly.
-    // See https://bugs.webkit.org/show_bug.cgi?id=131542.
-
     UnixFileDescriptor duplicate { m_fileDescriptor.value(), UnixFileDescriptor::Duplicate };
     if (!duplicate) {
         ASSERT_NOT_REACHED();
         return std::nullopt;
     }
+
+    int protectionMode;
+    switch (protection) {
+    case SharedMemory::Protection::ReadOnly:
+        protectionMode = PROT_READ;
+        break;
+    case SharedMemory::Protection::ReadWrite:
+        protectionMode = PROT_WRITE;
+        break;
+    }
+
+    if (ASharedMemory_setProt(duplicate.value(), protectionMode) == -1) {
+        ASSERT_NOT_REACHED();
+        return std::nullopt;
+    }
+
     return { Handle(WTFMove(duplicate), m_size) };
 }
 
 } // namespace WebCore
 
-#endif // !OS(ANDROID) && USE(UNIX_DOMAIN_SOCKETS)
+#endif // OS(ANDROID)
