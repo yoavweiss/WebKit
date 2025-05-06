0582309eda9989e76606df89f4ef9ed201f77766
Dylan Conway <dylan.conway567@gmail.com>
2025-05-06 14:43:29 -0700

    [JSC] `ModuleProgramExecutable::create` exception check https://bugs.webkit.org/show_bug.cgi?id=292338
    Reviewed by Yusuke Suzuki.
    
    Importing a large enough bigint literal can throw during
    bytecode generation (I think strings too). A check is
    needed.
    
    * JSTests/stress/bigint-oom-import.js: Added.
    (shouldThrowAsync):
    * JSTests/stress/import-tests/bigint-oom.js: Added.
    * Source/JavaScriptCore/runtime/JSModuleRecord.cpp:
    (JSC::JSModuleRecord::link):
    * Source/JavaScriptCore/runtime/ModuleProgramExecutable.cpp:
    (JSC::ModuleProgramExecutable::create):
    
    Canonical link: https://commits.webkit.org/294578@main

--- Source/JavaScriptCore/runtime/JSModuleRecord.cpp
+++ Source/JavaScriptCore/runtime/JSModuleRecord.cpp
@@ -95,12 +95,9 @@ Synchronousness JSModuleRecord::link(JSGlobalObject* globalObject, JSValue scrip
     if (SourceProfiler::g_profilerHook) [[unlikely]]
         SourceProfiler::profile(SourceProfiler::Type::Module, sourceCode());
 
-    ModuleProgramExecutable* executable = ModuleProgramExecutable::create(globalObject, sourceCode());
-    EXCEPTION_ASSERT(!!scope.exception() == !executable);
-    if (!executable) {
-        throwSyntaxError(globalObject, scope);
-        return Synchronousness::Sync;
-    }
+    ModuleProgramExecutable* executable = ModuleProgramExecutable::tryCreate(globalObject, sourceCode());
+    RETURN_IF_EXCEPTION(scope, Synchronousness::Sync);
+
     instantiateDeclarations(globalObject, executable, scriptFetcher);
     RETURN_IF_EXCEPTION(scope, Synchronousness::Sync);
     m_moduleProgramExecutable.set(vm, this, executable);
