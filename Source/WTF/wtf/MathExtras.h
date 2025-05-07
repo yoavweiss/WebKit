df226e8e58a5f2edaa765348b402c2b005f86882
Keita Nonaka <iKonnyaku40@gmail.com>
2025-05-07 07:29:29 -0700

    [WTF] Use `std::countl_zero` in `fastLog2` to improve readability and performance https://bugs.webkit.org/show_bug.cgi?id=292607
    Reviewed by Keith Miller.
    
    Use std::countl_zero to simplify the code and improve performance.
    std::countl_zero provides a more efficient and readable approach compared to manually branching for leading zero counts.
    This change is expected to improve both code readability and potential runtime efficiency.
    
    * Source/WTF/wtf/MathExtras.h:
    (WTF::fastLog2):
    
    Canonical link: https://commits.webkit.org/294620@main

--- Source/WTF/wtf/MathExtras.h
+++ Source/WTF/wtf/MathExtras.h
@@ -426,26 +426,11 @@ constexpr unsigned maskForSize(unsigned size)
 
 inline constexpr unsigned fastLog2(unsigned i)
 {
-    unsigned log2 = 0;
+    if (!i)
+        return 0;
+    constexpr int unsignedBitWidth = std::numeric_limits<unsigned>::digits - 1;
+    unsigned log2 = unsignedBitWidth - std::countl_zero(i);
     if (i & (i - 1))
-        log2 += 1;
-    if (i >> 16) {
-        log2 += 16;
-        i >>= 16;
-    }
-    if (i >> 8) {
-        log2 += 8;
-        i >>= 8;
-    }
-    if (i >> 4) {
-        log2 += 4;
-        i >>= 4;
-    }
-    if (i >> 2) {
-        log2 += 2;
-        i >>= 2;
-    }
-    if (i >> 1)
         log2 += 1;
     return log2;
 }
