5d3ed13c04269a42c08b856b30172a2f0fc1dddc
Mike Wyrzykowski <mwyrzykowski@apple.com>
2025-05-20 14:29:15 -0700

    [WebGPU] Add and use HAVE(...) macros for Metal family support instead of using WebGPU enablement macro https://bugs.webkit.org/show_bug.cgi?id=293302 radar://151704028
    Reviewed by David Kilzer.
    
    Use seperate macro for family 8 and 9 support for test bots.
    
    * Source/WebGPU/WebGPU/Device.mm:
    (WebGPU::computeMaxCountForDevice):
    (WebGPU::computeAppleGPUFamily):
    
    Canonical link: https://commits.webkit.org/295176@main

--- Source/WebGPU/WebGPU/Device.mm
+++ Source/WebGPU/WebGPU/Device.mm
@@ -254,33 +254,35 @@ Ref<Device> Device::create(id<MTLDevice> device, String&& deviceLabel, HardwareC
 
 static uint32_t computeMaxCountForDevice(id<MTLDevice> device)
 {
-    size_t result = 200 * MB;
-#if ENABLE(WEBGPU_BY_DEFAULT)
+#if HAVE(METAL_FAMILY_9)
     if ([device supportsFamily:MTLGPUFamilyApple9])
-        result = 300 * MB;
-    else if ([device supportsFamily:MTLGPUFamilyApple8])
-        result = 275 * MB;
-    else
+        return 300 * MB;
+#endif
+#if HAVE(METAL_FAMILY_8)
+    if ([device supportsFamily:MTLGPUFamilyApple8])
+        return 275 * MB;
 #endif
     if ([device supportsFamily:MTLGPUFamilyApple7])
-        result = 250 * MB;
-    else if ([device supportsFamily:MTLGPUFamilyApple6])
-        result = 225 * MB;
-    else if ([device supportsFamily:MTLGPUFamilyApple5])
-        result = 200 * MB;
-    else if ([device supportsFamily:MTLGPUFamilyApple4])
-        result = 200 * MB;
-    else if ([device supportsFamily:MTLGPUFamilyMac2])
-        result = 300 * MB;
+        return 250 * MB;
+    if ([device supportsFamily:MTLGPUFamilyApple6])
+        return 225 * MB;
+    if ([device supportsFamily:MTLGPUFamilyApple5])
+        return 200 * MB;
+    if ([device supportsFamily:MTLGPUFamilyApple4])
+        return 200 * MB;
+    if ([device supportsFamily:MTLGPUFamilyMac2])
+        return 300 * MB;
 
-    return result;
+    return 200 * MB;
 }
 
 static uint32_t computeAppleGPUFamily(id<MTLDevice> device)
 {
-#if ENABLE(WEBGPU_BY_DEFAULT)
+#if HAVE(METAL_FAMILY_9)
     if ([device supportsFamily:MTLGPUFamilyApple9])
         return 9;
+#endif
+#if HAVE(METAL_FAMILY_8)
     if ([device supportsFamily:MTLGPUFamilyApple8])
         return 8;
 #endif
