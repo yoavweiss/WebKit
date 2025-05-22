3e0af720d5341bcadd0b5c7897ead1c3b9301454
Sihui Liu <sihui_liu@apple.com>
2025-05-22 16:27:11 -0700

    Remove excessive logging about IndexedDB storage paths https://bugs.webkit.org/show_bug.cgi?id=293442 rdar://problem/151869118
    Reviewed by Per Arne Vollan.
    
    In exsiting implementation, OriginStorageManager::StorageBucket::resolvedIDBStoragePath logs about file paths when
    IDBStorageManager::migrateOriginData returns true. IDBStorageManager::migrateOriginData returns true not only if all
    move operations succeed, but also if no file needs to be moved (i.e. no file is at the old location). The latter case is
    expected common case and there is no need to log about it.
    
    * Source/WebKit/NetworkProcess/storage/OriginStorageManager.cpp:
    (WebKit::OriginStorageManager::StorageBucket::resolvedIDBStoragePath):
    
    Canonical link: https://commits.webkit.org/295309@main

--- Source/WebKit/NetworkProcess/storage/OriginStorageManager.cpp
+++ Source/WebKit/NetworkProcess/storage/OriginStorageManager.cpp
@@ -552,8 +552,6 @@ String OriginStorageManager::StorageBucket::resolvedIDBStoragePath()
     } else {
         auto idbStoragePath = typeStoragePath(StorageType::IndexedDB);
         auto moved = IDBStorageManager::migrateOriginData(m_customIDBStoragePath, idbStoragePath);
-        if (moved)
-            RELEASE_LOG(Storage, "%p - StorageBucket::resolvedIDBStoragePath New path '%" PUBLIC_LOG_STRING "'", this, idbStoragePath.utf8().data());
         if (!moved && FileSystem::fileExists(idbStoragePath)) {
             auto fileNames = FileSystem::listDirectory(m_customIDBStoragePath);
             auto newFileNames = FileSystem::listDirectory(idbStoragePath);
