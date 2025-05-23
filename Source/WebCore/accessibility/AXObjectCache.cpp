73cfeec8eddd592db34c7df8c14ed25555889f98
Andres Gonzalez <andresg_22@apple.com>
2025-05-22 19:25:11 -0700

    AX: Password field notifications should be spaced out in time at fixed intervals. https://bugs.webkit.org/show_bug.cgi?id=293298 <rdar://problem/151703251>
    Reviewed by Tyler Wilcock.
    
    At the moment, password field notifications are fired on a one-shot timer, similarly to any other notification. With this change, they are fired at least at a fixed interval which is the desired behavior to avoid any cadence analysis.
    
    * LayoutTests/accessibility/password-notifications-timing-expected.txt: Added.
    * LayoutTests/accessibility/password-notifications-timing.html: Added.
    * LayoutTests/platform/glib/TestExpectations:
    * LayoutTests/platform/mac/TestExpectations:
    * Source/WebCore/accessibility/AXObjectCache.cpp:
    (WebCore::isSecureFieldOrContainedBySecureField):
    (WebCore::AXObjectCache::postNotification):
    (WebCore::AXObjectCache::enqueuePasswordNotification):
    (WebCore::AXObjectCache::passwordNotificationTimerFired):
    * Source/WebCore/accessibility/AXObjectCache.h:
    
    Canonical link: https://commits.webkit.org/295318@main

--- Source/WebCore/accessibility/AXObjectCache.cpp
+++ Source/WebCore/accessibility/AXObjectCache.cpp
@@ -147,9 +147,16 @@ WTF_MAKE_TZONE_ALLOCATED_IMPL(AXObjectCache);
 using namespace HTMLNames;
 
 #if PLATFORM(COCOA)
-// Post value change notifications for password fields or elements contained in password fields at a 40hz interval to thwart analysis of typing cadence
+
+// Post notifications for secure fields or elements contained in secure fields at a 40hz interval to thwart analysis of typing cadence.
 static const Seconds accessibilityPasswordValueChangeNotificationInterval { 25_ms };
-#endif
+
+static bool isSecureFieldOrContainedBySecureField(AccessibilityObject& object)
+{
+    return object.isSecureField() || object.isContainedBySecureField();
+}
+
+#endif // PLATFORM(COCOA)
 
 static bool rendererNeedsDeferredUpdate(const RenderObject& renderer)
 {
@@ -1761,6 +1768,12 @@ void AXObjectCache::postNotification(AccessibilityObject* object, Document* docu
     if (!object)
         return;
 
+#if PLATFORM(COCOA)
+    if (notification == AXNotification::ValueChanged
+        && enqueuePasswordNotification(*object, { }))
+        return;
+#endif
+
     m_notificationsToPost.append(std::make_pair(Ref { *object }, notification));
     if (!m_notificationPostTimer.isActive())
         m_notificationPostTimer.startOneShot(0_s);
@@ -1773,6 +1786,12 @@ void AXObjectCache::postNotification(AccessibilityObject& object, AXNotification
     ASSERT(isMainThread());
 
     stopCachingComputedObjectAttributes();
+
+#if PLATFORM(COCOA)
+    if (notification == AXNotification::ValueChanged
+        && enqueuePasswordNotification(object, { }))
+        return;
+#endif
 
     m_notificationsToPost.append(std::make_pair(Ref { object }, notification));
     if (!m_notificationPostTimer.isActive())
@@ -2352,13 +2371,6 @@ void AXObjectCache::setIsSynchronizingSelection(bool isSynchronizing)
     m_isSynchronizingSelection = isSynchronizing;
 }
 
-#if PLATFORM(COCOA)
-static bool isSecureFieldOrContainedBySecureField(AccessibilityObject& object)
-{
-    return object.isSecureField() || object.isContainedBySecureField();
-}
-#endif
-
 void AXObjectCache::postTextStateChangeNotification(Node* node, const AXTextStateChangeIntent& intent, const VisibleSelection& selection)
 {
     if (!node)
@@ -2571,60 +2583,50 @@ bool AXObjectCache::enqueuePasswordNotification(AccessibilityObject& object, AXT
 
     m_passwordNotifications.append({ *observableObject, secureContext(*observableObject, context) });
     if (!m_passwordNotificationTimer.isActive())
-        m_passwordNotificationTimer.startOneShot(accessibilityPasswordValueChangeNotificationInterval);
+        m_passwordNotificationTimer.startRepeating(accessibilityPasswordValueChangeNotificationInterval);
 
     return true;
 }
 
 void AXObjectCache::passwordNotificationTimerFired()
 {
-    m_passwordNotificationTimer.stop();
+    if (m_passwordNotifications.isEmpty()) {
+        m_passwordNotificationTimer.stop();
+        return;
+    }
 
-    // In tests, posting notifications has a tendency to immediately queue up other notifications, which can lead to unexpected behavior
-    // when the notification list is cleared at the end. Instead copy this list at the start.
-    auto passwordNotifications = std::exchange(m_passwordNotifications, { });
-
+    auto notification = m_passwordNotifications.takeFirst();
+    auto& context = notification.second;
+    switch (context.intent.type) {
+    case AXTextStateChangeTypeEdit:
 #if ENABLE(ACCESSIBILITY_ISOLATED_TREE)
-    Vector<std::pair<Ref<AccessibilityObject>, AXNotification>> notifications;
-    for (const auto& note : passwordNotifications) {
-        switch (note.second.intent.type) {
-        case AXTextStateChangeTypeEdit:
-            notifications.append({ note.first, AXNotification::ValueChanged });
-            break;
-        case AXTextStateChangeTypeSelectionMove:
-        case AXTextStateChangeTypeSelectionExtend:
-        case AXTextStateChangeTypeSelectionBoundary:
-            notifications.append({ note.first, AXNotification::SelectedTextChanged });
-            break;
-        case AXTextStateChangeTypeUnknown:
-            break;
-        };
-    }
-    updateIsolatedTree(notifications);
-#endif // ENABLE(ACCESSIBILITY_ISOLATED_TREE)
-
-    for (const auto& note : passwordNotifications) {
-        auto& context = note.second;
-        switch (context.intent.type) {
-        case AXTextStateChangeTypeEdit:
-            if (context.intent.editType == AXTextEditTypeReplace) {
-                postTextReplacementPlatformNotification(note.first.ptr(),
-                    AXTextEditTypeDelete, context.deletedText, AXTextEditTypeInsert, context.insertedText, context.selection.start());
-            } else {
-                postTextStateChangePlatformNotification(note.first.ptr(),
-                    context.intent.editType, context.insertedText, context.selection.start());
-            }
-            break;
-        case AXTextStateChangeTypeSelectionMove:
-        case AXTextStateChangeTypeSelectionExtend:
-        case AXTextStateChangeTypeSelectionBoundary:
-            postTextSelectionChangePlatformNotification(note.first.ptr(),
-                context.intent, context.selection);
-            break;
-        case AXTextStateChangeTypeUnknown:
-            break;
-        };
-    }
+        updateIsolatedTree(notification.first, AXNotification::ValueChanged);
+#endif
+        if (context.intent.editType == AXTextEditTypeReplace) {
+            postTextReplacementPlatformNotification(notification.first.ptr(),
+                AXTextEditTypeDelete, context.deletedText, AXTextEditTypeInsert, context.insertedText, context.selection.start());
+        } else {
+            postTextStateChangePlatformNotification(notification.first.ptr(),
+                context.intent.editType, context.insertedText, context.selection.start());
+        }
+        break;
+    case AXTextStateChangeTypeSelectionMove:
+    case AXTextStateChangeTypeSelectionExtend:
+    case AXTextStateChangeTypeSelectionBoundary:
+#if ENABLE(ACCESSIBILITY_ISOLATED_TREE)
+        updateIsolatedTree(notification.first, AXNotification::SelectedTextChanged);
+#endif
+        postTextSelectionChangePlatformNotification(notification.first.ptr(),
+            context.intent, context.selection);
+        break;
+    case AXTextStateChangeTypeUnknown:
+        // No additional context, fallback to a ValueChanged notification.
+#if ENABLE(ACCESSIBILITY_ISOLATED_TREE)
+        updateIsolatedTree(notification.first, AXNotification::ValueChanged);
+#endif
+        postPlatformNotification(notification.first, AXNotification::ValueChanged);
+        break;
+    };
 }
 
 #endif // PLATFORM(COCOA)
