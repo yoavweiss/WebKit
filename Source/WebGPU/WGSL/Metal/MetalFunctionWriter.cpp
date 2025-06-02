db75d568b3befee90213d4b52fd2cd4732c188c4
Mike Wyrzykowski <mwyrzykowski@apple.com>
2025-06-02 09:10:35 -0700

    [WebGPU] Metal compiler crash with some calls to log() https://bugs.webkit.org/show_bug.cgi?id=293790 rdar://152250897
    Reviewed by Tadeu Zagallo.
    
    Workaround crash in MTLCompiler with pathological shaders by
    performing a volatile load in calls to log().
    
    * Source/WebGPU/WGSL/Metal/MetalFunctionWriter.cpp:
    
    Canonical link: https://commits.webkit.org/295691@main

--- Source/WebGPU/WGSL/Metal/MetalFunctionWriter.cpp
+++ Source/WebGPU/WGSL/Metal/MetalFunctionWriter.cpp
@@ -66,6 +66,22 @@ namespace Metal {
 #define DEFINE_BOUND_HELPER(__name, __capitalizedName, __lowerBound, __upperBound, ...) \
     DEFINE_BOUND_HELPER_RENAMED(__name, __capitalizedName, __name, __lowerBound, __upperBound, __VA_ARGS__)
 
+#define DEFINE_VOLATILE_BOUND_HELPER_RENAMED(__name, __capitalizedName, __metalFunction, __lowerBound, __upperBound, ...) \
+    DEFINE_HELPER(__capitalizedName,  \
+    template <typename T> \
+    T __wgsl##__capitalizedName(T value) \
+    { \
+    if constexpr(__wgslMetalAppleGPUFamily < 9) { \n\
+        volatile auto result = __metalFunction(select(value, T(0), value < T(__lowerBound) || value > T(__upperBound))); \
+        return result; \
+    } else { \n\
+        return __metalFunction(select(value, T(0), value < T(__lowerBound) || value > T(__upperBound))); \
+    }\n \
+    })
+
+#define DEFINE_VOLATILE_BOUND_HELPER(__name, __capitalizedName, __lowerBound, __upperBound, ...) \
+    DEFINE_VOLATILE_BOUND_HELPER_RENAMED(__name, __capitalizedName, __name, __lowerBound, __upperBound, __VA_ARGS__)
+
 #define DEFINE_VOLATILE_HELPER_RENAMED(__name, __capitalizedName) \
     DEFINE_HELPER(__capitalizedName, \
     template <typename T>\n \
@@ -96,7 +112,7 @@ DEFINE_BOUND_HELPER(asin, Asin, -1, 1)
 DEFINE_BOUND_HELPER(acosh, Acosh, 1, numeric_limits<T>::max())
 DEFINE_BOUND_HELPER(atanh, Atanh, -1, 1)
 DEFINE_BOUND_HELPER_RENAMED(inverseSqrt, InverseSqrt, rsqrt, 0, numeric_limits<T>::infinity())
-DEFINE_BOUND_HELPER(log, Log, 0, numeric_limits<T>::infinity())
+DEFINE_VOLATILE_BOUND_HELPER(log, Log, 0, numeric_limits<T>::infinity())
 DEFINE_BOUND_HELPER(log2, Log2, 0, numeric_limits<T>::infinity())
 DEFINE_BOUND_HELPER(sqrt, Sqrt, 0, numeric_limits<T>::infinity())
 DEFINE_VOLATILE_HELPER(pack_float_to_snorm2x16, PackFloatToSnorm2x16)
