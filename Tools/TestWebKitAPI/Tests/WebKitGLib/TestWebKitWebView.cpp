bee1215860804f4bc721b65ee5f4d66e41eb4d00
Carlos Garcia Campos <cgarcia@igalia.com>
2025-06-10 08:33:01 -0700

    REGRESSION(2.48): [GTK] Segfault when initialising on offscreen window https://bugs.webkit.org/show_bug.cgi?id=291372
    Reviewed by Michael Catanzaro.
    
    Check the view is in an onscreen window in realize and when updating the
    visibility.
    
    Canonical link: https://commits.webkit.org/296038@main

--- Tools/TestWebKitAPI/Tests/WebKitGLib/TestWebKitWebView.cpp
+++ Tools/TestWebKitAPI/Tests/WebKitGLib/TestWebKitWebView.cpp
@@ -1703,6 +1703,18 @@ static void testWebViewPreferredSize(WebViewTest* test, gconstpointer)
     g_assert_cmpint(naturalSize.width, ==, 325);
     g_assert_cmpint(naturalSize.height, ==, 615);
 }
+
+#if !USE(GTK4)
+static void testWebViewOffscreenWindow(WebViewTest* test, gconstpointer)
+{
+    GtkWidget* window = gtk_offscreen_window_new();
+    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(test->webView()));
+    test->loadURI(gServer->getURIForPath("/").data());
+    test->waitUntilLoadFinished();
+    gtk_widget_show_all(window);
+    gtk_widget_destroy(window);
+}
+#endif
 #endif
 
 class WebViewTitleTest: public WebViewTest {
@@ -2171,6 +2183,9 @@ void beforeAll()
 #endif
 #if PLATFORM(GTK)
     WebViewTest::add("WebKitWebView", "preferred-size", testWebViewPreferredSize);
+#if !USE(GTK4)
+    WebViewTest::add("WebKitWebView", "offscreen-window", testWebViewOffscreenWindow);
+#endif
 #endif
     WebViewTitleTest::add("WebKitWebView", "title-change", testWebViewTitleChange);
 #if PLATFORM(WPE)
