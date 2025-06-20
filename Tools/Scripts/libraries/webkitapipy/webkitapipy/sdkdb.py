f44522d52ab7ed9450f31697ed6cdf960b06b09f
Elliott Williams <emw@apple.com>
2025-06-20 10:01:09 -0700

    [webkitapipy] Use absolute paths to track files in the SDKDB cache https://bugs.webkit.org/show_bug.cgi?id=293527
    Reviewed by Sam Sneddon.
    
    Originally the SDKDB cache was designed to be portable between machines;
    now it's generated locally. As a result, it's fine to track files using
    the full path. This prevents binaries, partial SDKDBs, or allowlists
    with the same name from overwriting each other.
    
    * Tools/Scripts/libraries/webkitapipy/webkitapipy/sdkdb.py:
    (SDKDB._initialize_db):
    (SDKDB._cache_hit_preparing_to_insert):
    (SDKDB.add_partial_sdkdb):
    (SDKDB.add_binary):
    (SDKDB.add_tbd):
    (SDKDB._add_symbol):
    (SDKDB._add_objc_class.VALUES):
    (SDKDB._add_objc_class):
    (SDKDB._add_objc_selector):
    
    Canonical link: https://commits.webkit.org/296446@main

--- Tools/Scripts/libraries/webkitapipy/webkitapipy/sdkdb.py
+++ Tools/Scripts/libraries/webkitapipy/webkitapipy/sdkdb.py
@@ -33,7 +33,7 @@ from fnmatch import fnmatch
 
 # Increment this number to force clients to rebuild from scratch, to
 # accomodate schema changes or fix caching bugs.
-VERSION = 1
+VERSION = 2
 
 
 class SDKDB:
@@ -86,13 +86,13 @@ class SDKDB:
         if cur.fetchone() == (VERSION,):
             # The database was initialized while we were waiting.
             return
-        cur.execute('CREATE TABLE input_file(name PRIMARY KEY, hash)')
+        cur.execute('CREATE TABLE input_file(path PRIMARY KEY, hash)')
         cur.execute('CREATE TABLE symbol(name, input_file '
-                    'REFERENCES input_file(name) ON DELETE CASCADE)')
+                    'REFERENCES input_file(path) ON DELETE CASCADE)')
         cur.execute('CREATE TABLE objc_class(name, input_file '
-                    'REFERENCES input_file(name) ON DELETE CASCADE)')
+                    'REFERENCES input_file(path) ON DELETE CASCADE)')
         cur.execute('CREATE TABLE objc_selector(name, class, input_file '
-                    'REFERENCES input_file(name) ON DELETE CASCADE)')
+                    'REFERENCES input_file(path) ON DELETE CASCADE)')
         cur.execute('CREATE TABLE meta(key, value)')
         cur.execute('CREATE INDEX symbol_names ON symbol (name)')
         cur.execute('CREATE INDEX selector_names ON objc_selector (name)')
@@ -119,13 +119,14 @@ class SDKDB:
         else:
             self.con.commit()
 
-    def _cache_hit_preparing_to_insert(self, name: str, hash_: int) -> bool:
+    def _cache_hit_preparing_to_insert(self, file: Path, hash_: int) -> bool:
+        path = str(file.absolute())
         cur = self.con.cursor()
-        cur.execute('SELECT hash from input_file where name = ?', (name,))
+        cur.execute('SELECT hash from input_file where path = ?', (path,))
         if cur.fetchone() == (hash_,):
             return True
         cur.execute('INSERT OR REPLACE INTO input_file VALUES (?, ?)',
-                    (name, hash_))
+                    (path, hash_))
         return False
 
     def add_partial_sdkdb(self, sdkdb_file: Path, *, spi=False, abi=False,
@@ -133,7 +134,7 @@ class SDKDB:
         fd = open(sdkdb_file)
         sdkdb_hash = os.fstat(fd.fileno()).st_mtime_ns
 
-        if self._cache_hit_preparing_to_insert(sdkdb_file.name,
+        if self._cache_hit_preparing_to_insert(sdkdb_file,
                                                sdkdb_hash):
             return False
 
@@ -169,7 +170,7 @@ class SDKDB:
 
     def add_binary(self, binary: Path, arch: str) -> bool:
         stat_hash = binary.stat().st_mtime_ns
-        if self._cache_hit_preparing_to_insert(binary.name, stat_hash):
+        if self._cache_hit_preparing_to_insert(binary, stat_hash):
             return False
         report = APIReport.from_binary(binary, arch=arch, exports_only=True)
         for selector in report.methods:
@@ -191,7 +192,7 @@ class SDKDB:
         fd = open(tbd_file)
         stat_hash = os.fstat(fd.fileno()).st_mtime_ns
 
-        if self._cache_hit_preparing_to_insert(tbd_file.name, stat_hash):
+        if self._cache_hit_preparing_to_insert(tbd_file, stat_hash):
             return False
         for tbd in TBD.from_file(tbd_file):
             if only_including and all(not fnmatch(tbd.install_name, pattern)
@@ -253,17 +254,19 @@ class SDKDB:
 
     def _add_symbol(self, name: str, file: Path):
         cur = self.con.cursor()
-        cur.execute('INSERT INTO symbol VALUES (?, ?)', (name, file.name))
+        cur.execute('INSERT INTO symbol VALUES (?, ?)',
+                    (name, str(file.absolute())))
 
     def _add_objc_class(self, name: str, file: Path):
         cur = self.con.cursor()
-        cur.execute('INSERT INTO objc_class VALUES (?, ?)', (name, file.name))
+        cur.execute('INSERT INTO objc_class VALUES (?, ?)',
+                    (name, str(file.absolute())))
         cur.execute('INSERT INTO symbol VALUES (?, ?)',
-                    (f'_OBJC_CLASS_$_{name}', file.name))
+                    (f'_OBJC_CLASS_$_{name}', str(file.absolute())))
         cur.execute('INSERT INTO symbol VALUES (?, ?)',
-                    (f'_OBJC_METACLASS_$_{name}', file.name))
+                    (f'_OBJC_METACLASS_$_{name}', str(file.absolute())))
 
     def _add_objc_selector(self, name: str, class_name: Optional[str], file: Path):
         cur = self.con.cursor()
         cur.execute('INSERT INTO objc_selector VALUES (?, ?, ?)',
-                    (name, class_name, file.name))
+                    (name, class_name, str(file.absolute())))
