7019edfb5021298dca941228b8ae9def35d74f22
Anne van Kesteren <annevk@annevk.nl>
2025-07-03 04:33:38 -0700

    Adopt UniqueRef in WebCore/contentextensions https://bugs.webkit.org/show_bug.cgi?id=295384
    Reviewed by Youenn Fablet.
    
    Helps with https://github.com/WebKit/WebKit/wiki/Safer-CPP-Guidelines
    
    Canonical link: https://commits.webkit.org/296965@main

--- Source/WebCore/contentextensions/CombinedURLFilters.cpp
+++ Source/WebCore/contentextensions/CombinedURLFilters.cpp
@@ -78,15 +78,13 @@ static size_t recursiveMemoryUsed(const PrefixTreeVertex& vertex)
 
 size_t CombinedURLFilters::memoryUsed() const
 {
-    ASSERT(m_prefixTreeRoot);
-
     size_t actionsSize = 0;
     for (const auto& slot : m_actions)
         actionsSize += slot.value.capacity() * sizeof(uint64_t);
 
     return sizeof(CombinedURLFilters)
         + m_alphabet.memoryUsed()
-        + recursiveMemoryUsed(*m_prefixTreeRoot.get())
+        + recursiveMemoryUsed(m_prefixTreeRoot)
         + sizeof(HashMap<PrefixTreeVertex*, ActionList>)
         + m_actions.capacity() * (sizeof(PrefixTreeVertex*) + sizeof(ActionList))
         + actionsSize;
@@ -126,12 +124,12 @@ static void recursivePrint(const PrefixTreeVertex& vertex, const HashMap<const P
 
 void CombinedURLFilters::print() const
 {
-    recursivePrint(*m_prefixTreeRoot.get(), m_actions, 0);
+    recursivePrint(m_prefixTreeRoot, m_actions, 0);
 }
 #endif
 
 CombinedURLFilters::CombinedURLFilters()
-    : m_prefixTreeRoot(makeUnique<PrefixTreeVertex>())
+    : m_prefixTreeRoot(makeUniqueRef<PrefixTreeVertex>())
 {
 }
 
@@ -150,7 +148,7 @@ void CombinedURLFilters::addPattern(uint64_t actionId, const Vector<Term>& patte
         return;
 
     // Extend the prefix tree with the new pattern.
-    PrefixTreeVertex* lastPrefixTree = m_prefixTreeRoot.get();
+    auto* lastPrefixTree = m_prefixTreeRoot.ptr();
 
     for (const Term& term : pattern) {
         size_t nextEntryIndex = notFound;
@@ -408,7 +406,7 @@ bool CombinedURLFilters::processNFAs(size_t maxNFASize, Function<bool(NFA&&)>&& 
     while (true) {
         // Traverse out to a leaf.
         Vector<PrefixTreeVertex*, 128> stack;
-        PrefixTreeVertex* vertex = m_prefixTreeRoot.get();
+        auto* vertex = m_prefixTreeRoot.ptr();
         while (true) {
             ASSERT(vertex);
             stack.append(vertex);
