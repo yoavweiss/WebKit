13df74d810414cddc9636e1b752eb5ab578cffb7
Anne van Kesteren <annevk@annevk.nl>
2025-07-04 07:54:46 -0700

    Reduce unsafeness in permissions module https://bugs.webkit.org/show_bug.cgi?id=295454
    Reviewed by Youenn Fablet.
    
    Also inline a number of trivial WorkerThread getters so WorkerThread
    doesn't have to be protected. And make some of them const as well.
    
    Helps with https://github.com/WebKit/WebKit/wiki/Safer-CPP-Guidelines
    
    Canonical link: https://commits.webkit.org/297024@main

--- Source/WebCore/dom/BroadcastChannel.cpp
+++ Source/WebCore/dom/BroadcastChannel.cpp
@@ -115,7 +115,7 @@ void BroadcastChannel::MainThreadBridge::ensureOnMainThread(Function<void(Page*)
         return;
     }
 
-    CheckedPtr workerLoaderProxy = downcast<WorkerGlobalScope>(*context).protectedThread()->workerLoaderProxy();
+    CheckedPtr workerLoaderProxy = downcast<WorkerGlobalScope>(*context).thread().workerLoaderProxy();
     if (!workerLoaderProxy)
         return;
 
