8b02ebb0928253d01f0e56e5a19a5a3ade40ade8
Alex Christensen <achristensen@apple.com>
2025-07-09 13:58:40 -0700

    Revert 297138@main https://bugs.webkit.org/show_bug.cgi?id=295658 rdar://155462142
    Unreviewed.
    
    It apparently broke many API tests.
    
    * Source/WebKit/Shared/JavaScriptEvaluationResult.cpp:
    (WTF::DefaultHash<JSC::Strong<JSC::JSCell>>::hash): Deleted.
    (WTF::DefaultHash<JSC::Strong<JSC::JSCell>>::equal): Deleted.
    (WebKit::JavaScriptEvaluationResult::toAPI): Deleted.
    (WebKit::JavaScriptEvaluationResult::addObjectToMap): Deleted.
    (WebKit::roundTripThroughSerializedScriptValue): Deleted.
    (WebKit::JavaScriptEvaluationResult::toVariant): Deleted.
    * Source/WebKit/Shared/JavaScriptEvaluationResult.h:
    * Source/WebKit/Shared/JavaScriptEvaluationResult.mm:
    (WebKit::JavaScriptEvaluationResult::JavaScriptEvaluationResult):
    (WebKit::JavaScriptEvaluationResult::toAPI):
    (WebKit::JavaScriptEvaluationResult::toWK):
    (WebKit::JavaScriptEvaluationResult::toVariant):
    (WebKit::JavaScriptEvaluationResult::addObjectToMap):
    (WebKit::roundTripThroughSerializedScriptValue):
    (WebKit::JavaScriptEvaluationResult::extract):
    (WebKit::JavaScriptEvaluationResult::jsValueToVariant):
    (WebKit::JavaScriptEvaluationResult::toJS):
    
    Canonical link: https://commits.webkit.org/297183@main

--- Source/WebKit/Shared/JavaScriptEvaluationResult.h
+++ Source/WebKit/Shared/JavaScriptEvaluationResult.h
@@ -27,17 +27,16 @@
 
 #include "WKRetainPtr.h"
 #include <JavaScriptCore/APICast.h>
-#include <JavaScriptCore/Strong.h>
+#include <WebCore/SerializedScriptValue.h>
 #include <optional>
 #include <wtf/HashMap.h>
 #include <wtf/ObjectIdentifier.h>
 
 #if PLATFORM(COCOA)
 #include <wtf/RetainPtr.h>
+OBJC_CLASS JSValue;
 OBJC_CLASS NSMutableArray;
 OBJC_CLASS NSMutableDictionary;
-#else
-#include <WebCore/SerializedScriptValue.h>
 #endif
 
 namespace API {
@@ -100,13 +99,10 @@ private:
 
     RetainPtr<id> toID(Variant&&);
     RefPtr<API::Object> toAPI(Variant&&);
-    JSValueRef toJS(JSGlobalContextRef, Variant&&);
 
     Variant toVariant(id);
     JSObjectID addObjectToMap(id);
-
-    Variant toVariant(JSGlobalContextRef, JSValueRef);
-    JSObjectID addObjectToMap(JSGlobalContextRef, JSValueRef);
+    Variant jsValueToVariant(JSValue *);
 
     // Used for deserializing from IPC to ObjC
     HashMap<JSObjectID, RetainPtr<id>> m_instantiatedNSObjects;
@@ -119,14 +115,9 @@ private:
     Vector<std::pair<Vector<JSObjectID>, Ref<API::Array>>> m_arrays;
 
     // Used for serializing to IPC
-    HashMap<JSC::Strong<JSC::JSCell>, JSObjectID> m_jsObjectsInMap;
+    HashMap<RetainPtr<JSValue>, JSObjectID> m_jsObjectsInMap;
     HashMap<RetainPtr<id>, JSObjectID> m_objectsInMap;
     std::optional<JSObjectID> m_nullObjectID;
-
-    // Used for deserializing from IPC to JS
-    HashMap<JSObjectID, JSValueRef> m_instantiatedJSObjects;
-    Vector<std::pair<HashMap<JSObjectID, JSObjectID>, JSObjectRef>> m_jsDictionaries;
-    Vector<std::pair<Vector<JSObjectID>, JSValueRef>> m_jsArrays;
 
     // IPC representation
     HashMap<JSObjectID, Variant> m_map;
