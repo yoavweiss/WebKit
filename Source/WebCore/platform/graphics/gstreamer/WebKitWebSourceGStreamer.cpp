464f2b6d74942a9f0624a5c87e2aa738cb7c4617
Philippe Normand <philn@igalia.com>
2025-07-18 00:42:52 -0700

    [GStreamer] ASSERT triggered when attempting a seek on a paused pipeline playing an http URI https://bugs.webkit.org/show_bug.cgi?id=290178
    Reviewed by Xabier Rodriguez-Calvar.
    
    * Source/WebCore/platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp:
    (webKitWebSrcDoSeek): Remove the assert checking for the element current state, nothing prevents a
    paused pipeline to process a seek event.
    
    Canonical link: https://commits.webkit.org/297575@main

--- Source/WebCore/platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
@@ -746,8 +746,6 @@ static gboolean webKitWebSrcDoSeek(GstBaseSrc* baseSrc, GstSegment* segment)
     // continuing seamless looping, or between flushes. In any case, basesrc holds the STREAM_LOCK, so we know create() is not running.
     // Also, both webKitWebSrcUnLock() and webKitWebSrcUnLockStop() are guaranteed to be called *before* this function.
     // [See gst_base_src_perform_seek()].
-    ASSERT(GST_ELEMENT(baseSrc)->current_state < GST_STATE_PAUSED || GST_ELEMENT(baseSrc)->current_state == GST_STATE_PLAYING || GST_PAD_IS_FLUSHING(baseSrc->srcpad));
-
     // Except for the initial seek, this function is only called if isSeekable() returns true.
     ASSERT(GST_ELEMENT(baseSrc)->current_state < GST_STATE_PAUSED || webKitWebSrcIsSeekable(baseSrc));
 
