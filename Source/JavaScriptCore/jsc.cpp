6da719bea07d0e844d33800d3c3c9b5762d71f9b
Keith Miller <keith_miller@apple.com>
2025-07-17 17:31:04 -0700

    add support for import.meta.url to jsc CLI https://bugs.webkit.org/show_bug.cgi?id=296132 rdar://156071047
    Reviewed by Yusuke Suzuki.
    
    This patch adds import.meta.url to the JSC CLI and API module import.meta objects. This
    matches the web's behavior and is desired for https://github.com/WebKit/JetStream/pull/73.
    
    Canonical link: https://commits.webkit.org/297565@main

--- Source/JavaScriptCore/jsc.cpp
+++ Source/JavaScriptCore/jsc.cpp
@@ -1527,6 +1527,9 @@ JSObject* GlobalObject::moduleLoaderCreateImportMetaProperties(JSGlobalObject* g
     metaProperties->putDirect(vm, Identifier::fromString(vm, "filename"_s), key);
     RETURN_IF_EXCEPTION(scope, nullptr);
 
+    metaProperties->putDirect(vm, JSC::Identifier::fromString(vm, "url"_s), key);
+    RETURN_IF_EXCEPTION(scope, nullptr);
+
     return metaProperties;
 }
 
