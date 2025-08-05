yoavweiss/WebKit

commit 00d5122b17f1d0baec9835d7854ce49b7714d0a3

Add Document support to _WKSerializedNode https://bugs.webkit.org/show_bug.cgi?id=296506 rdar://156746228
Reviewed by Ryosuke Niwa.

This PR is mostly moving things around to be shared between node cloning and serialization.
I begin to add the shape of cloning ContainerNodes, but it's not really implemented much yet.
I add IDL support for serializeNode(node, {deep:true}).
I changed cloneDocumentWithoutChildren from a virtual function to Document::clonedDocumentType
and Document::createCloned which know about all supported subclasses of Document.
When deserializing a Document, some properties are taken from the serialized values and some
are taken from the context Document into which you are deserializing a Node.
JavaScriptEvaluationResult::Value has become too big to fit into a HashMap value without
hitting the static assertion in HashMap::inlineLookup, so I switch to a UniqueRef<Value>.

* Source/WebCore/Sources.txt:
* Source/WebCore/WebCore.xcodeproj/project.pbxproj:
* Source/WebCore/dom/ContainerNode.cpp:
(WebCore::ContainerNode::cloneChildNodes const):
(WebCore::ContainerNode::serializeChildNodes const):
* Source/WebCore/dom/ContainerNode.h:
* Source/WebCore/dom/Document.cpp:
(WebCore::Document::clonedDocumentType const):
(WebCore::Document::cloneNodeInternal const):
(WebCore::Document::serializeNode const):
(WebCore::Document::createCloned):
(WebCore::Document::cloneDocumentWithoutChildren const): Deleted.
(WebCore::Document::cloneDataFromDocument): Deleted.
* Source/WebCore/dom/Document.h:
(WebCore::Document::compatibilityMode const):
* Source/WebCore/dom/Node.cpp:
(WebCore::Node::deserializeNode): Deleted.
* Source/WebCore/dom/Node.h:
* Source/WebCore/dom/SerializedNode.cpp: Added.
(WebCore::SerializedNode::deserialize):
* Source/WebCore/dom/SerializedNode.h:
* Source/WebCore/html/HTMLDocument.cpp:
(WebCore::HTMLDocument::cloneDocumentWithoutChildren const): Deleted.
* Source/WebCore/html/HTMLDocument.h:
* Source/WebCore/page/WebKitNamespace.cpp:

Source/WebCore/dom/SerializedNode.cpp
@@ -1,0 +1,76 @@
+/*
+ * Copyright (C) 2025 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "SerializedNode.h"
+
+#include "Attr.h"
+#include "CDATASection.h"
+#include "Comment.h"
+#include "DocumentType.h"
+#include "JSNode.h"
+#include "ProcessingInstruction.h"
+#include "SecurityOriginPolicy.h"
+#include "Text.h"
+
+namespace WebCore {
+
+JSC::JSValue SerializedNode::deserialize(SerializedNode&& serializedNode, JSC::JSGlobalObject* lexicalGlobalObject, JSDOMGlobalObject* domGlobalObject, WebCore::Document& document)
+{
+    // FIXME: Support other kinds of nodes and change RefPtr to Ref.
+    RefPtr node = WTF::switchOn(WTFMove(serializedNode.data), [&] (SerializedNode::Text&& text) -> RefPtr<Node> {
+        return WebCore::Text::create(document, WTFMove(text.data));
+    }, [&] (SerializedNode::ProcessingInstruction&& instruction) -> RefPtr<Node> {
+        return WebCore::ProcessingInstruction::create(document, WTFMove(instruction.target), WTFMove(instruction.data));
+    }, [&] (SerializedNode::DocumentType&& type) -> RefPtr<Node> {
+        return WebCore::DocumentType::create(document, type.name, type.publicId, type.systemId);
+    }, [&] (SerializedNode::Comment&& comment) -> RefPtr<Node> {
+        return WebCore::Comment::create(document, WTFMove(comment.data));
+    }, [&] (SerializedNode::CDATASection&& section) -> RefPtr<Node> {
+        return WebCore::CDATASection::create(document, WTFMove(section.data));
+    }, [&] (SerializedNode::Attr&& attr) -> RefPtr<Node> {
+        QualifiedName name(AtomString(WTFMove(attr.prefix)), AtomString(WTFMove(attr.localName)), AtomString(WTFMove(attr.namespaceURI)));
+        return WebCore::Attr::create(document, name, AtomString(WTFMove(attr.value)));
+    }, [&] (SerializedNode::Document&& serializedDocument) -> RefPtr<Node> {
+        return WebCore::Document::createCloned(
+            serializedDocument.type,
+            document.settings(),
+            serializedDocument.url,
+            serializedDocument.baseURL,
+            serializedDocument.baseURLOverride,
+            serializedDocument.documentURI,
+            document.compatibilityMode(),
+            document,
+            RefPtr { document.securityOriginPolicy() }.get(),
+            serializedDocument.contentType,
+            document.protectedDecoder().get()
+        );
+    }, [] (auto&&) -> RefPtr<Node> {
+        return nullptr;
+    });
+    return toJSNewlyCreated(lexicalGlobalObject, domGlobalObject, WTFMove(node));
+}
+
+}

Source/WebCore/dom/SerializedNode.h
@@ -25,9 +25,20 @@
 
 #pragma once
 
+#include "SecurityOriginData.h"
 #include <wtf/text/WTFString.h>
 
+namespace JSC {
+class JSGlobalObject;
+class JSValue;
+}
+
 namespace WebCore {
+
+class Document;
+class JSDOMGlobalObject;
+
+enum class ClonedDocumentType : uint8_t;
 
 struct SerializedNode {
     struct Attr {
@@ -36,10 +47,18 @@ struct SerializedNode {
         String namespaceURI;
         String value;
     };
-    struct Document {
-        // FIXME: Implement.
+    struct ContainerNode {
+        Vector<SerializedNode> children;
     };
-    struct DocumentFragment {
+    struct Document : public ContainerNode {
+        ClonedDocumentType type;
+        URL url;
+        URL baseURL;
+        URL baseURLOverride;
+        Variant<String, URL> documentURI;
+        String contentType;
+    };
+    struct DocumentFragment : public ContainerNode {
         // FIXME: Implement.
     };
     struct DocumentType {
@@ -47,13 +66,13 @@ struct SerializedNode {
         String publicId;
         String systemId;
     };
-    struct Element {
+    struct Element : public ContainerNode {
         // FIXME: Implement.
     };
-    struct ShadowRoot {
+    struct ShadowRoot : public DocumentFragment {
         // FIXME: Implement.
     };
-    struct HTMLTemplateElement {
+    struct HTMLTemplateElement : public Element {
         // FIXME: Implement.
     };
     struct CharacterData {
@@ -67,6 +86,8 @@ struct SerializedNode {
     };
 
     Variant<Attr, CDATASection, Comment, Document, DocumentFragment, DocumentType, Element, ProcessingInstruction, ShadowRoot, Text, HTMLTemplateElement> data;
+
+    WEBCORE_EXPORT static JSC::JSValue deserialize(SerializedNode&&, JSC::JSGlobalObject* lexicalGlobalObject, JSDOMGlobalObject*, WebCore::Document&);
 };
 
 }

commit 75c2b6c6213dae60fe4823baa5b8b3d1272cd793

Fix Speedometer3 regression from 297895@main https://bugs.webkit.org/show_bug.cgi?id=296904 rdar://157213758
Reviewed by Ryosuke Niwa.

When running Speedometer3, approximately 6000 JavaScriptEvaluationResults are created.
Each JavaScriptEvaluationResult contains approximately 80 Value variants.
In 297895@main I introduced an allocation and several unnecessary copies of Value.
This undoes that and solves the problem a different way, by taking the large and
uncommon alternative of the Variant and making it a UniqueRef instead of the whole Variant.
This removes about a half million allocations and copies from Speedometer3.

* Source/WTF/wtf/TZoneMalloc.h:
* Source/WebCore/dom/SerializedNode.cpp:
* Source/WebCore/dom/SerializedNode.h:
* Source/WebKit/Shared/JavaScriptEvaluationResult.cpp:
(WebKit::JavaScriptEvaluationResult::JavaScriptEvaluationResult):
(WebKit::JavaScriptEvaluationResult::toAPI):
(WebKit::JavaScriptEvaluationResult::addObjectToMap):
(WebKit::JavaScriptEvaluationResult::toValue):
(WebKit::JavaScriptEvaluationResult::toJS):
(WebKit::JavaScriptEvaluationResult::toString const):
* Source/WebKit/Shared/JavaScriptEvaluationResult.h:
(WebKit::JavaScriptEvaluationResult::map const):
* Source/WebKit/Shared/JavaScriptEvaluationResult.mm:
(WebKit::JavaScriptEvaluationResult::toID):
(WebKit::JavaScriptEvaluationResult::toValue):
(WebKit::JavaScriptEvaluationResult::addObjectToMap):
* Source/WebKit/Shared/JavaScriptEvaluationResult.serialization.in:
* Source/WebKit/Shared/glib/JavaScriptEvaluationResultGLib.cpp:
(WebKit::JavaScriptEvaluationResult::addObjectToMap):
(WebKit::JavaScriptEvaluationResult::toValue):

Canonical link: https://commits.webkit.org/298219@main

Source/WTF/wtf/TZoneMalloc.h
@@ -40,6 +40,7 @@
 
 // struct allocators with FastMalloc fallback if TZoneHeap is disabled.
 #define WTF_MAKE_STRUCT_TZONE_ALLOCATED(name) WTF_DEPRECATED_MAKE_STRUCT_FAST_ALLOCATED(name)
+#define WTF_MAKE_STRUCT_TZONE_ALLOCATED_EXPORT(name, exportMacro) WTF_DEPRECATED_MAKE_STRUCT_FAST_ALLOCATED(name)
 
 // template allocators with FastMalloc fallback if TZoneHeap is disabled.
 #define WTF_MAKE_TZONE_ALLOCATED_TEMPLATE(name) WTF_DEPRECATED_MAKE_FAST_ALLOCATED(name)
@@ -153,6 +154,7 @@
 
 // struct allocators with FastMalloc fallback if TZoneHeap is enabled.
 #define WTF_MAKE_STRUCT_TZONE_ALLOCATED(name) MAKE_STRUCT_BTZONE_MALLOCED(name, NonCompact, WTF_NOEXPORT)
+#define WTF_MAKE_STRUCT_TZONE_ALLOCATED_EXPORT(name, exportMacro) MAKE_STRUCT_BTZONE_MALLOCED(name, NonCompact, exportMacro)
 
 // template allocators with FastMalloc fallback if TZoneHeap is enabled.
 #define WTF_MAKE_TZONE_ALLOCATED_TEMPLATE(name) MAKE_BTZONE_MALLOCED_TEMPLATE(name, NonCompact, WTF_NOEXPORT)

Source/WebCore/dom/SerializedNode.cpp
@@ -44,6 +44,8 @@
 
 namespace WebCore {
 
+WTF_MAKE_STRUCT_TZONE_ALLOCATED_IMPL(SerializedNode);
+
 static void setAttributes(Element& element, Vector<SerializedNode::Element::Attribute>&& attributes)
 {
     element.parserSetAttributes(WTF::map(WTFMove(attributes), [] (auto&& attribute) {

Source/WebCore/dom/SerializedNode.h
@@ -43,6 +43,7 @@ class JSDOMGlobalObject;
 enum class ClonedDocumentType : uint8_t;
 
 struct SerializedNode {
+    WTF_MAKE_STRUCT_TZONE_ALLOCATED_EXPORT(SerializedNode, WEBCORE_EXPORT);
     struct QualifiedName {
         String prefix;
         String localName;

Source/WebKit/Shared/JavaScriptEvaluationResult.cpp
@@ -44,15 +44,13 @@
 
 namespace WebKit {
 
-WTF_MAKE_STRUCT_TZONE_ALLOCATED_IMPL(JavaScriptEvaluationResult::Value);
-
-JavaScriptEvaluationResult::JavaScriptEvaluationResult(JSObjectID root, HashMap<JSObjectID, UniqueRef<Value>>&& map)
+JavaScriptEvaluationResult::JavaScriptEvaluationResult(JSObjectID root, HashMap<JSObjectID, Value>&& map)
     : m_map(WTFMove(map))
     , m_root(root) { }
 
 RefPtr<API::Object> JavaScriptEvaluationResult::toAPI(Value&& root)
 {
-    return WTF::switchOn(WTFMove(root.value), [] (EmptyType) -> RefPtr<API::Object> {
+    return WTF::switchOn(WTFMove(root), [] (EmptyType) -> RefPtr<API::Object> {
         return nullptr;
     }, [] (bool value) -> RefPtr<API::Object> {
         return API::Boolean::create(value);
@@ -72,7 +70,7 @@ RefPtr<API::Object> JavaScriptEvaluationResult::toAPI(Value&& root)
         return { WTFMove(dictionary) };
     }, [] (NodeInfo&&) -> RefPtr<API::Object> {
         return nullptr;
-    }, [] (WebCore::SerializedNode&&) -> RefPtr<API::Object> {
+    }, [] (UniqueRef<WebCore::SerializedNode>&&) -> RefPtr<API::Object> {
         return nullptr;
     });
 }
@@ -80,7 +78,7 @@ RefPtr<API::Object> JavaScriptEvaluationResult::toAPI(Value&& root)
 RefPtr<API::Object> JavaScriptEvaluationResult::toAPI()
 {
     for (auto&& [identifier, value] : std::exchange(m_map, { }))
-        m_instantiatedObjects.add(identifier, toAPI(WTFMove(value.get())));
+        m_instantiatedObjects.add(identifier, toAPI(WTFMove(value)));
     for (auto [vector, array] : std::exchange(m_arrays, { })) {
         for (auto identifier : vector) {
             if (RefPtr object = m_instantiatedObjects.get(identifier))
@@ -111,7 +109,7 @@ JSObjectID JavaScriptEvaluationResult::addObjectToMap(JSGlobalContextRef context
     if (!object) {
         if (!m_nullObjectID) {
             m_nullObjectID = JSObjectID::generate();
-            m_map.add(*m_nullObjectID, makeUniqueRef<Value>(Value { { EmptyType::Undefined } }));
+            m_map.add(*m_nullObjectID, Value { EmptyType::Undefined });
         }
         return *m_nullObjectID;
     }
@@ -123,7 +121,7 @@ JSObjectID JavaScriptEvaluationResult::addObjectToMap(JSGlobalContextRef context
 
     auto identifier = JSObjectID::generate();
     m_jsObjectsInMap.set(WTFMove(value), identifier);
-    m_map.add(identifier, makeUniqueRef<Value>(toValue(context, object)));
+    m_map.add(identifier, toValue(context, object));
     return identifier;
 }
 
@@ -157,51 +155,51 @@ auto JavaScriptEvaluationResult::toValue(JSGlobalContextRef context, JSValueRef 
 {
     if (!JSValueIsObject(context, value)) {
         if (JSValueIsBoolean(context, value))
-            return { JSValueToBoolean(context, value) };
+            return JSValueToBoolean(context, value);
         if (JSValueIsNumber(context, value)) {
             value = JSValueMakeNumber(context, JSValueToNumber(context, value, 0));
-            return { JSValueToNumber(context, value, 0) };
+            return JSValueToNumber(context, value, 0);
         }
         if (JSValueIsString(context, value)) {
             auto* globalObject = ::toJS(context);
             JSC::JSValue jsValue = ::toJS(globalObject, value);
-            return { jsValue.toWTFString(globalObject) };
+            return jsValue.toWTFString(globalObject);
         }
         if (JSValueIsNull(context, value))
-            return { EmptyType::Null };
-        return { EmptyType::Undefined };
+            return EmptyType::Null;
+        return EmptyType::Undefined;
     }
 
     JSObjectRef object = JSValueToObject(context, value, 0);
 
     if (auto* info = jsDynamicCast<WebCore::JSWebKitNodeInfo*>(::toJS(::toJS(context), object))) {
         Ref nodeInfo { info->wrapped() };
-        return { NodeInfo { nodeInfo->nodeIdentifier(), nodeInfo->contentFrameIdentifier() } };
+        return NodeInfo { nodeInfo->nodeIdentifier(), nodeInfo->contentFrameIdentifier() };
     }
 
     if (auto* node = jsDynamicCast<WebCore::JSWebKitSerializedNode*>(::toJS(::toJS(context), object))) {
         Ref serializedNode { node->wrapped() };
-        return { WebCore::SerializedNode { serializedNode->serializedNode() } };
+        return makeUniqueRef<WebCore::SerializedNode>(serializedNode->serializedNode());
     }
 
     if (JSValueIsDate(context, object))
-        return { Seconds(JSValueToNumber(context, object, 0) / 1000.0) };
+        return Seconds(JSValueToNumber(context, object, 0) / 1000.0);
 
     if (JSValueIsArray(context, object)) {
         SUPPRESS_UNCOUNTED_ARG JSValueRef lengthPropertyName = JSValueMakeString(context, adopt(JSStringCreateWithUTF8CString("length")).get());
         JSValueRef lengthValue = JSObjectGetPropertyForKey(context, object, lengthPropertyName, nullptr);
         double lengthDouble = JSValueToNumber(context, lengthValue, nullptr);
         if (lengthDouble < 0 || lengthDouble > static_cast<double>(std::numeric_limits<size_t>::max()))
-            return { EmptyType::Undefined };
+            return EmptyType::Undefined;
 
         size_t length = lengthDouble;
         Vector<JSObjectID> vector;
         if (!vector.tryReserveInitialCapacity(length))
-            return { EmptyType::Undefined };
+            return EmptyType::Undefined;
 
         for (size_t i = 0; i < length; ++i)
             vector.append(addObjectToMap(context, JSObjectGetPropertyAtIndex(context, object, i, nullptr)));
-        return { { WTFMove(vector) } };
+        return { WTFMove(vector) };
     }
 
     JSPropertyNameArrayRef names = JSObjectCopyPropertyNames(context, object);
@@ -212,7 +210,7 @@ auto JavaScriptEvaluationResult::toValue(JSGlobalContextRef context, JSValueRef 
         SUPPRESS_UNCOUNTED_ARG map.add(addObjectToMap(context, JSValueMakeString(context, key.get())), addObjectToMap(context, JSObjectGetPropertyForKey(context, object, JSValueMakeString(context, key.get()), nullptr)));
     }
     JSPropertyNameArrayRelease(names);
-    return { { WTFMove(map) } };
+    return { WTFMove(map) };
 }
 
 JavaScriptEvaluationResult::JavaScriptEvaluationResult(JSGlobalContextRef context, JSValueRef value)
@@ -233,7 +231,7 @@ JSValueRef JavaScriptEvaluationResult::toJS(JSGlobalContextRef context, Value&& 
         return std::make_tuple(lexicalGlobalObject, domGlobalObject, WTFMove(document));
     };
 
-    return WTF::switchOn(WTFMove(root.value), [&] (EmptyType emptyType) -> JSValueRef {
+    return WTF::switchOn(WTFMove(root), [&] (EmptyType emptyType) -> JSValueRef {
         switch (emptyType) {
         case EmptyType::Undefined:
             return JSValueMakeUndefined(context);
@@ -266,16 +264,16 @@ JSValueRef JavaScriptEvaluationResult::toJS(JSGlobalContextRef context, Value&& 
         if (document.get() != &node->document())
             return JSValueMakeUndefined(context);
         return ::toRef(lexicalGlobalObject, WebCore::toJS(lexicalGlobalObject, domGlobalObject, *node));
-    }, [&] (WebCore::SerializedNode&& serializedNode) -> JSValueRef {
+    }, [&] (UniqueRef<WebCore::SerializedNode>&& serializedNode) -> JSValueRef {
         auto [lexicalGlobalObject, domGlobalObject, document] = globalObjectTuple(context);
-        return ::toRef(lexicalGlobalObject, WebCore::SerializedNode::deserialize(WTFMove(serializedNode), lexicalGlobalObject, domGlobalObject, *document));
+        return ::toRef(lexicalGlobalObject, WebCore::SerializedNode::deserialize(WTFMove(serializedNode.get()), lexicalGlobalObject, domGlobalObject, *document));
     });
 }
 
 Protected<JSValueRef> JavaScriptEvaluationResult::toJS(JSGlobalContextRef context)
 {
     for (auto&& [identifier, value] : std::exchange(m_map, { }))
-        m_instantiatedJSObjects.add(identifier, Protected<JSValueRef>(context, toJS(context, WTFMove(value.get()))));
+        m_instantiatedJSObjects.add(identifier, Protected<JSValueRef>(context, toJS(context, WTFMove(value))));
     for (auto& [vector, array] : std::exchange(m_jsArrays, { })) {
         JSObjectRef jsArray = JSValueToObject(context, array.get(), 0);
         for (size_t index = 0; index < vector.size(); ++index) {
@@ -313,7 +311,7 @@ String JavaScriptEvaluationResult::toString() const
     auto it = m_map.find(m_root);
     if (it == m_map.end())
         return { };
-    auto* string = std::get_if<String>(&it->value->value);
+    auto* string = std::get_if<String>(&it->value);
     if (!string)
         return { };
     return *string;

Source/WebKit/Shared/JavaScriptEvaluationResult.h
@@ -61,12 +61,9 @@ using JSObjectID = ObjectIdentifier<JSObjectIDType>;
 class JavaScriptEvaluationResult {
 public:
     enum class EmptyType : bool { Undefined, Null };
-    struct Value {
-        WTF_MAKE_STRUCT_TZONE_ALLOCATED(Value);
-        Variant<EmptyType, bool, double, String, Seconds, Vector<JSObjectID>, HashMap<JSObjectID, JSObjectID>, NodeInfo, WebCore::SerializedNode> value;
-    };
+    using Value = Variant<EmptyType, bool, double, String, Seconds, Vector<JSObjectID>, HashMap<JSObjectID, JSObjectID>, NodeInfo, UniqueRef<WebCore::SerializedNode>>;
 
-    JavaScriptEvaluationResult(JSObjectID, HashMap<JSObjectID, UniqueRef<Value>>&&);
+    JavaScriptEvaluationResult(JSObjectID, HashMap<JSObjectID, Value>&&);
     static std::optional<JavaScriptEvaluationResult> extract(JSGlobalContextRef, JSValueRef);
 
     JavaScriptEvaluationResult(JavaScriptEvaluationResult&&);
@@ -74,7 +71,7 @@ public:
     ~JavaScriptEvaluationResult();
 
     JSObjectID root() const { return m_root; }
-    const HashMap<JSObjectID, UniqueRef<Value>>& map() const { return m_map; }
+    const HashMap<JSObjectID, Value>& map() const { return m_map; }
 
     String toString() const;
 
@@ -139,7 +136,7 @@ private:
     Vector<std::pair<Vector<JSObjectID>, Protected<JSValueRef>>> m_jsArrays;
 
     // IPC representation
-    HashMap<JSObjectID, UniqueRef<Value>> m_map;
+    HashMap<JSObjectID, Value> m_map;
     JSObjectID m_root;
 };
 

Source/WebKit/Shared/JavaScriptEvaluationResult.mm
@@ -35,7 +35,7 @@ namespace WebKit {
 
 RetainPtr<id> JavaScriptEvaluationResult::toID(Value&& root)
 {
-    return WTF::switchOn(WTFMove(root.value), [] (EmptyType type) -> RetainPtr<id> {
+    return WTF::switchOn(WTFMove(root), [] (EmptyType type) -> RetainPtr<id> {
         switch (type) {
         case EmptyType::Null:
             return NSNull.null;
@@ -61,15 +61,15 @@ RetainPtr<id> JavaScriptEvaluationResult::toID(Value&& root)
         return dictionary;
     }, [] (NodeInfo&& nodeInfo) -> RetainPtr<id> {
         return wrapper(API::NodeInfo::create(WTFMove(nodeInfo)).get());
-    }, [] (WebCore::SerializedNode&& serializedNode) -> RetainPtr<id> {
-        return wrapper(API::SerializedNode::create(WTFMove(serializedNode)).get());
+    }, [] (UniqueRef<WebCore::SerializedNode>&& serializedNode) -> RetainPtr<id> {
+        return wrapper(API::SerializedNode::create(WTFMove(serializedNode.get())).get());
     });
 }
 
 RetainPtr<id> JavaScriptEvaluationResult::toID()
 {
     for (auto&& [identifier, value] : std::exchange(m_map, { }))
-        m_instantiatedNSObjects.add(identifier, toID(WTFMove(value.get())));
+        m_instantiatedNSObjects.add(identifier, toID(WTFMove(value)));
     for (auto [vector, array] : std::exchange(m_nsArrays, { })) {
         for (auto identifier : vector) {
             if (RetainPtr element = m_instantiatedNSObjects.get(identifier))
@@ -95,32 +95,32 @@ RetainPtr<id> JavaScriptEvaluationResult::toID()
 auto JavaScriptEvaluationResult::toValue(id object) -> Value
 {
     if (!object)
-        return { EmptyType::Undefined };
+        return EmptyType::Undefined;
 
     if ([object isKindOfClass:NSNull.class])
-        return { EmptyType::Null };
+        return EmptyType::Null;
 
     if ([object isKindOfClass:NSNumber.class]) {
         if (CFNumberGetType((CFNumberRef)object) == kCFNumberCharType) {
             if ([object isEqual:@YES])
-                return { true };
+                return true;
             if ([object isEqual:@NO])
-                return { false };
+                return false;
         }
-        return { [(NSNumber *)object doubleValue] };
+        return [(NSNumber *)object doubleValue];
     }
 
     if (auto* nsString = dynamic_objc_cast<NSString>(object))
-        return { String(nsString) };
+        return String(nsString);
 
     if ([object isKindOfClass:NSDate.class])
-        return { Seconds([(NSDate *)object timeIntervalSince1970]) };
+        return Seconds([(NSDate *)object timeIntervalSince1970]);
 
     if ([object isKindOfClass:NSArray.class]) {
         Vector<JSObjectID> vector;
         for (id element : (NSArray *)object)
             vector.append(addObjectToMap(element));
-        return { { WTFMove(vector) } };
+        return { WTFMove(vector) };
     }
 
     if ([object isKindOfClass:NSDictionary.class]) {
@@ -128,18 +128,18 @@ auto JavaScriptEvaluationResult::toValue(id object) -> Value
         [(NSDictionary *)object enumerateKeysAndObjectsUsingBlock:[&](id key, id value, BOOL *) {
             map.add(addObjectToMap(key), addObjectToMap(value));
         }];
-        return { { WTFMove(map) } };
+        return { WTFMove(map) };
     }
 
     if ([object isKindOfClass:_WKSerializedNode.class])
-        return { WebCore::SerializedNode { ((_WKSerializedNode *)object)->_node->coreSerializedNode() } };
+        return makeUniqueRef<WebCore::SerializedNode>(((_WKSerializedNode *)object)->_node->coreSerializedNode());
 
     if ([object isKindOfClass:_WKNodeInfo.class])
-        return { NodeInfo { ((_WKNodeInfo *)object)->_info->info() } };
+        return NodeInfo { ((_WKNodeInfo *)object)->_info->info() };
 
     // This object has been null checked and went through isSerializable which only supports these types.
     ASSERT_NOT_REACHED();
-    return { EmptyType::Undefined };
+    return EmptyType::Undefined;
 }
 
 JSObjectID JavaScriptEvaluationResult::addObjectToMap(id object)
@@ -147,7 +147,7 @@ JSObjectID JavaScriptEvaluationResult::addObjectToMap(id object)
     if (!object) {
         if (!m_nullObjectID) {
             m_nullObjectID = JSObjectID::generate();
-            m_map.add(*m_nullObjectID, makeUniqueRef<Value>(Value { { EmptyType::Undefined } }));
+            m_map.add(*m_nullObjectID, EmptyType::Undefined);
         }
         return *m_nullObjectID;
     }
@@ -158,7 +158,7 @@ JSObjectID JavaScriptEvaluationResult::addObjectToMap(id object)
 
     auto identifier = JSObjectID::generate();
     m_objectsInMap.set(object, identifier);
-    m_map.add(identifier, makeUniqueRef<Value>(toValue(object)));
+    m_map.add(identifier, toValue(object));
     return identifier;
 }
 

Source/WebKit/Shared/JavaScriptEvaluationResult.serialization.in
@@ -25,13 +25,9 @@ struct WebKit::NodeInfo {
     Markable<WebCore::FrameIdentifier> contentFrameIdentifier;
 };
 
-[Nested] struct WebKit::JavaScriptEvaluationResult::Value {
-    Variant<WebKit::JavaScriptEvaluationResult::EmptyType, bool, double, String, Seconds, Vector<WebKit::JSObjectID>, HashMap<WebKit::JSObjectID, WebKit::JSObjectID>, WebKit::NodeInfo, WebCore::SerializedNode> value;
-}
-
 class WebKit::JavaScriptEvaluationResult {
     WebKit::JSObjectID root()
-    HashMap<WebKit::JSObjectID, UniqueRef<WebKit::JavaScriptEvaluationResult::Value>> map()
+    HashMap<WebKit::JSObjectID, Variant<WebKit::JavaScriptEvaluationResult::EmptyType, bool, double, String, Seconds, Vector<WebKit::JSObjectID>, HashMap<WebKit::JSObjectID, WebKit::JSObjectID>, WebKit::NodeInfo, UniqueRef<WebCore::SerializedNode>>> map()
 }
 
 [Nested] enum class WebKit::JavaScriptEvaluationResult::EmptyType : bool

Source/WebKit/Shared/glib/JavaScriptEvaluationResultGLib.cpp
@@ -45,7 +45,7 @@ GRefPtr<JSCValue> JavaScriptEvaluationResult::toJSC()
 JSObjectID JavaScriptEvaluationResult::addObjectToMap(GVariant* variant)
 {
     auto identifier = JSObjectID::generate();
-    m_map.add(identifier, makeUniqueRef<Value>(toValue(variant)));
+    m_map.add(identifier, toValue(variant));
     return identifier;
 }
 
@@ -61,33 +61,33 @@ auto JavaScriptEvaluationResult::toValue(GVariant* variant) -> Value
             if (!key || !value)
                 continue;
             auto keyID = JSObjectID::generate();
-            m_map.add(keyID, makeUniqueRef<Value>(Value { { String::fromUTF8(key) } }));
+            m_map.add(keyID, String::fromUTF8(key));
             auto valueID = JSObjectID::generate();
-            m_map.add(valueID, makeUniqueRef<Value>(toValue(value)));
+            m_map.add(valueID, toValue(value));
             map.add(keyID, valueID);
         }
-        return { { WTFMove(map) } };
+        return { WTFMove(map) };
     }
 
     if (g_variant_is_of_type(variant, G_VARIANT_TYPE_UINT32))
-        return { static_cast<double>(g_variant_get_uint32(variant)) };
+        return static_cast<double>(g_variant_get_uint32(variant));
     if (g_variant_is_of_type(variant, G_VARIANT_TYPE_INT32))
-        return { static_cast<double>(g_variant_get_int32(variant)) };
+        return static_cast<double>(g_variant_get_int32(variant));
     if (g_variant_is_of_type(variant, G_VARIANT_TYPE_UINT64))
-        return { static_cast<double>(g_variant_get_uint64(variant)) };
+        return static_cast<double>(g_variant_get_uint64(variant));
     if (g_variant_is_of_type(variant, G_VARIANT_TYPE_INT64))
-        return { static_cast<double>(g_variant_get_int64(variant)) };
+        return static_cast<double>(g_variant_get_int64(variant));
     if (g_variant_is_of_type(variant, G_VARIANT_TYPE_INT16))
-        return { static_cast<double>(g_variant_get_int16(variant)) };
+        return static_cast<double>(g_variant_get_int16(variant));
     if (g_variant_is_of_type(variant, G_VARIANT_TYPE_UINT16))
-        return { static_cast<double>(g_variant_get_uint16(variant)) };
+        return static_cast<double>(g_variant_get_uint16(variant));
     if (g_variant_is_of_type(variant, G_VARIANT_TYPE_DOUBLE))
-        return { static_cast<double>(g_variant_get_double(variant)) };
+        return static_cast<double>(g_variant_get_double(variant));
 
     if (g_variant_is_of_type(variant, G_VARIANT_TYPE_STRING))
-        return { String::fromUTF8(g_variant_get_string(variant, nullptr)) };
+        return String::fromUTF8(g_variant_get_string(variant, nullptr));
 
-    return { EmptyType::Null };
+    return EmptyType::Null;
 }
 
 JavaScriptEvaluationResult::JavaScriptEvaluationResult(GVariant* variant)