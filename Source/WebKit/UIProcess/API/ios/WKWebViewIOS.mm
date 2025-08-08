e5e28a5fb38ca98f27be4062d01f7a45e3c43cb6
Abrar Rahman Protyasha <a_protyasha@apple.com>
2025-08-07 23:21:10 -0700

    CARenderServerRender* APIs are deprecated https://bugs.webkit.org/show_bug.cgi?id=297098 rdar://157581000
    Reviewed by Simon Fraser.
    
    CARenderServerRender* APIs, used to composite layers onto a destination
    surface, have been deprecated in recent SDKs.
    
    In this patch, we migrate to CARenderServerSnapshot, which is the
    preferred alternative.
    
    * Source/WebCore/PAL/pal/spi/cocoa/QuartzCoreSPI.h:
    Removed (now unused) forward declarations, and introduce a few new
    delcarations.
    * Source/WebKit/UIProcess/API/ios/WKWebViewIOS.mm:
    (-[WKWebView _takeViewSnapshot]):
    (-[WKWebView _snapshotRectAfterScreenUpdates:rectInViewCoordinates:intoImageOfWidth:completionHandler:]):
    
    Canonical link: https://commits.webkit.org/298375@main

--- Source/WebKit/UIProcess/API/ios/WKWebViewIOS.mm
+++ Source/WebKit/UIProcess/API/ios/WKWebViewIOS.mm
@@ -1601,10 +1601,17 @@ static void configureScrollViewWithOverlayRegionsIDs(RetainPtr<WKBaseScrollView>
     if (!surface)
         return nullptr;
 
-    // FIXME: rdar://157581000
-    ALLOW_DEPRECATED_DECLARATIONS_BEGIN
-    CARenderServerRenderLayerWithTransform(MACH_PORT_NULL, self.layer.context.contextId, reinterpret_cast<uint64_t>(self.layer), surface->surface(), 0, 0, &transform);
-    ALLOW_DEPRECATED_DECLARATIONS_END
+    CARenderServerSnapshot(MACH_PORT_NULL, @{
+        kCASnapshotMode: kCASnapshotModeLayer,
+        kCASnapshotDisplayName: kCARenderServerDefaultDisplay,
+        kCASnapshotContextId: @(self.layer.context.contextId),
+        kCASnapshotLayerId: @(reinterpret_cast<uint64_t>(self.layer)),
+        kCASnapshotDestination: (__bridge id)surface->surface(),
+        kCASnapshotOriginX: @(0),
+        kCASnapshotOriginY: @(0),
+        kCASnapshotTransform: @(transform),
+        kCASnapshotTimeOffset: @(0),
+    });
 
 #if HAVE(IOSURFACE_ACCELERATOR)
     WebCore::IOSurface::Format compressedFormat = WebCore::IOSurface::Format::YUV422;
@@ -4821,9 +4828,19 @@ static bool isLockdownModeWarningNeeded()
         CGFloat imageScaleInViewCoordinates = imageWidth / rectInViewCoordinates.size.width;
         CATransform3D transform = CATransform3DMakeScale(imageScaleInViewCoordinates, imageScaleInViewCoordinates, 1);
         transform = CATransform3DTranslate(transform, -rectInViewCoordinates.origin.x, -rectInViewCoordinates.origin.y, 0);
-        ALLOW_DEPRECATED_DECLARATIONS_BEGIN
-        CARenderServerRenderDisplayLayerWithTransformAndTimeOffset(MACH_PORT_NULL, (CFStringRef)displayName, self.layer.context.contextId, reinterpret_cast<uint64_t>(self.layer), surface->surface(), 0, 0, &transform, 0);
-        ALLOW_DEPRECATED_DECLARATIONS_END
+
+        CARenderServerSnapshot(MACH_PORT_NULL, @{
+            kCASnapshotMode: kCASnapshotModeLayer,
+            kCASnapshotDisplayName: displayName,
+            kCASnapshotContextId: @(self.layer.context.contextId),
+            kCASnapshotLayerId: @(reinterpret_cast<uint64_t>(self.layer)),
+            kCASnapshotDestination: (__bridge id)surface->surface(),
+            kCASnapshotOriginX: @(0),
+            kCASnapshotOriginY: @(0),
+            kCASnapshotTransform: @(transform),
+            kCASnapshotTimeOffset: @(0),
+        });
+
         completionHandler(WebCore::IOSurface::sinkIntoImage(WTFMove(surface)).get());
         return;
     }
