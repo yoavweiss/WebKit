yoavweiss/WebKit

commit 57714dd1461f8f99bbf6525cf183eaa3a471faa9

[Live Range Selection] editing/execCommand/insert-list-nested-with-orphaned.html ends with wrong selection https://bugs.webkit.org/show_bug.cgi?id=248981
Reviewed by Darin Adler.

Fixed the bug that these two tests were leaving caret selection instead of selecting across the lists as they should.
New behavior matches that of Firefox & Chrome as well as user's expectation.

* LayoutTests/editing/execCommand/insert-list-nested-with-orphaned-expected.txt:
* LayoutTests/editing/execCommand/insert-list-nested-with-orphaned-live-range-expected.txt:
* Source/WebCore/editing/InsertListCommand.cpp:
(WebCore::InsertListCommand::doApply): Added the code to recover from orphaned selection start.
(WebCore::InsertListCommand::doApplyForSingleParagraph): Fixed the bug that this code was always setting
the ending selection to be in the first position in the new list. Select the entire list instead.

Canonical link: https://commits.webkit.org/257613@main

Source/WebCore/editing/InsertListCommand.cpp
@@ -145,6 +145,9 @@ void InsertListCommand::doApply()
             VisiblePosition endOfSelection = selection.visibleEnd();
             VisiblePosition startOfLastParagraph = startOfParagraph(endOfSelection, CanSkipOverEditingBoundary);
 
+            RefPtr<ContainerNode> startScope;
+            int startIndex = indexForVisiblePosition(startOfSelection, startScope);
+
             if (startOfLastParagraph.isNotNull() && startOfParagraph(startOfSelection, CanSkipOverEditingBoundary) != startOfLastParagraph) {
                 bool forceCreateList = !selectionHasListOfType(selection, listTag);
 
@@ -193,6 +196,8 @@ void InsertListCommand::doApply()
                 doApplyForSingleParagraph(forceCreateList, listTag, currentSelection);
                 // Fetch the end of the selection, for the reason mentioned above.
                 endOfSelection = endingSelection().visibleEnd();
+                if (startOfSelection.isOrphan())
+                    startOfSelection = visiblePositionForIndex(startIndex, startScope.get());
                 setEndingSelection(VisibleSelection(startOfSelection, endOfSelection, endingSelection().isDirectional()));
                 return;
             }
@@ -268,7 +273,8 @@ void InsertListCommand::doApplyForSingleParagraph(bool forceCreateList, const HT
             if (rangeEndIsInList && newList)
                 currentSelection.end = makeBoundaryPointAfterNodeContents(*newList);
 
-            setEndingSelection(VisiblePosition(firstPositionInNode(newList.get())));
+            setEndingSelection(VisibleSelection(makeContainerOffsetPosition(currentSelection.start),
+                makeContainerOffsetPosition(currentSelection.end)));
 
             return;
         }

LayoutTests/editing/execCommand/insert-list-nested-with-orphaned-expected.txt
@@ -3,7 +3,7 @@ This tests hang when listifying nested lists with an orphaned list child in betw
 | <ol>
 |   <ol>
 |     <li>
-|       "hello"
+|       "<#selection-anchor>hello"
 |   "\n    world\n    "
 |   <ol>
 |     <li>
@@ -13,6 +13,6 @@ This tests hang when listifying nested lists with an orphaned list child in betw
 |     "\n        "
 |     <ol>
 |       <li>
-|         "<#selection-caret>because of you"
+|         "because of you<#selection-focus>"
 |   "\n    "
 | "\n"

LayoutTests/editing/execCommand/insert-list-nested-with-orphaned-live-range-expected.txt
@@ -3,7 +3,7 @@ This tests hang when listifying nested lists with an orphaned list child in betw
 | <ol>
 |   <ol>
 |     <li>
-|       "hello"
+|       "<#selection-anchor>hello"
 |   "\n    world\n    "
 |   <ol>
 |     <li>
@@ -13,7 +13,6 @@ This tests hang when listifying nested lists with an orphaned list child in betw
 |     "\n        "
 |     <ol>
 |       <li>
-|         "because of you"
+|         "because of you<#selection-focus>"
 |   "\n    "
-| <#selection-caret>
 | "\n"

commit a9fa4c4410f453d438430acfd89f53c125b4f17f

stack-overflow | WebCore::RenderBox::contentBoxWidth; WebCore::RenderBoxModelObject::paddingRight; WebCore::RenderBox::contentBoxWidth https://bugs.webkit.org/show_bug.cgi?id=288446
Reviewed by Ryosuke Niwa.

The test case regressed with 257613@main. To fix this, revert to the old behavior if the selection
starts and ends outside of the list.

* LayoutTests/editing/execCommand/insert-ordered-list-crash3-expected.txt: Added.
* LayoutTests/editing/execCommand/insert-ordered-list-crash3.html: Added.
* Source/WebCore/editing/InsertListCommand.cpp:
(WebCore::InsertListCommand::doApplyForSingleParagraph):

Originally-landed-as: 292955.9@webkit-embargoed (d77b797e1244). rdar://157791967
Canonical link: https://commits.webkit.org/298441@main

Source/WebCore/editing/InsertListCommand.cpp
@@ -291,8 +291,11 @@ void InsertListCommand::doApplyForSingleParagraph(bool forceCreateList, const HT
             if (rangeEndIsInList && newList)
                 currentSelection.end = makeBoundaryPointAfterNodeContents(*newList);
 
-            setEndingSelection(VisibleSelection(makeContainerOffsetPosition(currentSelection.start),
-                makeContainerOffsetPosition(currentSelection.end)));
+            if ((rangeStartIsInList || rangeEndIsInList) && newList) {
+                setEndingSelection(VisibleSelection(makeContainerOffsetPosition(currentSelection.start),
+                    makeContainerOffsetPosition(currentSelection.end)));
+            } else
+                setEndingSelection(VisibleSelection(firstPositionInNode(newList.get())));
 
             return;
         }

LayoutTests/editing/execCommand/insert-ordered-list-crash3-expected.txt
@@ -1,0 +1,2 @@
+PASS if no crash.
+

LayoutTests/editing/execCommand/insert-ordered-list-crash3.html
@@ -1,0 +1,16 @@
+<!DOCTYPE html>
+<script>
+function main() {
+  if (window.testRunner)
+    testRunner.dumpAsText();
+
+  document.designMode = "on";
+  dl.innerText = "PASS if no crash.";
+  document.execCommand("selectAll");
+  document.execCommand("InsertOrderedList");
+}
+</script>
+<body onload="main()">
+<li> </li>
+<dl id="dl"></dl>
+<table> </table>