c1c96efa07ab7e56877e689a8c0f9d5e2f02f772
Wenson Hsieh <wenson_hsieh@apple.com>
2025-08-15 20:41:49 -0700

    Unreviewed, fix the visionOS build after 298782@main rdar://158456779
    Add API availability for visionOS as well.
    
    * Source/WebKit/UIProcess/API/Cocoa/WKWebViewPrivate.h:
    
    Canonical link: https://commits.webkit.org/298802@main

--- Source/WebKit/UIProcess/API/Cocoa/WKWebViewPrivate.h
+++ Source/WebKit/UIProcess/API/Cocoa/WKWebViewPrivate.h
@@ -636,7 +636,7 @@ typedef NS_OPTIONS(NSUInteger, _WKWebViewDataType) {
 - (NSUInteger)accessibilityUIProcessLocalTokenHash;
 #endif
 
-- (void)_debugTextWithConfiguration:(_WKTextExtractionConfiguration *)configuration completionHandler:(WK_SWIFT_UI_ACTOR void(^)(NSString *))completionHandler WK_API_AVAILABLE(macos(WK_MAC_TBA), ios(WK_IOS_TBA)) NS_SWIFT_NAME(_debugText(with:completionHandler:));
+- (void)_debugTextWithConfiguration:(_WKTextExtractionConfiguration *)configuration completionHandler:(WK_SWIFT_UI_ACTOR void(^)(NSString *))completionHandler WK_API_AVAILABLE(macos(WK_MAC_TBA), ios(WK_IOS_TBA), visionos(WK_XROS_TBA)) NS_SWIFT_NAME(_debugText(with:completionHandler:));
 
 @end
 
