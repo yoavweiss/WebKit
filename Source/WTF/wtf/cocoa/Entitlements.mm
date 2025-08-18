f554797429d2716fb1a2bd89a2319a6c19b3d046
Mark Lam <mark.lam@apple.com>
2025-08-18 00:39:46 -0700

    Make WTF::hasEntitlement() preserve errno. https://bugs.webkit.org/show_bug.cgi?id=297517 rdar://158550300
    Reviewed by Yusuke Suzuki.
    
    We use WTF::hasEntitlement() to sometimes check for the presence of an entitlement, and
    sometimes, to check for its absence.  When the entitlement is absent, it can end up
    setting errno.  This might impact other code running subsequently that check errno.  For
    example, WTF::stringToDouble() was checking errno, and failing due to this.  Though
    WTF::stringToDouble() has now been fixed in 298754@main, we should just make
    hasEntitlement() not change the errno value to avoid other potential fallout like this.
    
    * Source/WTF/wtf/cocoa/Entitlements.mm:
    (WTF::hasEntitlement):
    
    Canonical link: https://commits.webkit.org/298830@main

--- Source/WTF/wtf/cocoa/Entitlements.mm
+++ Source/WTF/wtf/cocoa/Entitlements.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2021 Apple Inc. All rights reserved.
+ * Copyright (C) 2018-2025 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -37,8 +37,11 @@ bool hasEntitlement(SecTaskRef task, ASCIILiteral entitlement)
 {
     if (!task)
         return false;
+    auto savedErrno = errno;
     auto string = entitlement.createCFString();
-    return adoptCF(SecTaskCopyValueForEntitlement(task, string.get(), nullptr)) == kCFBooleanTrue;
+    auto result = adoptCF(SecTaskCopyValueForEntitlement(task, string.get(), nullptr)) == kCFBooleanTrue;
+    errno = savedErrno;
+    return result;
 }
 
 bool hasEntitlement(audit_token_t token, ASCIILiteral entitlement)
