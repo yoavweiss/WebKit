33777aff18f133fb902b6f6b08438fe36f2bbeaa
Javier Fernandez <jfernandez@igalia.com>
2025-08-27 07:10:21 -0700

    Rename the key-pair elements returned in the gcryptGenerateX25519Keys method https://bugs.webkit.org/show_bug.cgi?id=289693
    Reviewed by Carlos Garcia Campos.
    
    The gcryptGenerateX25519Keys method returns a std::pair of KeyMaterial variables
    which should contain the public and private key. The caller expects the first
    element to be the public key.
    
    We were returning the data in the wrong order, so among other issues, it caused
    that the public key generated by the JWK export of the private key doesn't match
    the original key-pair's public key material.
    
    This change fixes the issue by switching the variable names used to hold the
    private and public key pair. This is also more consistent with the nomenclature
    used in the Ed25519 generateKey() method..
    
    Regarding tests, this change make the successes_X25519.https.any.html tests to
    pass, so updating also the platform-specific TestExpectations file.
    
    * LayoutTests/platform/glib/TestExpectations:
    * Source/WebCore/crypto/gcrypt/CryptoKeyOKPGCrypt.cpp:
    (WebCore::gcryptGenerateX25519Keys):
    
    Canonical link: https://commits.webkit.org/299198@main

--- Source/WebCore/crypto/gcrypt/CryptoKeyOKPGCrypt.cpp
+++ Source/WebCore/crypto/gcrypt/CryptoKeyOKPGCrypt.cpp
@@ -100,13 +100,13 @@ static std::optional<std::pair<Vector<uint8_t>, Vector<uint8_t>>> gcryptGenerate
     // private key is just 32 random bytes
     PAL::GCrypt::Handle<gcry_mpi_t> mpi(gcry_mpi_new(256));
     gcry_mpi_randomize(mpi, 256, GCRY_STRONG_RANDOM);
-    auto q = mpiData(mpi);
-    if (!q) [[unlikely]]
+    auto d = mpiData(mpi);
+    if (!d) [[unlikely]]
         return std::nullopt;
 
     // public key being X25519(a, 9), as defined in [RFC7748], section 6.1.
-    auto d = GCrypt::RFC7748::X25519(*q, GCrypt::RFC7748::c_X25519BasePointU);
-    if (!d) [[unlikely]]
+    auto q = GCrypt::RFC7748::X25519(*d, GCrypt::RFC7748::c_X25519BasePointU);
+    if (!q) [[unlikely]]
         return std::nullopt;
 
     return std::make_pair(WTFMove(*q), WTFMove(*d));
