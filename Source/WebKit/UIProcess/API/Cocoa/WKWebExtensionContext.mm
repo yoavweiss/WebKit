yoavweiss/WebKit

commit d348f170390f0ee57cafcfdb0a7040303992775c

Use API::Error instead of NSError on WebExtensionController and WebExtensionContext https://webkit.org/b/297301
Reviewed by Timothy Hatcher.

NSError is Cocoa-only. Using API::Error will allow for cross-platform support of WebExtensionController and WebExtensionContext.

* Source/WebCore/platform/network/cf/ResourceError.h:
* Source/WebCore/platform/network/mac/ResourceErrorMac.mm:
(WebCore::createNSErrorFromResourceErrorBase):
(WebCore::ResourceError::nsError const):
(WebCore::ResourceError::cfError const):
* Source/WebKit/Shared/API/APIError.h:
(API::Error::create):
(API::Error::underlyingError const):
(API::Error::Error):
* Source/WebKit/Shared/Cocoa/WKNSError.mm:
(-[WKNSError _web_createTarget]):
* Source/WebKit/UIProcess/API/Cocoa/WKWebExtensionContext.mm:
(-[WKWebExtensionContext errors]):
(-[WKWebExtensionContext loadBackgroundContentWithCompletionHandler:]):
* Source/WebKit/UIProcess/API/Cocoa/WKWebExtensionController.mm:
(-[WKWebExtensionController loadExtensionContext:error:]):
(-[WKWebExtensionController unloadExtensionContext:error:]):
* Source/WebKit/UIProcess/Extensions/Cocoa/API/WebExtensionContextAPIScriptingCocoa.mm:
(WebKit::WebExtensionContext::createInjectedContentForScripts):
* Source/WebKit/UIProcess/Extensions/Cocoa/WebExtensionActionCocoa.mm:
(WebKit::WebExtensionAction::icon):
* Source/WebKit/UIProcess/Extensions/Cocoa/WebExtensionContextCocoa.mm:
(-[_WKWebExtensionContextDelegate webView:didFailNavigation:withError:]):
(WebKit::WebExtensionContext::recordError):
(WebKit::WebExtensionContext::clearError):
(WebKit::WebExtensionContext::load):
(WebKit::WebExtensionContext::unload):
(WebKit::WebExtensionContext::reload):
(WebKit::WebExtensionContext::loadBackgroundContent):
(WebKit::WebExtensionContext::loadBackgroundWebView):
(WebKit::WebExtensionContext::didFailNavigation):
(WebKit::WebExtensionContext::addInjectedContent):
(WebKit::WebExtensionContext::loadDeclarativeNetRequestRules):
(WebKit::toAPI): Deleted.

Source/WebKit/UIProcess/API/Cocoa/WKWebExtensionContext.mm
@@ -31,6 +31,7 @@
 #import "WKWebExtensionContextInternal.h"
 
 #import "CocoaHelpers.h"
+#import "WKNSError.h"
 #import "WKWebExtensionCommandInternal.h"
 #import "WKWebExtensionControllerInternal.h"
 #import "WKWebExtensionInternal.h"
@@ -110,7 +111,9 @@ WK_OBJECT_DEALLOC_IMPL_ON_MAIN_THREAD(WKWebExtensionContext, WebExtensionContext
 
 -(NSArray<NSError *> *)errors
 {
-    return Ref { *_webExtensionContext }->errors();
+    return createNSArray(Ref { *_webExtensionContext }->errors(), [](auto&& child) -> id {
+        return wrapper(child);
+    }).autorelease();
 }
 
 - (NSURL *)baseURL
@@ -541,7 +544,12 @@ static inline WebKit::WebExtensionContext::PermissionState toImpl(WKWebExtension
 
 - (void)loadBackgroundContentWithCompletionHandler:(void (^)(NSError *error))completionHandler
 {
-    Ref { *_webExtensionContext }->loadBackgroundContent(makeBlockPtr(completionHandler));
+    Ref { *_webExtensionContext }->loadBackgroundContent([&](RefPtr<API::Error> error) {
+        if (error)
+            completionHandler(wrapper(error));
+
+        completionHandler(nil);
+    });
 }
 
 - (WKWebExtensionAction *)actionForTab:(id<WKWebExtensionTab>)tab

Source/WebCore/platform/network/cf/ResourceError.h
@@ -50,9 +50,11 @@ public:
     WEBCORE_EXPORT ResourceError(CFErrorRef error);
 
     WEBCORE_EXPORT CFErrorRef cfError() const;
+    WEBCORE_EXPORT CFErrorRef cfError(CFErrorRef) const;
     WEBCORE_EXPORT operator CFErrorRef() const;
     WEBCORE_EXPORT ResourceError(NSError *);
     WEBCORE_EXPORT NSError *nsError() const;
+    WEBCORE_EXPORT NSError *nsError(NSError *) const;
     WEBCORE_EXPORT operator NSError *() const;
 
 

Source/WebCore/platform/network/mac/ResourceErrorMac.mm
@@ -95,7 +95,7 @@ static NSDictionary* dictionaryThatCanCode(NSDictionary* src)
 
 namespace WebCore {
 
-static RetainPtr<NSError> createNSErrorFromResourceErrorBase(const ResourceErrorBase& resourceError)
+static RetainPtr<NSError> createNSErrorFromResourceErrorBase(const ResourceErrorBase& resourceError, NSError *underlyingError = nil)
 {
     RetainPtr<NSMutableDictionary> userInfo = adoptNS([[NSMutableDictionary alloc] init]);
 
@@ -109,6 +109,9 @@ static RetainPtr<NSError> createNSErrorFromResourceErrorBase(const ResourceError
         if (RetainPtr cocoaURL = resourceError.failingURL().createNSURL())
             [userInfo setValue:cocoaURL.get() forKey:NSURLErrorFailingURLErrorKey];
     }
+
+    if (underlyingError)
+        [userInfo setValue:underlyingError forKey:NSUnderlyingErrorKey];
 
     return adoptNS([[NSError alloc] initWithDomain:resourceError.domain().createNSString().get() code:resourceError.errorCode() userInfo:userInfo.get()]);
 }
@@ -264,6 +267,19 @@ NSError *ResourceError::nsError() const
     return m_platformError.get();
 }
 
+NSError *ResourceError::nsError(NSError *underlyingError) const
+{
+    if (isNull()) {
+        ASSERT(!m_platformError);
+        return nil;
+    }
+
+    if (!m_platformError)
+        m_platformError = createNSErrorFromResourceErrorBase(*this, underlyingError);
+
+    return m_platformError.get();
+}
+
 ResourceError::operator NSError *() const
 {
     return nsError();
@@ -272,6 +288,11 @@ ResourceError::operator NSError *() const
 CFErrorRef ResourceError::cfError() const
 {
     return (__bridge CFErrorRef)nsError();
+}
+
+CFErrorRef ResourceError::cfError(CFErrorRef underlyingError) const
+{
+    return (__bridge CFErrorRef)nsError((__bridge NSError *)underlyingError);
 }
 
 ResourceError::operator CFErrorRef() const

Source/WebKit/Shared/API/APIError.h
@@ -42,6 +42,11 @@ public:
         return adoptRef(*new Error(error));
     }
 
+    static Ref<Error> create(const WebCore::ResourceError& error, const RefPtr<API::Error>& underlyingError)
+    {
+        return adoptRef(*new Error(error, underlyingError));
+    }
+
     enum General {
         Internal = 300
     };
@@ -100,6 +105,7 @@ public:
     const WTF::String& localizedDescription() const { return m_platformError.localizedDescription(); }
 
     const WebCore::ResourceError& platformError() const { return m_platformError; }
+    const RefPtr<Error> underlyingError() const { return m_underlyingError; }
 
 private:
     Error()
@@ -111,7 +117,14 @@ private:
     {
     }
 
+    Error(const WebCore::ResourceError& error, const RefPtr<Error>& underlyingError)
+        : m_platformError(error)
+        , m_underlyingError(underlyingError)
+    {
+    }
+
     WebCore::ResourceError m_platformError;
+    RefPtr<Error> m_underlyingError;
 };
 
 } // namespace API

Source/WebKit/Shared/Cocoa/WKNSError.mm
@@ -32,6 +32,9 @@
 
 - (NSObject *)_web_createTarget
 {
+    RefPtr underlyingError = downcast<API::Error>(&self._apiObject)->underlyingError();
+    if (underlyingError)
+        return [(__bridge NSError *)downcast<API::Error>(&self._apiObject)->platformError().cfError((__bridge CFErrorRef)wrapper(*underlyingError)) copy];
     return [(__bridge NSError *)downcast<API::Error>(&self._apiObject)->platformError().cfError() copy];
 }
 

Source/WebKit/UIProcess/API/Cocoa/WKWebExtensionController.mm
@@ -30,6 +30,7 @@
 #import "config.h"
 #import "WKWebExtensionControllerInternal.h"
 
+#import "WKNSError.h"
 #import "WKWebExtensionContextInternal.h"
 #import "WKWebExtensionControllerConfigurationInternal.h"
 #import "WKWebExtensionDataRecordInternal.h"
@@ -79,14 +80,36 @@ WK_OBJECT_DEALLOC_IMPL_ON_MAIN_THREAD(WKWebExtensionController, WebExtensionCont
 {
     NSParameterAssert([extensionContext isKindOfClass:WKWebExtensionContext.class]);
 
-    return Ref { *_webExtensionController }->load(Ref { extensionContext._webExtensionContext }, outError);
+    if (outError)
+        *outError = nil;
+
+    auto loadResult = Ref { *_webExtensionController }->load(Ref { extensionContext._webExtensionContext });
+
+    if (!loadResult) {
+        if (outError)
+            *outError = wrapper(loadResult.error());
+        return false;
+    }
+
+    return loadResult.value();
 }
 
 - (BOOL)unloadExtensionContext:(WKWebExtensionContext *)extensionContext error:(NSError **)outError
 {
     NSParameterAssert([extensionContext isKindOfClass:WKWebExtensionContext.class]);
 
-    return Ref { *_webExtensionController }->unload(Ref { extensionContext._webExtensionContext }, outError);
+    if (outError)
+        *outError = nil;
+
+    auto unloadResult = Ref { *_webExtensionController }->unload(Ref { extensionContext._webExtensionContext });
+
+    if (!unloadResult) {
+        if (outError)
+            *outError = wrapper(unloadResult.error());
+        return false;
+    }
+
+    return unloadResult.value();
 }
 
 - (WKWebExtensionContext *)extensionContextForExtension:(WKWebExtension *)extension

Source/WebKit/UIProcess/Extensions/Cocoa/API/WebExtensionContextAPIScriptingCocoa.mm
@@ -369,7 +369,7 @@ bool WebExtensionContext::createInjectedContentForScripts(const Vector<WebExtens
         for (NSString *scriptPath in scriptPaths) {
             RefPtr<API::Error> error;
             if (!extension->resourceStringForPath(scriptPath, error)) {
-                recordError(::WebKit::wrapper(*error));
+                recordError(*error);
                 *errorMessage = toErrorString(callingAPIName, nullString(), @"invalid resource '%@'", scriptPath).createNSString().autorelease();
                 return false;
             }
@@ -384,7 +384,7 @@ bool WebExtensionContext::createInjectedContentForScripts(const Vector<WebExtens
         for (NSString *styleSheetPath in styleSheetPaths) {
             RefPtr<API::Error> error;
             if (!extension->resourceStringForPath(styleSheetPath, error)) {
-                recordError(::WebKit::wrapper(*error));
+                recordError(*error);
                 *errorMessage = toErrorString(callingAPIName, nullString(), @"invalid resource '%@'", styleSheetPath).createNSString().autorelease();
                 return false;
             }

Source/WebKit/UIProcess/Extensions/Cocoa/WebExtensionActionCocoa.mm
@@ -750,13 +750,13 @@ RefPtr<WebCore::Icon> WebExtensionAction::icon(WebCore::FloatSize idealSize)
 #if ENABLE(WK_WEB_EXTENSIONS_ICON_VARIANTS)
         if (m_customIconVariants) {
             result = extensionContext->protectedExtension()->bestIconVariant(m_customIconVariants, WebCore::FloatSize(idealSize), [&](Ref<API::Error> error) {
-                extensionContext->recordError(::WebKit::wrapper(error));
+                extensionContext->recordError(error);
             });
         } else
 #endif // ENABLE(WK_WEB_EXTENSIONS_ICON_VARIANTS)
         if (m_customIcons) {
             result = extensionContext->protectedExtension()->bestIcon(m_customIcons, WebCore::FloatSize(idealSize), [&](Ref<API::Error> error) {
-                extensionContext->recordError(::WebKit::wrapper(error));
+                extensionContext->recordError(error);
             });
         }
 

commit f0dcf69e2b27e4742cb324de120947ec2012a490

Safari crash when opening extension background content that isn't loaded https://bugs.webkit.org/show_bug.cgi?id=298128 rdar://159469302
Reviewed by Brian Weinstein.

This patch fixes a crash that was introduced by 299009@main. It was using capture by reference for
the completion handler block, causing us to reference deallocated memory. We need to be using
makeBlockPtr instead to properly retain the completion handler block for later use.

* Source/WebKit/UIProcess/API/Cocoa/WKWebExtensionContext.mm:
(-[WKWebExtensionContext loadBackgroundContentWithCompletionHandler:]):

Canonical link: https://commits.webkit.org/299355@main

Source/WebKit/UIProcess/API/Cocoa/WKWebExtensionContext.mm
@@ -544,11 +544,11 @@ static inline WebKit::WebExtensionContext::PermissionState toImpl(WKWebExtension
 
 - (void)loadBackgroundContentWithCompletionHandler:(void (^)(NSError *error))completionHandler
 {
-    Ref { *_webExtensionContext }->loadBackgroundContent([&](RefPtr<API::Error> error) {
+    Ref { *_webExtensionContext }->loadBackgroundContent([capturedBlock = makeBlockPtr(completionHandler)](RefPtr<API::Error> error) {
         if (error)
-            completionHandler(wrapper(error));
+            capturedBlock(wrapper(error));
 
-        completionHandler(nil);
+        capturedBlock(nil);
     });
 }
 