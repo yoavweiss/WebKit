yoavweiss/WebKit

commit aa1932b98171888b068213d2117d791a4e69810c

[iOS] Increase the duration of capture without prompt in case of user gesture rdar://159739007 https://bugs.webkit.org/show_bug.cgi?id=298307
Reviewed by Eric Carlson.

We increase the prompt timer in iOS for 1 minute to 10 minutes.
But we keep prompting after 1 minute of inactive capture if getUserMedia is not called from a user gesture.
If called from a user gesture, we will prompt after 10 minutes.

No change on macOS.

* Source/WTF/Scripts/Preferences/UnifiedWebPreferences.yaml:
* Source/WebKit/Shared/WebPreferencesDefaultValues.cpp:
(WebKit::defaultInactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes):
* Source/WebKit/Shared/WebPreferencesDefaultValues.h:
* Source/WebKit/UIProcess/API/Cocoa/WKPreferences.mm:
(-[WKPreferences _inactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes]):
(-[WKPreferences _setInactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes:]):
* Source/WebKit/UIProcess/API/Cocoa/WKPreferencesPrivate.h:
* Source/WebKit/UIProcess/UserMediaPermissionRequestManagerProxy.cpp:
(WebKit::UserMediaPermissionRequestManagerProxy::inactiveMediaCaptureStreamDuration const):
(WebKit::UserMediaPermissionRequestManagerProxy::hasGrantedRequest const):
(WebKit::UserMediaPermissionRequestManagerProxy::getRequestAction):
(WebKit::UserMediaPermissionRequestManagerProxy::shouldChangePromptToGrantForCamera const):
(WebKit::UserMediaPermissionRequestManagerProxy::shouldChangePromptToGrantForMicrophone const):
(WebKit::UserMediaPermissionRequestManagerProxy::captureStateChanged):
(WebKit::UserMediaPermissionRequestManagerProxy::searchForGrantedRequest const): Deleted.
* Source/WebKit/UIProcess/UserMediaPermissionRequestManagerProxy.h:
* Tools/TestWebKitAPI/Tests/WebKit/GetUserMediaReprompt.mm:
(TestWebKitAPI::TEST(WebKit2, GetUserMediaRepromptWithoutUserGesture)):

Canonical link: https://commits.webkit.org/299600@main

Source/WebKit/UIProcess/API/Cocoa/WKPreferencesPrivate.h
@@ -122,6 +122,7 @@ typedef NS_ENUM(NSInteger, _WKPitchCorrectionAlgorithm) {
 @property (nonatomic, setter=_setEnumeratingAllNetworkInterfacesEnabled:) BOOL _enumeratingAllNetworkInterfacesEnabled WK_API_AVAILABLE(macos(10.13), ios(11.0));
 @property (nonatomic, setter=_setICECandidateFilteringEnabled:) BOOL _iceCandidateFilteringEnabled WK_API_AVAILABLE(macos(10.13.4), ios(11.3));
 @property (nonatomic, setter=_setInactiveMediaCaptureStreamRepromptIntervalInMinutes:) double _inactiveMediaCaptureStreamRepromptIntervalInMinutes WK_API_AVAILABLE(macos(10.13.4), ios(11.3));
+@property (nonatomic, setter=_setInactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes:) double _inactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes WK_API_AVAILABLE(WK_MAC_TBA, WK_IOS_TBA);
 @property (nonatomic, setter=_setInterruptAudioOnPageVisibilityChangeEnabled:) BOOL _interruptAudioOnPageVisibilityChangeEnabled WK_API_AVAILABLE(macos(10.15), ios(13.0));
 
 @property (nonatomic, setter=_setJavaScriptCanAccessClipboard:) BOOL _javaScriptCanAccessClipboard WK_API_AVAILABLE(macos(10.13), ios(11.0));

Source/WTF/Scripts/Preferences/UnifiedWebPreferences.yaml
@@ -3575,7 +3575,17 @@ InactiveMediaCaptureStreamRepromptIntervalInMinutes:
   exposed: [ WebKit ]
   defaultValue:
     WebKit:
-      default: defaultInactiveMediaCaptureStreamRepromptIntervalInMinutes()
+      default: 10
+
+InactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes:
+  type: double
+  status: embedder
+  condition: ENABLE(MEDIA_STREAM)
+  webcoreBinding: none
+  exposed: [ WebKit ]
+  defaultValue:
+    WebKit:
+      default: defaultInactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes()
 
 IncludeIgnoredInCoreAXTree:
   type: bool

Source/WebKit/Shared/WebPreferencesDefaultValues.cpp
@@ -113,7 +113,6 @@ bool defaultAppleMailPaginationQuirkEnabled()
 #endif
 
 #if ENABLE(MEDIA_STREAM)
-
 bool defaultCaptureAudioInGPUProcessEnabled()
 {
 #if ENABLE(GPU_PROCESS_BY_DEFAULT)
@@ -133,7 +132,7 @@ bool defaultManageCaptureStatusBarInGPUProcessEnabled()
 #endif
 }
 
-double defaultInactiveMediaCaptureStreamRepromptIntervalInMinutes()
+double defaultInactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes()
 {
     constexpr double inactiveMediaCaptureStreamRepromptIntervalForDesktop = 10;
 

Source/WebKit/Shared/WebPreferencesDefaultValues.h
@@ -103,7 +103,7 @@ bool defaultAppleMailPaginationQuirkEnabled();
 #if ENABLE(MEDIA_STREAM)
 bool defaultCaptureAudioInGPUProcessEnabled();
 bool defaultManageCaptureStatusBarInGPUProcessEnabled();
-double defaultInactiveMediaCaptureStreamRepromptIntervalInMinutes();
+double defaultInactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes();
 #endif
 
 #if ENABLE(MEDIA_SOURCE) && PLATFORM(IOS_FAMILY)

Source/WebKit/UIProcess/API/Cocoa/WKPreferences.mm
@@ -722,6 +722,16 @@ static _WKStorageBlockingPolicy toAPI(WebCore::StorageBlockingPolicy policy)
     _preferences->setInactiveMediaCaptureStreamRepromptIntervalInMinutes(interval);
 }
 
+- (double)_inactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes
+{
+    return _preferences->inactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes();
+}
+
+- (void)_setInactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes:(double)interval
+{
+    _preferences->setInactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes(interval);
+}
+
 - (BOOL)_interruptAudioOnPageVisibilityChangeEnabled
 {
     return _preferences->interruptAudioOnPageVisibilityChangeEnabled();

Source/WebKit/UIProcess/UserMediaPermissionRequestManagerProxy.cpp
@@ -428,11 +428,19 @@ void UserMediaPermissionRequestManagerProxy::resetAccess(WebFrameProxy* frame)
     m_deniedRequests.clear();
 }
 
-const UserMediaPermissionRequestProxy* UserMediaPermissionRequestManagerProxy::searchForGrantedRequest(std::optional<FrameIdentifier> frameID, const SecurityOrigin& userMediaDocumentOrigin, const SecurityOrigin& topLevelDocumentOrigin, bool needsAudio, bool needsVideo) const
+Seconds UserMediaPermissionRequestManagerProxy::inactiveMediaCaptureStreamDuration() const
+{
+    return m_lastCaptureTime ? MonotonicTime::now() - *m_lastCaptureTime : 0_s;
+}
+
+bool UserMediaPermissionRequestManagerProxy::hasGrantedRequest(std::optional<FrameIdentifier> frameID, const SecurityOrigin& userMediaDocumentOrigin, const SecurityOrigin& topLevelDocumentOrigin, bool needsAudio, bool needsVideo, bool isUserGesturePriviledged) const
 {
     RefPtr page = m_page.get();
-    if (!page || page->isMediaStreamCaptureMuted())
-        return nullptr;
+    if (!page || page->isMediaStreamCaptureMuted() || m_grantedRequests.isEmpty())
+        return false;
+
+    if (!isUserGesturePriviledged && inactiveMediaCaptureStreamDuration().minutes() > page->protectedPreferences()->inactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes())
+        return false;
 
     bool checkForAudio = needsAudio;
     bool checkForVideo = needsVideo;
@@ -455,9 +463,9 @@ const UserMediaPermissionRequestProxy* UserMediaPermissionRequestManagerProxy::s
         if (checkForVideo || checkForAudio)
             continue;
 
-        return grantedRequest.ptr();
+        return true;
     }
-    return nullptr;
+    return false;
 }
 
 static bool isMatchingDeniedRequest(const UserMediaPermissionRequestProxy& request, const UserMediaPermissionRequestManagerProxy::DeniedRequest& deniedRequest)
@@ -544,7 +552,7 @@ UserMediaPermissionRequestManagerProxy::RequestAction UserMediaPermissionRequest
     if (requestingScreenCapture)
         return RequestAction::Prompt;
 
-    return searchForGrantedRequest(request.frameID(), request.userMediaDocumentSecurityOrigin(), request.topLevelDocumentSecurityOrigin(), requestingMicrophone, requestingCamera) ? RequestAction::Grant : RequestAction::Prompt;
+    return hasGrantedRequest(request.frameID(), request.userMediaDocumentSecurityOrigin(), request.topLevelDocumentSecurityOrigin(), requestingMicrophone, requestingCamera, request.isUserGesturePriviledged()) ? RequestAction::Grant : RequestAction::Prompt;
 }
 #endif
 
@@ -860,12 +868,12 @@ bool UserMediaPermissionRequestManagerProxy::shouldChangeDeniedToPromptForMicrop
 
 bool UserMediaPermissionRequestManagerProxy::shouldChangePromptToGrantForCamera(const ClientOrigin& origin) const
 {
-    return searchForGrantedRequest(std::nullopt, origin.clientOrigin.securityOrigin().get(), origin.topOrigin.securityOrigin().get(), false, true);
+    return hasGrantedRequest(std::nullopt, origin.clientOrigin.securityOrigin().get(), origin.topOrigin.securityOrigin().get(), false, true, true);
 }
 
 bool UserMediaPermissionRequestManagerProxy::shouldChangePromptToGrantForMicrophone(const ClientOrigin& origin) const
 {
-    return searchForGrantedRequest(std::nullopt, origin.clientOrigin.securityOrigin().get(), origin.topOrigin.securityOrigin().get(), true, false);
+    return hasGrantedRequest(std::nullopt, origin.clientOrigin.securityOrigin().get(), origin.topOrigin.securityOrigin().get(), true, false, true);
 }
 
 void UserMediaPermissionRequestManagerProxy::clearUserMediaPermissionRequestHistory(WebCore::PermissionName permissionName)
@@ -1164,10 +1172,13 @@ void UserMediaPermissionRequestManagerProxy::captureStateChanged(MediaProducerMe
     m_captureState = newState & activeCaptureMask;
 
     Seconds interval;
-    if (m_captureState & activeCaptureMask)
+    if (m_captureState & activeCaptureMask) {
         interval = Seconds::fromHours(page->protectedPreferences()->longRunningMediaCaptureStreamRepromptIntervalInHours());
-    else
+        m_lastCaptureTime = { };
+    } else {
         interval = Seconds::fromMinutes(page->protectedPreferences()->inactiveMediaCaptureStreamRepromptIntervalInMinutes());
+        m_lastCaptureTime = MonotonicTime::now();
+    }
 
     if (interval == m_currentWatchdogInterval)
         return;

Source/WebKit/UIProcess/UserMediaPermissionRequestManagerProxy.h
@@ -165,7 +165,7 @@ private:
 #if ENABLE(MEDIA_STREAM)
     void finishGrantingRequest(UserMediaPermissionRequestProxy&);
 
-    const UserMediaPermissionRequestProxy* searchForGrantedRequest(std::optional<WebCore::FrameIdentifier>, const WebCore::SecurityOrigin& userMediaDocumentOrigin, const WebCore::SecurityOrigin& topLevelDocumentOrigin, bool needsAudio, bool needsVideo) const;
+    bool hasGrantedRequest(std::optional<WebCore::FrameIdentifier>, const WebCore::SecurityOrigin& userMediaDocumentOrigin, const WebCore::SecurityOrigin& topLevelDocumentOrigin, bool needsAudio, bool needsVideo, bool isUserGesturePriviledged) const;
     bool wasRequestDenied(const UserMediaPermissionRequestProxy&, bool needsAudio, bool needsVideo, bool needsScreenCapture);
 
     void getUserMediaPermissionInfo(WebCore::FrameIdentifier, Ref<WebCore::SecurityOrigin>&& userMediaDocumentOrigin, Ref<WebCore::SecurityOrigin>&& topLevelDocumentOrigin, CompletionHandler<void(WebCore::PermissionState, WebCore::PermissionState)>&&);
@@ -200,6 +200,8 @@ private:
 
     String ephemeralDeviceHashSaltForFrame(WebCore::FrameIdentifier);
 
+    Seconds inactiveMediaCaptureStreamDuration() const;
+
     RefPtr<UserMediaPermissionRequestProxy> m_currentUserMediaRequest;
     Deque<Ref<UserMediaPermissionRequestProxy>> m_pendingUserMediaRequests;
     HashSet<MediaDevicePermissionRequestIdentifier> m_pendingDeviceRequests;
@@ -233,6 +235,7 @@ private:
     HashCountedSet<String> m_monitoredDeviceIds;
     RetainPtr<WKRotationCoordinatorObserver> m_objcObserver;
 #endif
+    std::optional<MonotonicTime> m_lastCaptureTime;
 };
 
 String convertEnumerationToString(UserMediaPermissionRequestManagerProxy::RequestAction);

Tools/TestWebKitAPI/Tests/WebKit/GetUserMediaReprompt.mm
@@ -108,6 +108,45 @@ TEST(WebKit2, GetUserMediaReprompt)
     [delegate waitUntilPrompted];
 }
 
+TEST(WebKit2, GetUserMediaRepromptWithoutUserGesture)
+{
+    auto configuration = adoptNS([[WKWebViewConfiguration alloc] init]);
+    auto processPoolConfig = adoptNS([[_WKProcessPoolConfiguration alloc] init]);
+    auto preferences = [configuration preferences];
+    preferences._inactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes = 0.5 / 60;
+
+    initializeMediaCaptureConfiguration(configuration.get());
+    auto webView = adoptNS([[GetUserMediaRepromptTestView alloc] initWithFrame:CGRectMake(0, 0, 320, 500) configuration:configuration.get() processPoolConfiguration:processPoolConfig.get()]);
+    auto delegate = adoptNS([[UserMediaCaptureUIDelegate alloc] init]);
+    [webView setUIDelegate:delegate.get()];
+
+    [webView loadTestPageNamed:@"getUserMedia"];
+    [delegate waitUntilPrompted];
+
+    EXPECT_TRUE([webView haveStream:YES]);
+
+    [webView stringByEvaluatingJavaScript:@"stop()"];
+    EXPECT_TRUE([webView haveStream:NO]);
+
+    // Sleep a bit but not long enough for reprompting capture without user gesture.
+    Util::runFor(0.1_s);
+
+    EXPECT_FALSE([delegate wasPrompted]);
+    [webView objectByEvaluatingJavaScript:@"promptForCapture()"];
+    EXPECT_TRUE([webView haveStream:YES]);
+    EXPECT_FALSE([delegate wasPrompted]);
+
+    [webView stringByEvaluatingJavaScript:@"stop()"];
+    EXPECT_TRUE([webView haveStream:NO]);
+
+    // Sleep long enough for the time without capture to be above 0.5s.
+    Util::runFor(1_s);
+
+    // Start capture without user gesture.
+    [webView objectByEvaluatingJavaScript:@"promptForCapture()"];
+    [delegate waitUntilPrompted];
+}
+
 TEST(WebKit2, GetUserMediaRepromptAfterAudioVideoBeingDenied)
 {
     auto configuration = adoptNS([[WKWebViewConfiguration alloc] init]);
@@ -168,7 +207,7 @@ TEST(WebKit2, GetUserMediaDefaultInactiveCaptureRepromptInterval)
 #endif
         return 10;
     };
-    EXPECT_EQ(preferences._inactiveMediaCaptureStreamRepromptIntervalInMinutes, expectedInactiveMediaCaptureStreamRepromptInterval());
+    EXPECT_EQ(preferences._inactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes, expectedInactiveMediaCaptureStreamRepromptInterval());
 }
 
 } // namespace TestWebKitAPI

commit ae9b3a6697577db8d25cafa3b0724285ede0bce0

REGRESSION(299600@main): Broke production builds https://bugs.webkit.org/show_bug.cgi?id=298437 rdar://159921175
Unreviewed build fix.

Production builds broken with 299600@main because the availability
annotations were missing platform identifiers `macos`/`ios` around the
version placeholders, producing build errors like this:

```
/System/Library/Frameworks/WebKit.framework/PrivateHeaders/WKPreferencesPrivate.h:125:184: error: expected ','
123 | @property (nonatomic, setter=_setICECandidateFilteringEnabled:) BOOL _iceCandidateFilteringEnabled API_AVAILABLE(macos(10.13.4), ios(11.3));
124 | @property (nonatomic, setter=_setInactiveMediaCaptureStreamRepromptIntervalInMinutes:) double _inactiveMediaCaptureStreamRepromptIntervalInMinutes API_AVAILABLE(macos(10.13.4), ios(11.3));
125 | @property (nonatomic, setter=_setInactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes:) double _inactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes API_AVAILABLE(NA, NA);
    |                                                                                                                                                                                        `- error: expected ','
126 | @property (nonatomic, setter=_setInterruptAudioOnPageVisibilityChangeEnabled:) BOOL _interruptAudioOnPageVisibilityChangeEnabled API_AVAILABLE(macos(10.15), ios(13.0));
127 |
```

* Source/WebKit/UIProcess/API/Cocoa/WKPreferencesPrivate.h:

Canonical link: https://commits.webkit.org/299609@main

Source/WebKit/UIProcess/API/Cocoa/WKPreferencesPrivate.h
@@ -122,7 +122,7 @@ typedef NS_ENUM(NSInteger, _WKPitchCorrectionAlgorithm) {
 @property (nonatomic, setter=_setEnumeratingAllNetworkInterfacesEnabled:) BOOL _enumeratingAllNetworkInterfacesEnabled WK_API_AVAILABLE(macos(10.13), ios(11.0));
 @property (nonatomic, setter=_setICECandidateFilteringEnabled:) BOOL _iceCandidateFilteringEnabled WK_API_AVAILABLE(macos(10.13.4), ios(11.3));
 @property (nonatomic, setter=_setInactiveMediaCaptureStreamRepromptIntervalInMinutes:) double _inactiveMediaCaptureStreamRepromptIntervalInMinutes WK_API_AVAILABLE(macos(10.13.4), ios(11.3));
-@property (nonatomic, setter=_setInactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes:) double _inactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes WK_API_AVAILABLE(WK_MAC_TBA, WK_IOS_TBA);
+@property (nonatomic, setter=_setInactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes:) double _inactiveMediaCaptureStreamRepromptWithoutUserGestureIntervalInMinutes WK_API_AVAILABLE(macos(WK_MAC_TBA), ios(WK_IOS_TBA));
 @property (nonatomic, setter=_setInterruptAudioOnPageVisibilityChangeEnabled:) BOOL _interruptAudioOnPageVisibilityChangeEnabled WK_API_AVAILABLE(macos(10.15), ios(13.0));
 
 @property (nonatomic, setter=_setJavaScriptCanAccessClipboard:) BOOL _javaScriptCanAccessClipboard WK_API_AVAILABLE(macos(10.13), ios(11.0));