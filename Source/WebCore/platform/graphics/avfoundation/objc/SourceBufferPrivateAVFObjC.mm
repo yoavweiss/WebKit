yoavweiss/WebKit

commit 06ba3466f891d55e663919bc034b02ff88e11047

Remove UseModernAVContentKeySession preference. https://bugs.webkit.org/show_bug.cgi?id=298165 rdar://159544468
Reviewed by Youenn Fablet.

The preferences has been on for a while, remove dead-code.

* Source/WTF/Scripts/Preferences/UnifiedWebPreferences.yaml:
* Source/WebCore/platform/audio/cocoa/MediaSessionManagerCocoa.h:
* Source/WebCore/platform/audio/cocoa/MediaSessionManagerCocoa.mm:
(WebCore::MediaSessionManagerCocoa::setShouldUseModernAVContentKeySession): Deleted.
(WebCore::MediaSessionManagerCocoa::shouldUseModernAVContentKeySession): Deleted.
* Source/WebCore/platform/graphics/avfoundation/objc/CDMInstanceFairPlayStreamingAVFObjC.mm:
(WebCore::CDMInstanceFairPlayStreamingAVFObjC::contentKeySession):
(WebCore::CDMInstanceSessionFairPlayStreamingAVFObjC::protectionStatusForRequest const):
(WebCore::CDMInstanceSessionFairPlayStreamingAVFObjC::ensureSessionOrGroup):
* Source/WebCore/platform/graphics/avfoundation/objc/CDMSessionAVContentKeySession.mm:
(WebCore::CDMSessionAVContentKeySession::contentKeySession):
* Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm:
(WebCore::MediaPlayerPrivateAVFoundationObjC::createAVAssetForURL):
* Source/WebCore/platform/graphics/avfoundation/objc/SourceBufferParserAVFObjC.mm:
(WebCore::SourceBufferParserAVFObjC::SourceBufferParserAVFObjC):
* Source/WebCore/platform/graphics/avfoundation/objc/SourceBufferPrivateAVFObjC.mm:
(WebCore::shouldAddContentKeyRecipients):
(WebCore::SourceBufferPrivateAVFObjC::didProvideContentKeyRequestInitializationDataForTrackID):
(WebCore::SourceBufferPrivateAVFObjC::needsVideoLayer const):
(WebCore::SourceBufferPrivateAVFObjC::attemptToDecrypt):
(WebCore::SourceBufferPrivateAVFObjC::canEnqueueSample):
(WebCore::SourceBufferPrivateAVFObjC::attachContentKeyToSampleIfNeeded):
* Source/WebKit/GPUProcess/GPUProcess.cpp:
(WebKit::GPUProcess::updateGPUProcessPreferences):
* Source/WebKit/GPUProcess/GPUProcessCreationParameters.h:

Source/WebCore/platform/graphics/avfoundation/objc/SourceBufferPrivateAVFObjC.mm
@@ -98,7 +98,7 @@ static inline bool supportsAttachContentKey()
 
 static inline bool shouldAddContentKeyRecipients()
 {
-    return MediaSessionManagerCocoa::shouldUseModernAVContentKeySession() && !supportsAttachContentKey();
+    return !supportsAttachContentKey();
 }
 
 Ref<SourceBufferPrivateAVFObjC> SourceBufferPrivateAVFObjC::create(MediaSourcePrivateAVFObjC& parent, Ref<SourceBufferParser>&& parser)
@@ -314,10 +314,6 @@ void SourceBufferPrivateAVFObjC::didProvideContentKeyRequestInitializationDataFo
     mediaSource->sourceBufferKeyNeeded(this, initData);
 
     if (RefPtr session = player->cdmSession()) {
-        if (MediaSessionManagerCocoa::shouldUseModernAVContentKeySession()) {
-            // no-op.
-        } else if (auto parser = this->streamDataParser())
-            session->addParser(parser);
         if (hasSessionSemaphore)
             hasSessionSemaphore->signal();
         return;
@@ -344,10 +340,6 @@ void SourceBufferPrivateAVFObjC::didProvideContentKeyRequestInitializationDataFo
 
     if (RefPtr cdmInstance = m_cdmInstance) {
         if (RefPtr instanceSession = cdmInstance->sessionForKeyIDs(keyIDs.value())) {
-            if (MediaSessionManagerCocoa::shouldUseModernAVContentKeySession()) {
-                // no-op.
-            } else if (auto parser = this->streamDataParser())
-                [instanceSession->contentKeySession() addContentKeyRecipient:parser];
             if (m_hasSessionSemaphore) {
                 m_hasSessionSemaphore->signal();
                 m_hasSessionSemaphore = nullptr;
@@ -375,14 +367,11 @@ bool SourceBufferPrivateAVFObjC::needsVideoLayer() const
     if (!m_protectedTrackID)
         return false;
 
-    if (!isEnabledVideoTrackID(*m_protectedTrackID))
-        return false;
-
     // When video content is protected and keys are assigned through
     // the renderers, decoding content through decompression sessions
     // will fail. In this scenario, ask the player to create a layer
     // instead.
-    return MediaSessionManagerCocoa::shouldUseModernAVContentKeySession();
+    return !isEnabledVideoTrackID(*m_protectedTrackID);
 }
 
 Ref<MediaPromise> SourceBufferPrivateAVFObjC::appendInternal(Ref<SharedBuffer>&& data)
@@ -747,11 +736,6 @@ void SourceBufferPrivateAVFObjC::attemptToDecrypt()
         RefPtr instanceSession = cdmInstance->sessionForKeyIDs(m_keyIDs);
         if (!instanceSession)
             return;
-
-        if (!MediaSessionManagerCocoa::shouldUseModernAVContentKeySession()) {
-            if (auto parser = this->streamDataParser())
-                [instanceSession->contentKeySession() addContentKeyRecipient:parser];
-        }
     } else if (!m_session.get())
         return;
 
@@ -952,10 +936,6 @@ bool SourceBufferPrivateAVFObjC::canEnqueueSample(TrackID trackID, const MediaSa
     if (!sample.isProtected())
         return true;
 
-    // If sample buffers don't support modern AVContentKeySession: enqueue sample
-    if (!MediaSessionManagerCocoa::shouldUseModernAVContentKeySession())
-        return true;
-
     // if sample is encrypted, but we are not attached to a CDM: do not enqueue sample.
     if (!m_cdmInstance && !m_session.get())
         return false;
@@ -1121,7 +1101,7 @@ void SourceBufferPrivateAVFObjC::enqueueSampleBuffer(MediaSampleAVFObjC& sample,
 
 void SourceBufferPrivateAVFObjC::attachContentKeyToSampleIfNeeded(const MediaSampleAVFObjC& sample)
 {
-    if (!MediaSessionManagerCocoa::shouldUseModernAVContentKeySession() || !supportsAttachContentKey())
+    if (!supportsAttachContentKey())
         return;
 
     if (RefPtr cdmInstance = m_cdmInstance)

Source/WTF/Scripts/Preferences/UnifiedWebPreferences.yaml
@@ -7140,22 +7140,6 @@ ShouldTakeNearSuspendedAssertions:
     WebCore:
       default: true
 
-ShouldUseModernAVContentKeySession:
-  type: bool
-  status: internal
-  category: media
-  humanReadableName: "Modern AVContentKeySession"
-  humanReadableDescription: "Use modern AVContentKeySession"
-  webcoreOnChange: shouldUseModernAVContentKeySessionChanged
-  condition: USE(MODERN_AVCONTENTKEYSESSION)
-  defaultValue:
-    WebKitLegacy:
-      default: true
-    WebKit:
-      default: true
-    WebCore:
-      default: true
-
 # FIXME: This is not relevent for WebKitLegacy, so should be excluded from WebKitLegacy entirely (though we should still set the default value to false when initializing settings).
 # FIXME: This is not used in WebCore, so should not have a binding to WebCore::Settings.
 ShouldUseServiceWorkerShortTimeout:

Source/WTF/wtf/PlatformHave.h
@@ -773,11 +773,6 @@
 #define HAVE_AVCONTENTKEYREQUEST_PENDING_PROTECTION_STATUS 1
 #endif
 
-#if !defined(HAVE_AVCONTENTKEYREQUEST_COMPATABILITIY_MODE) \
-    && (PLATFORM(MAC) || (PLATFORM(IOS_FAMILY) && !PLATFORM(IOS_FAMILY_SIMULATOR)))
-#define HAVE_AVCONTENTKEYREQUEST_COMPATABILITIY_MODE 1
-#endif
-
 #if !defined(HAVE_AVCONTENTKEYREQUEST_RANDOMIZATION_API) \
     && ((PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 260000) \
     || (PLATFORM(IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 260000) \

Source/WTF/wtf/PlatformUse.h
@@ -421,12 +421,6 @@
 #define USE_EXIT_XPC_MESSAGE_WORKAROUND 1
 #endif
 
-#if !defined(USE_MODERN_AVCONTENTKEYSESSION) \
-    && ((PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 140400) \
-    || (PLATFORM(IOS_FAMILY) && !PLATFORM(IOS_FAMILY_SIMULATOR)))
-#define USE_MODERN_AVCONTENTKEYSESSION 1
-#endif
-
 #if !defined(USE_MODERN_AVCONTENTKEYSESSION_WITH_VTDECOMPRESSIONSESSION) \
     && ((PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 150000) \
     || (PLATFORM(IOS_FAMILY) && !PLATFORM(IOS_FAMILY_SIMULATOR)))

Source/WebCore/Configurations/AllowedSPI-legacy.toml
@@ -298,7 +298,6 @@ selectors = [
     { name = "columnSpan", class = "?" },
     { name = "containsAttachments", class = "?" },
     { name = "contentDispositionEncodingFallbackArray", class = "?" },
-    { name = "contentKeySessionWithLegacyWebKitCompatibilityModeAndKeySystem:storageDirectoryAtURL:", class = "?" },
     { name = "coreResult", class = "?" },
     { name = "createFencePort", class = "?" },
     { name = "createPluginBindingsInstance:", class = "?" },

Source/WebCore/Modules/encryptedmedia/MediaKeySession.cpp
@@ -809,13 +809,11 @@ String MediaKeySession::mediaKeysStorageDirectory() const
 
 CDMKeyGroupingStrategy MediaKeySession::keyGroupingStrategy() const
 {
-#if USE(MODERN_AVCONTENTKEYSESSION)
-    RefPtr document = downcast<Document>(scriptExecutionContext());
-    if (document && document->settings().shouldUseModernAVContentKeySession())
-        return CDMKeyGroupingStrategy::BuiltIn;
-#endif
-
+#if USE(AVFOUNDATION)
+    return CDMKeyGroupingStrategy::BuiltIn;
+#else
     return CDMKeyGroupingStrategy::Platform;
+#endif
 }
 
 bool MediaKeySession::virtualHasPendingActivity() const

Source/WebCore/PAL/pal/spi/cocoa/AVFoundationSPI.h
@@ -268,14 +268,6 @@ typedef NS_ENUM(NSInteger, AVExternalContentProtectionStatus) {
 @end
 #endif // HAVE(AVCONTENTKEYREQUEST_PENDING_PROTECTION_STATUS)
 
-#if HAVE(AVCONTENTKEYREQUEST_COMPATABILITIY_MODE)
-NS_ASSUME_NONNULL_BEGIN
-@interface AVContentKeyRequest (AVContentKeyRequest_WebKitCompatibilityMode)
-+ (instancetype)contentKeySessionWithLegacyWebKitCompatibilityModeAndKeySystem:(AVContentKeySystem)keySystem storageDirectoryAtURL:(NSURL *)storageURL;
-@end
-NS_ASSUME_NONNULL_END
-#endif
-
 #endif // HAVE(AVCONTENTKEYSESSION)
 
 #endif // USE(APPLE_INTERNAL_SDK)

Source/WebCore/page/SettingsBase.cpp
@@ -55,9 +55,6 @@
 #if ENABLE(MEDIA_STREAM)
 #include "MockRealtimeMediaSourceCenter.h"
 #endif
-#if USE(MODERN_AVCONTENTKEYSESSION)
-#include "MediaSessionManagerCocoa.h"
-#endif
 
 namespace WebCore {
 
@@ -524,14 +521,6 @@ void SettingsBase::resourceUsageOverlayVisibleChanged()
         m_page->setResourceUsageOverlayVisible(m_page->settings().resourceUsageOverlayVisible());
 #endif
 }
-
-#if USE(MODERN_AVCONTENTKEYSESSION)
-void SettingsBase::shouldUseModernAVContentKeySessionChanged()
-{
-    if (m_page)
-        MediaSessionManagerCocoa::setShouldUseModernAVContentKeySession(m_page->settings().shouldUseModernAVContentKeySession());
-}
-#endif
 
 void SettingsBase::useSystemAppearanceChanged()
 {

commit f3d00e7a8f81b7b0f351ab3c0a87630ca459b1bd

Netflix playback will error https://bugs.webkit.org/show_bug.cgi?id=298518 rdar://160078410
Reviewed by Gerald Squelart.

Typo introduced in 299390@main.

* Source/WebCore/platform/graphics/avfoundation/objc/SourceBufferPrivateAVFObjC.mm:
(WebCore::SourceBufferPrivateAVFObjC::needsVideoLayer const):

Canonical link: https://commits.webkit.org/299684@main

Source/WebCore/platform/graphics/avfoundation/objc/SourceBufferPrivateAVFObjC.mm
@@ -364,14 +364,11 @@ void SourceBufferPrivateAVFObjC::didProvideContentKeyRequestInitializationDataFo
 
 bool SourceBufferPrivateAVFObjC::needsVideoLayer() const
 {
-    if (!m_protectedTrackID)
-        return false;
-
     // When video content is protected and keys are assigned through
     // the renderers, decoding content through decompression sessions
     // will fail. In this scenario, ask the player to create a layer
     // instead.
-    return !isEnabledVideoTrackID(*m_protectedTrackID);
+    return m_protectedTrackID && isEnabledVideoTrackID(*m_protectedTrackID);
 }
 
 Ref<MediaPromise> SourceBufferPrivateAVFObjC::appendInternal(Ref<SharedBuffer>&& data)