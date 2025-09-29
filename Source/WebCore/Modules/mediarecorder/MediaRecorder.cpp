9671d6e9c9773473d974c053cd8c7f1bc6a9788c
Youenn Fablet <youenn@apple.com>
2025-09-29 05:28:03 -0700

    Limit the events fired by a stopped MediaRecorder in case of change of track states rdar://161124260 https://bugs.webkit.org/show_bug.cgi?id=299601
    Reviewed by Jean-Yves Avenard.
    
    When a MediaRecorder is stopped just after its stream has changed (track stopped or track addition/removal),
    we were previously firing error events, even though the actual recording stop was triggered by the web page.
    
    We are now exiting early if the recorder is stopped while we process the stream changes.
    This aligns with Chrome behavior.
    After the patch, we get the same behavior for Firefox in one case and get closer to Firefox in another case.
    
    * LayoutTests/imported/w3c/web-platform-tests/mediacapture-record/MediaRecorder-stop-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/mediacapture-record/MediaRecorder-stop.html:
    * LayoutTests/platform/glib/imported/w3c/web-platform-tests/mediacapture-record/MediaRecorder-stop-expected.txt: Removed.
    * Source/WebCore/Modules/mediarecorder/MediaRecorder.cpp:
    (WebCore::MediaRecorder::handleTrackChange):
    (WebCore::MediaRecorder::trackEnded):
    
    Canonical link: https://commits.webkit.org/300682@main

--- Source/WebCore/Modules/mediarecorder/MediaRecorder.cpp
+++ Source/WebCore/Modules/mediarecorder/MediaRecorder.cpp
@@ -383,6 +383,9 @@ void MediaRecorder::stopRecordingInternal(CompletionHandler<void()>&& completion
 void MediaRecorder::handleTrackChange()
 {
     queueTaskKeepingObjectAlive(*this, TaskSource::Networking, [](auto& recorder) {
+        if (recorder.state() == RecordingState::Inactive)
+            return;
+
         recorder.stopRecordingInternal([pendingActivity = recorder.makePendingActivity(recorder)] {
             Ref protectedRecorder = pendingActivity->object();
             queueTaskKeepingObjectAlive(protectedRecorder.get(), TaskSource::Networking, [](auto& recorder) {
@@ -418,6 +421,9 @@ void MediaRecorder::trackEnded(MediaStreamTrackPrivate&)
         return;
 
     queueTaskKeepingObjectAlive(*this, TaskSource::Networking, [](auto& recorder) {
+        if (recorder.state() == RecordingState::Inactive)
+            return;
+
         recorder.stopRecordingInternal([pendingActivity = recorder.makePendingActivity(recorder)] {
             queueTaskKeepingObjectAlive(pendingActivity->object(), TaskSource::Networking, [](auto& recorder) {
                 if (!recorder.m_isActive)
