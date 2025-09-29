cf37c23ad69cb407e17d9f132de8740e4ae7f54a
Razvan Caliman <rcaliman@apple.com>
2025-09-29 10:44:18 -0700

    Web Inspector: Rename WI.DOMTreeElement.prototype._elementCloseTag https://bugs.webkit.org/show_bug.cgi?id=299519 rdar://161318308
    Reviewed by Devin Rousso.
    
    While investigating https://commits.webkit.org/300471@main, the naming of `_elementCloseTag` proved confusing at times.
    It is a boolean indicating whether the `WI.DOMTreeElement` represents the closing tag of a DOM element.
    
    There's a similarly named member, `_closeTagTreeElement`,
    which references the actual element of the close tag in the DOM tree outline.
    
    This patch renames `_elementCloseTag` to `isElementCloseTag`, renames the public getter,
    and uses the same name across all call sites to make it easier to track across objects.
    
    * Source/WebInspectorUI/UserInterface/Views/DOMTreeContentView.js:
    (WI.DOMTreeContentView.prototype.get selectionPathComponents):
    * Source/WebInspectorUI/UserInterface/Views/DOMTreeElement.js:
    (WI.DOMTreeElement.prototype.get isElementCloseTag):
    (WI.DOMTreeElement.prototype.get expandedChildCount):
    (WI.DOMTreeElement.prototype.showChildNode):
    (WI.DOMTreeElement.prototype.onpopulate):
    (WI.DOMTreeElement.prototype.updateChildren):
    (WI.DOMTreeElement.prototype.insertChildElement):
    (WI.DOMTreeElement.prototype._updateChildren):
    (WI.DOMTreeElement.prototype.onexpand):
    (WI.DOMTreeElement.prototype.oncollapse):
    (WI.DOMTreeElement.prototype.ondblclick):
    (WI.DOMTreeElement.prototype._startEditing):
    (WI.DOMTreeElement.prototype._nodeTitleInfo):
    (WI.DOMTreeElement.prototype._updatePseudoClassIndicator):
    (WI.DOMTreeElement.prototype._createBadges):
    (WI.DOMTreeElement.prototype.isCloseTag): Deleted.
    * Source/WebInspectorUI/UserInterface/Views/DOMTreeElementPathComponent.js:
    (WI.DOMTreeElementPathComponent.prototype.get nextSibling):
    * Source/WebInspectorUI/UserInterface/Views/DOMTreeOutline.js:
    (WI.DOMTreeOutline.prototype.update):
    (WI.DOMTreeOutline.prototype.ondelete):
    (WI.DOMTreeOutline.prototype.objectForSelection):
    (WI.DOMTreeOutline.prototype._ondrop):
    
    Canonical link: https://commits.webkit.org/300703@main

--- Source/WebInspectorUI/UserInterface/Views/DOMTreeContentView.js
+++ Source/WebInspectorUI/UserInterface/Views/DOMTreeContentView.js
@@ -188,7 +188,7 @@ WI.DOMTreeContentView = class DOMTreeContentView extends WI.ContentView
         while (treeElement && !treeElement.root) {
             // The close tag is contained within the element it closes. So skip it since we don't want to
             // show the same node twice in the hierarchy.
-            if (treeElement.isCloseTag()) {
+            if (treeElement.isElementCloseTag) {
                 treeElement = treeElement.parent;
                 continue;
             }
