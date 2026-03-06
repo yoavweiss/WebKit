afb79e1a6b1c124a105769be957d7fe3c7b7b800
Simon Pena <spena@igalia.com>
2026-03-06 05:56:19 -0800

    [WPE] Implement playEffect by supporting gamepad rumble https://bugs.webkit.org/show_bug.cgi?id=309269
    Reviewed by Carlos Garcia Campos.
    
    This implements playEffect and stopEffects in GamepadProviderWPE by
    dispatching a playEffect request to PlatformGamepadWPE, which routes it
    to WPEGamepad.
    
    WPEGamepad defines new has_rumble and rumble methods, and a
    libmanette-based implementation is provided. playEffect is then mapped
    into a rumble call, gating it by the specific effect being supported.
    
    The concrete libmanette-based implementation does a version check, since
    prior to 0.2.13 the API expected uints, and afterwards doubles like the
    gamepad spec.
    
    defaultGamepadVibrationActuatorEnabled is therefore enabled for WPE
    Platform.
    
    Tested manually on https://hardwaretester.com/gamepad.
    
    * Source/WebKit/Shared/WebPreferencesDefaultValues.cpp:
    (WebKit::defaultGamepadVibrationActuatorEnabled):
    * Source/WebKit/UIProcess/Gamepad/wpe/GamepadProviderWPE.cpp:
    (WebKit::GamepadProviderWPE::gamepadDisconnected):
    (WebKit::GamepadProviderWPE::playEffect):
    (WebKit::GamepadProviderWPE::stopEffects):
    * Source/WebKit/UIProcess/Gamepad/wpe/PlatformGamepadWPE.cpp:
    (WebKit::PlatformGamepadWPE::PlatformGamepadWPE):
    (WebKit::PlatformGamepadWPE::playEffect):
    (WebKit::PlatformGamepadWPE::stopEffects):
    (WebKit::PlatformGamepadWPE::effectDelayTimerFired):
    (WebKit::PlatformGamepadWPE::startRumble):
    (WebKit::PlatformGamepadWPE::effectDurationTimerFired):
    * Source/WebKit/UIProcess/Gamepad/wpe/PlatformGamepadWPE.h:
    * Source/WebKit/WPEPlatform/wpe/WPEGamepad.cpp:
    (wpe_gamepad_has_rumble):
    (wpe_gamepad_rumble):
    * Source/WebKit/WPEPlatform/wpe/WPEGamepad.h:
    * Source/WebKit/WPEPlatform/wpe/WPEGamepadManette.cpp:
    (wpeGamepadManetteHasRumble):
    (wpeGamepadManetteRumble):
    (wpe_gamepad_manette_class_init):
    
    Canonical link: https://commits.webkit.org/308792@main

--- Source/WebKit/UIProcess/Gamepad/wpe/PlatformGamepadWPE.h
+++ Source/WebKit/UIProcess/Gamepad/wpe/PlatformGamepadWPE.h
@@ -26,6 +26,7 @@
 #pragma once
 
 #if ENABLE(GAMEPAD) && ENABLE(WPE_PLATFORM)
+#include <WebCore/GamepadEffectParameters.h>
 #include <WebCore/PlatformGamepad.h>
 #include <wtf/TZoneMalloc.h>
 #include <wtf/glib/GRefPtr.h>
@@ -44,13 +45,23 @@ public:
 private:
     const Vector<WebCore::SharedGamepadValue>& buttonValues() const final { return m_buttonValues; }
     const Vector<WebCore::SharedGamepadValue>& axisValues() const final { return m_axisValues; }
+    void playEffect(GamepadHapticEffectType, const GamepadEffectParameters&, CompletionHandler<void(bool)>&&) final;
+    void stopEffects(CompletionHandler<void()>&&) final;
 
     void buttonEvent(size_t button, bool isPressed);
     void axisEvent(size_t axis, double value);
 
+    void effectDelayTimerFired();
+    void effectDurationTimerFired();
+    void startRumble(const GamepadEffectParameters&);
+
     GRefPtr<WPEGamepad> m_gamepad;
     Vector<WebCore::SharedGamepadValue> m_buttonValues;
     Vector<WebCore::SharedGamepadValue> m_axisValues;
+    RunLoop::Timer m_effectDelayTimer;
+    RunLoop::Timer m_effectDurationTimer;
+    CompletionHandler<void(bool)> m_effectCompletionHandler;
+    GamepadEffectParameters m_pendingEffectParameters;
 };
 
 } // namespace WebKit
