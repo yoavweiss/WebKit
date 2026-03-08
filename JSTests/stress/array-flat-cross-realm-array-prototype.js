yoavweiss/WebKit

commit 546d47afe6bf2406ad983b5c35e074b85d1643e1

[JSC] Implement `Array.prototype.flat` in C++ https://bugs.webkit.org/show_bug.cgi?id=294503
Reviewed by Yusuke Suzuki.

This patch reverts 300343@main to reland 298127@main.

The only change from the original patch is to treat depthNum
as 1 instead of 0 in patterns like `array.flat(undefined)`.

I confirmed that Google Docs works with this patch on my local
Minibrowser build.

-----
(commit message of the original patch)

This patch implements `Array.prototype.flat` in C++.

The new fast path for `Array#flat` first calculates the length
of the resulting array using O(n) `calculateFlattenedLength`,
then directly sets values into the butterfly buffer of the
resulting array using O(n) `fastFlatIntoBuffer`.

                                             TipOfTree                  Patched

array-prototype-flat-depth-1-string      474.3101+-47.2922    ^    146.0750+-46.3992       ^ definitely 3.2470x faster
array-prototype-flat-depth-1-double      352.0065+-14.2086    ^    156.4253+-54.2939       ^ definitely 2.2503x faster
array-prototype-flat-large-nested        110.8177+-40.8710    ^     46.0452+-18.8216       ^ definitely 2.4067x faster
array-prototype-flat-huge-arrays          41.7200+-14.7628    ^     20.4875+-5.9254        ^ definitely 2.0364x faster
array-prototype-flat-small-arrays         10.1293+-4.8824     ^      3.8282+-0.4837        ^ definitely 2.6460x faster
array-prototype-flat-depth-infinity      536.4026+-177.3859   ^    222.0425+-35.8608       ^ definitely 2.4158x faster
array-prototype-flat-depth-2             475.2299+-93.6660    ^    179.1500+-63.5912       ^ definitely 2.6527x faster
array-prototype-flat-depth-1-int32       453.2263+-184.5728   ^    154.1721+-56.2397       ^ definitely 2.9397x faster
array-prototype-flat-depth-3             471.8799+-105.4956   ^    151.9625+-72.1298       ^ definitely 3.1052x faster
array-prototype-flat-sparse-array        172.9858+-58.6417    ?    219.7328+-64.4558       ? might be 1.2702x slower

Source/JavaScriptCore/runtime/ArrayPrototype.cpp
@@ -71,6 +71,7 @@ static JSC_DECLARE_HOST_FUNCTION(arrayProtoFuncWith);
 static JSC_DECLARE_HOST_FUNCTION(arrayProtoFuncIncludes);
 static JSC_DECLARE_HOST_FUNCTION(arrayProtoFuncCopyWithin);
 static JSC_DECLARE_HOST_FUNCTION(arrayProtoFuncToSpliced);
+static JSC_DECLARE_HOST_FUNCTION(arrayProtoFuncFlat);
 
 // ------------------------------ ArrayPrototype ----------------------------
 
@@ -117,7 +118,7 @@ void ArrayPrototype::finishCreation(VM& vm, JSGlobalObject* globalObject)
     JSC_NATIVE_INTRINSIC_FUNCTION_WITHOUT_TRANSITION(vm.propertyNames->builtinNames().indexOfPublicName(), arrayProtoFuncIndexOf, static_cast<unsigned>(PropertyAttribute::DontEnum), 1, ImplementationVisibility::Public, ArrayIndexOfIntrinsic);
     JSC_NATIVE_FUNCTION_WITHOUT_TRANSITION("lastIndexOf"_s, arrayProtoFuncLastIndexOf, static_cast<unsigned>(PropertyAttribute::DontEnum), 1, ImplementationVisibility::Public);
     JSC_BUILTIN_FUNCTION_WITHOUT_TRANSITION(vm.propertyNames->builtinNames().filterPublicName(), arrayPrototypeFilterCodeGenerator, static_cast<unsigned>(PropertyAttribute::DontEnum));
-    JSC_BUILTIN_FUNCTION_WITHOUT_TRANSITION(vm.propertyNames->builtinNames().flatPublicName(), arrayPrototypeFlatCodeGenerator, static_cast<unsigned>(PropertyAttribute::DontEnum));
+    JSC_NATIVE_FUNCTION_WITHOUT_TRANSITION(vm.propertyNames->flat, arrayProtoFuncFlat, static_cast<unsigned>(PropertyAttribute::DontEnum), 0, ImplementationVisibility::Public);
     JSC_BUILTIN_FUNCTION_WITHOUT_TRANSITION(vm.propertyNames->builtinNames().flatMapPublicName(), arrayPrototypeFlatMapCodeGenerator, static_cast<unsigned>(PropertyAttribute::DontEnum));
     JSC_BUILTIN_FUNCTION_WITHOUT_TRANSITION(vm.propertyNames->builtinNames().reducePublicName(), arrayPrototypeReduceCodeGenerator, static_cast<unsigned>(PropertyAttribute::DontEnum));
     JSC_BUILTIN_FUNCTION_WITHOUT_TRANSITION(vm.propertyNames->builtinNames().reduceRightPublicName(), arrayPrototypeReduceRightCodeGenerator, static_cast<unsigned>(PropertyAttribute::DontEnum));
@@ -155,7 +156,7 @@ void ArrayPrototype::finishCreation(VM& vm, JSGlobalObject* globalObject)
         &vm.propertyNames->builtinNames().findIndexPublicName(),
         &vm.propertyNames->builtinNames().findLastPublicName(),
         &vm.propertyNames->builtinNames().findLastIndexPublicName(),
-        &vm.propertyNames->builtinNames().flatPublicName(),
+        &vm.propertyNames->flat,
         &vm.propertyNames->builtinNames().flatMapPublicName(),
         &vm.propertyNames->includes,
         &vm.propertyNames->builtinNames().keysPublicName(),
@@ -2108,6 +2109,114 @@ JSC_DEFINE_HOST_FUNCTION(arrayProtoFuncToSpliced, (JSGlobalObject* globalObject,
         RETURN_IF_EXCEPTION(scope, { });
     }
 
+    return JSValue::encode(result);
+}
+
+static uint64_t flatIntoArray(JSGlobalObject* globalObject, JSObject* target, JSObject* source, uint64_t sourceLength, uint64_t targetIndex, uint64_t depth)
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    if (!vm.isSafeToRecurseSoft()) [[unlikely]] {
+        throwStackOverflowError(globalObject, scope);
+        return std::numeric_limits<uint64_t>::max();
+    }
+
+    for (uint64_t sourceIndex = 0; sourceIndex < sourceLength; ++sourceIndex) {
+        JSValue element = source->getIfPropertyExists(globalObject, sourceIndex);
+        RETURN_IF_EXCEPTION(scope, { });
+        if (!element)
+            continue;
+
+        bool elementIsArray = isArray(globalObject, element);
+        RETURN_IF_EXCEPTION(scope, { });
+        if (depth > 0 && elementIsArray) {
+            uint64_t newDepth = depth - 1;
+            if (depth == std::numeric_limits<uint64_t>::max()) [[unlikely]]
+                newDepth = depth;
+            JSObject* elementObject = asObject(element);
+            uint64_t elementLength = toLength(globalObject, elementObject);
+            RETURN_IF_EXCEPTION(scope, { });
+            targetIndex = flatIntoArray(globalObject, target, elementObject, elementLength, targetIndex, newDepth);
+            RETURN_IF_EXCEPTION(scope, { });
+        } else {
+            if (targetIndex >= maxSafeIntegerAsUInt64()) [[unlikely]] {
+                throwTypeError(globalObject, scope, "flatten array exceeds 2**52 - 1");
+                return { };
+            }
+            target->putDirectIndex(globalObject, targetIndex, element, 0, PutDirectIndexShouldThrow);
+            RETURN_IF_EXCEPTION(scope, { });
+            targetIndex++;
+        }
+    }
+
+    return targetIndex;
+}
+
+JSC_DEFINE_HOST_FUNCTION(arrayProtoFuncFlat, (JSGlobalObject* globalObject, CallFrame* callFrame))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSValue thisValue = callFrame->thisValue();
+
+    thisValue = thisValue.toThis(globalObject, ECMAMode::strict());
+    RETURN_IF_EXCEPTION(scope, { });
+    if (thisValue.isUndefinedOrNull()) [[unlikely]]
+        return throwVMTypeError(globalObject, scope, "Array.prototype.flat requires that |this| not be null or undefined"_s);
+
+    auto* thisObject = thisValue.toObject(globalObject);
+    RETURN_IF_EXCEPTION(scope, { });
+
+    uint64_t length = toLength(globalObject, thisObject);
+    RETURN_IF_EXCEPTION(scope, { });
+
+    uint64_t depthNum = 1;
+    if (callFrame->argumentCount()) {
+        JSValue depthValue = callFrame->uncheckedArgument(0);
+        if (!depthValue.isUndefined()) {
+            depthNum = [&]() -> uint64_t {
+                if (depthValue.isInt32()) [[likely]] {
+                    int32_t depthInt32 = depthValue.asInt32();
+                    if (depthInt32 < 0) [[unlikely]]
+                        return 0;
+                    return static_cast<uint64_t>(depthInt32);
+                }
+                double depthDouble = depthValue.toIntegerOrInfinity(globalObject);
+                RETURN_IF_EXCEPTION(scope, { });
+                if (depthDouble < 0) [[unlikely]]
+                    return 0;
+                if (std::isinf(depthDouble)) [[unlikely]]
+                    return std::numeric_limits<uint64_t>::max();
+                return static_cast<uint64_t>(depthDouble);
+            }();
+            RETURN_IF_EXCEPTION(scope, { });
+        }
+    }
+
+    if (isJSArray(thisObject) && arraySpeciesWatchpointIsValid(vm, thisObject)) [[likely]] {
+        JSArray* thisArray = jsCast<JSArray*>(thisObject);
+        auto fastResult = thisArray->fastFlat(globalObject, depthNum, length);
+        RETURN_IF_EXCEPTION(scope, { });
+        if (fastResult) [[likely]]
+            return JSValue::encode(fastResult);
+    }
+
+    std::pair<SpeciesConstructResult, JSObject*> speciesResult = speciesConstructArray(globalObject, thisObject, 0);
+    EXCEPTION_ASSERT(!!scope.exception() == (speciesResult.first == SpeciesConstructResult::Exception));
+    if (speciesResult.first == SpeciesConstructResult::Exception) [[unlikely]]
+        return { };
+
+    JSObject* result;
+    if (speciesResult.first == SpeciesConstructResult::CreatedObject) [[unlikely]]
+        result = speciesResult.second;
+    else {
+        result = constructEmptyArray(globalObject, nullptr);
+        RETURN_IF_EXCEPTION(scope, { });
+    }
+
+    scope.release();
+    flatIntoArray(globalObject, asObject(result), thisObject, length, /* start */ 0, depthNum);
     return JSValue::encode(result);
 }
 

Source/JavaScriptCore/runtime/JSArray.cpp
@@ -2212,6 +2212,233 @@ JSArray* tryCloneArrayFromFast(JSGlobalObject* globalObject, JSValue arrayValue)
 template JSArray* tryCloneArrayFromFast<ArrayFillMode::Undefined>(JSGlobalObject*, JSValue);
 template JSArray* tryCloneArrayFromFast<ArrayFillMode::Empty>(JSGlobalObject*, JSValue);
 
+static uint64_t calculateFlattenedLength(JSGlobalObject* globalObject, JSArray* sourceArray, uint64_t sourceLength, uint64_t depth)
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    if (!vm.isSafeToRecurseSoft()) [[unlikely]] {
+        throwStackOverflowError(globalObject, scope);
+        return std::numeric_limits<uint64_t>::max();
+    }
+
+    CheckedUint64 resultLength = 0;
+    auto lengthExceeded = [&]() -> bool {
+        return resultLength.hasOverflowed() || resultLength > maxSafeIntegerAsUInt64();
+    };
+
+    IndexingType sourceType = sourceArray->indexingType();
+    switch (sourceType) {
+    case ArrayWithInt32: {
+        auto* sourceBuffer = sourceArray->butterfly()->contiguous().data();
+        for (uint64_t i = 0; i < sourceLength; ++i) {
+            JSValue element = sourceBuffer[i].get();
+            if (!element) [[unlikely]]
+                continue;
+            resultLength++;
+            if (lengthExceeded()) [[unlikely]] {
+                throwTypeError(globalObject, scope, "flatten array exceeds 2**52 - 1");
+                return std::numeric_limits<uint64_t>::max();
+            }
+        }
+        break;
+    }
+    case ArrayWithContiguous: {
+        auto* sourceBuffer = sourceArray->butterfly()->contiguous().data();
+        for (uint64_t i = 0; i < sourceLength; ++i) {
+            JSValue element = sourceBuffer[i].get();
+            if (!element) [[unlikely]]
+                continue;
+            if (depth > 0 && isJSArray(element)) {
+                JSArray* elementArray = jsCast<JSArray*>(element);
+                uint64_t newDepth = (depth == std::numeric_limits<uint64_t>::max()) ? depth : depth - 1;
+                uint64_t flatLength = calculateFlattenedLength(globalObject, elementArray, elementArray->length(), newDepth);
+                RETURN_IF_EXCEPTION(scope, flatLength);
+                if (flatLength == std::numeric_limits<uint64_t>::max()) [[unlikely]]
+                    return flatLength;
+                resultLength += flatLength;
+                if (lengthExceeded()) [[unlikely]] {
+                    throwTypeError(globalObject, scope, "flatten array exceeds 2**52 - 1");
+                    return std::numeric_limits<uint64_t>::max();
+                }
+            } else {
+                if (element.isObject()) {
+                    auto elementObject = asObject(element);
+                    if (elementObject->isProxy()) [[unlikely]]
+                        return std::numeric_limits<uint64_t>::max();
+                }
+                resultLength++;
+                if (lengthExceeded()) [[unlikely]] {
+                    throwTypeError(globalObject, scope, "flatten array exceeds 2**52 - 1");
+                    return std::numeric_limits<uint64_t>::max();
+                }
+            }
+        }
+        break;
+    }
+    case ArrayWithDouble: {
+        auto* sourceBuffer = sourceArray->butterfly()->contiguousDouble().data();
+        for (uint64_t i = 0; i < sourceLength; ++i) {
+            double value = sourceBuffer[i];
+            if (std::isnan(value)) [[unlikely]]
+                continue;
+            resultLength++;
+            if (lengthExceeded()) [[unlikely]] {
+                throwTypeError(globalObject, scope, "flatten array exceeds 2**52 - 1");
+                return std::numeric_limits<uint64_t>::max();
+            }
+        }
+        break;
+    }
+    default:
+        return std::numeric_limits<uint64_t>::max();
+    }
+
+    return resultLength;
+}
+
+template<typename T>
+static uint64_t fastFlatIntoBuffer(JSGlobalObject* globalObject, T* resultBuffer, uint64_t& resultIndex, JSArray* sourceArray, uint64_t sourceLength, uint64_t depth, uint64_t vectorLength)
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    if (!vm.isSafeToRecurseSoft()) [[unlikely]] {
+        throwStackOverflowError(globalObject, scope);
+        return std::numeric_limits<uint64_t>::max();
+    }
+
+    IndexingType sourceType = sourceArray->indexingType();
+
+    switch (sourceType) {
+    case ArrayWithInt32: {
+        auto* sourceBuffer = sourceArray->butterfly()->contiguous().data();
+        for (uint64_t i = 0; i < sourceLength; ++i) {
+            if (resultIndex >= vectorLength) [[unlikely]]
+                return std::numeric_limits<uint64_t>::max();
+            JSValue element = sourceBuffer[i].get();
+            if (!element) [[unlikely]]
+                continue;
+            if constexpr (std::is_same_v<T, double>)
+                resultBuffer[resultIndex] = element.asNumber();
+            else
+                resultBuffer[resultIndex].setWithoutWriteBarrier(element);
+            ++resultIndex;
+        }
+        break;
+    }
+    case ArrayWithContiguous: {
+        auto* sourceBuffer = sourceArray->butterfly()->contiguous().data();
+        for (uint64_t i = 0; i < sourceLength; ++i) {
+            if (resultIndex >= vectorLength) [[unlikely]]
+                return std::numeric_limits<uint64_t>::max();
+            JSValue element = sourceBuffer[i].get();
+            if (!element) [[unlikely]]
+                continue;
+            if (depth > 0 && isJSArray(element)) {
+                JSArray* elementArray = jsCast<JSArray*>(element);
+                uint64_t newDepth = (depth == std::numeric_limits<uint64_t>::max()) ? depth : depth - 1;
+                resultIndex = fastFlatIntoBuffer(globalObject, resultBuffer, resultIndex, elementArray, elementArray->length(), newDepth, vectorLength);
+                RETURN_IF_EXCEPTION(scope, resultIndex);
+                if (resultIndex == std::numeric_limits<uint64_t>::max())
+                    return std::numeric_limits<uint64_t>::max();
+            } else {
+                if constexpr (std::is_same_v<T, double>)
+                    resultBuffer[resultIndex] = element.asNumber();
+                else
+                    resultBuffer[resultIndex].setWithoutWriteBarrier(element);
+                ++resultIndex;
+            }
+        }
+        break;
+    }
+    case ArrayWithDouble: {
+        auto* sourceBuffer = sourceArray->butterfly()->contiguousDouble().data();
+        for (uint64_t i = 0; i < sourceLength; ++i) {
+            if (resultIndex >= vectorLength) [[unlikely]]
+                return std::numeric_limits<uint64_t>::max();
+            double value = sourceBuffer[i];
+            if (std::isnan(value)) [[unlikely]]
+                continue;
+            if constexpr (std::is_same_v<T, double>)
+                resultBuffer[resultIndex] = value;
+            else
+                resultBuffer[resultIndex].setWithoutWriteBarrier(JSValue(value));
+            ++resultIndex;
+        }
+        break;
+    }
+    default: {
+        RELEASE_ASSERT_NOT_REACHED();
+        return std::numeric_limits<uint64_t>::max();
+    }
+    }
+    return resultIndex;
+}
+
+JSArray* JSArray::fastFlat(JSGlobalObject* globalObject, uint64_t depth, uint64_t length)
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_CATCH_SCOPE(vm);
+
+    IndexingType sourceType = this->indexingType();
+
+    switch (sourceType) {
+    case ArrayWithInt32:
+    case ArrayWithDouble:
+    case ArrayWithContiguous: {
+        if (length > this->butterfly()->vectorLength()) [[unlikely]]
+            return nullptr;
+
+        if (holesMustForwardToPrototype()) [[unlikely]]
+            return nullptr;
+
+        uint64_t flattenedLength = calculateFlattenedLength(globalObject, this, length, depth);
+        RETURN_IF_EXCEPTION(scope, nullptr);
+        if (flattenedLength == std::numeric_limits<uint64_t>::max()) [[unlikely]]
+            return nullptr;
+
+        Structure* resultStructure = globalObject->arrayStructureForIndexingTypeDuringAllocation(sourceType);
+
+        IndexingType indexingType = resultStructure->indexingType();
+        if (hasAnyArrayStorage(indexingType)) [[unlikely]]
+            return nullptr;
+        ASSERT(!globalObject->isHavingABadTime());
+
+        auto vectorLength = Butterfly::optimalContiguousVectorLength(resultStructure, flattenedLength);
+
+        void* memory = vm.auxiliarySpace().allocate(
+            vm,
+            Butterfly::totalSize(0, 0, true, vectorLength * sizeof(EncodedJSValue)),
+            nullptr, AllocationFailureMode::ReturnNull);
+        if (!memory) [[unlikely]]
+            return nullptr;
+
+        auto* butterfly = Butterfly::fromBase(memory, 0, 0);
+        butterfly->setVectorLength(vectorLength);
+        butterfly->setPublicLength(flattenedLength);
+
+        uint64_t resultIndex = 0;
+        if (indexingType == ArrayWithDouble) {
+            auto* resultBuffer = butterfly->contiguousDouble().data();
+            resultIndex = fastFlatIntoBuffer(globalObject, resultBuffer, resultIndex, this, length, depth, vectorLength);
+        } else {
+            auto* resultBuffer = butterfly->contiguous().data();
+            resultIndex = fastFlatIntoBuffer(globalObject, resultBuffer, resultIndex, this, length, depth, vectorLength);
+        }
+        RETURN_IF_EXCEPTION(scope, nullptr);
+        if (resultIndex == std::numeric_limits<uint64_t>::max())
+            return nullptr;
+
+        Butterfly::clearRange(indexingType, butterfly, resultIndex, vectorLength);
+        return createWithButterfly(vm, nullptr, resultStructure, butterfly);
+    }
+    default: {
+        return nullptr;
+    }
+    }
+}
+
 } // namespace JSC
 
 WTF_ALLOW_UNSAFE_BUFFER_USAGE_END

JSTests/microbenchmarks/array-prototype-flat-depth-1-double.js
@@ -1,0 +1,15 @@
+function shouldBe(a, b) {
+    if (a !== b)
+        throw new Error(`Expected ${b} but got ${a}`);
+}
+
+const array = new Array(1024);
+array.fill(99.99);
+array[512] = [1.1, 2.2, 3.3, 4.4, 5.5];
+
+let r;
+for (let i = 0; i < 1e5; i++) {
+    r = array.flat();
+}
+
+shouldBe(r.length, 1023 + 5);

JSTests/microbenchmarks/array-prototype-flat-depth-1-int32.js
@@ -1,0 +1,15 @@
+function shouldBe(a, b) {
+    if (a !== b)
+        throw new Error(`Expected ${b} but got ${a}`);
+}
+
+const array = new Array(1024);
+array.fill(99);
+array[512] = [1, 2, 3, 4, 5];
+
+let r;
+for (let i = 0; i < 1e5; i++) {
+    r = array.flat();
+}
+
+shouldBe(r.length, 1023 + 5);

JSTests/microbenchmarks/array-prototype-flat-depth-1-mixed.js
@@ -1,0 +1,21 @@
+function shouldBe(a, b) {
+    if (a !== b)
+        throw new Error(`Expected ${b} but got ${a}`);
+}
+
+const array = new Array(1024);
+for (let i = 0; i < array.length; i++) {
+    if (i % 3 === 0) array[i] = i;
+    else if (i % 3 === 1) array[i] = `str${i}`;
+    else array[i] = i * 1.1;
+}
+array[256] = [true, false, null, undefined, {}];
+array[512] = [42, "hello", 3.14, BigInt(100), Symbol.for("test")];
+array[768] = [[], {a: 1}, new Date(), /regex/, function() {}];
+
+let r;
+for (let i = 0; i < 1e5; i++) {
+    r = array.flat();
+}
+
+shouldBe(r.length, 1021 + 5 + 5 + 5);

JSTests/microbenchmarks/array-prototype-flat-depth-1-string.js
@@ -1,0 +1,15 @@
+function shouldBe(a, b) {
+    if (a !== b)
+        throw new Error(`Expected ${b} but got ${a}`);
+}
+
+const array = new Array(1024);
+array.fill("test");
+array[512] = ["hello", "world", "from", "webkit", "test"];
+
+let r;
+for (let i = 0; i < 1e5; i++) {
+    r = array.flat();
+}
+
+shouldBe(r.length, 1023 + 5);

JSTests/microbenchmarks/array-prototype-flat-depth-2.js
@@ -1,0 +1,17 @@
+function shouldBe(a, b) {
+    if (a !== b)
+        throw new Error(`Expected ${b} but got ${a}`);
+}
+
+const array = new Array(1024);
+array.fill(99);
+array[256] = [[1, 2], [3, 4], [5, 6]];
+array[512] = [[7, 8, 9], [10, 11, 12]];
+array[768] = [[13], [14, 15], [16, 17, 18]];
+
+let r;
+for (let i = 0; i < 1e5; i++) {
+    r = array.flat(2);
+}
+
+shouldBe(r.length, 1021 + 6 + 6 + 6);

JSTests/microbenchmarks/array-prototype-flat-depth-3.js
@@ -1,0 +1,17 @@
+function shouldBe(a, b) {
+    if (a !== b)
+        throw new Error(`Expected ${b} but got ${a}`);
+}
+
+const array = new Array(1024);
+array.fill(99);
+array[256] = [[[1, 2]], [[3], [4, 5]], [[[6]]]];
+array[512] = [[[7, 8, 9]], [[[10]], [[11, 12]]]];
+array[768] = [[[[13]]], [[[14, 15]], [[16]]], [[[17], [18]]]];
+
+let r;
+for (let i = 0; i < 1e5; i++) {
+    r = array.flat(3);
+}
+
+shouldBe(r.length, 1037);

commit 58336b9e2fcd6c2c769c4226ba8cd94474da9009

[JSC] Array#flat should skip IsArray at depth 0 and bail fast path for DerivedArrayType https://bugs.webkit.org/show_bug.cgi?id=309396
Reviewed by Yusuke Suzuki.

Two spec bugs were introduced in 546d47afe6:

1. flat(0) with a revoked Proxy element throws TypeError.
   FlattenIntoArray step 5.c.iv requires IsArray only when depth > 0.
   Short-circuit the check so isArray() is not called at depth 0.

2. fastFlat fails to flatten a cross-realm Array.prototype.
     isJSArray() checks ArrayType only, but Array.prototype has
     DerivedArrayType. Bail to the slow path for DerivedArrayType,
     matching the existing concat fast path check.

Tests: JSTests/stress/array-flat-cross-realm-array-prototype.js
       JSTests/stress/array-flat-depth-zero-revoked-proxy.js

* JSTests/stress/array-flat-cross-realm-array-prototype.js: Added.
(shouldBe):
* JSTests/stress/array-flat-depth-zero-revoked-proxy.js: Added.
(shouldBe):
(shouldBe.get let):
* Source/JavaScriptCore/runtime/ArrayPrototype.cpp:
(JSC::flatIntoArray):
* Source/JavaScriptCore/runtime/JSArray.cpp:
(JSC::calculateFlattenedLength):

Canonical link: https://commits.webkit.org/308887@main

JSTests/stress/array-flat-cross-realm-array-prototype.js
@@ -1,0 +1,67 @@
+function shouldBe(actual, expected) {
+    if (actual !== expected)
+        throw new Error(`bad value: ${String(actual)}`);
+}
+
+{
+    let other = createGlobalObject();
+    other.Array.prototype.length = 2;
+    other.Array.prototype[0] = "A";
+    other.Array.prototype[1] = "B";
+
+    let result = [other.Array.prototype].flat();
+    shouldBe(result.length, 2);
+    shouldBe(result[0], "A");
+    shouldBe(result[1], "B");
+}
+
+{
+    let other = createGlobalObject();
+    other.Array.prototype.length = 3;
+    other.Array.prototype[0] = 10;
+    other.Array.prototype[1] = 20;
+    other.Array.prototype[2] = 30;
+
+    let result = [1, other.Array.prototype, 2].flat();
+    shouldBe(result.length, 5);
+    shouldBe(result[0], 1);
+    shouldBe(result[1], 10);
+    shouldBe(result[2], 20);
+    shouldBe(result[3], 30);
+    shouldBe(result[4], 2);
+}
+
+{
+    let other = createGlobalObject();
+    other.Array.prototype.length = 2;
+    other.Array.prototype[0] = "x";
+    other.Array.prototype[1] = "y";
+
+    let result = [[other.Array.prototype]].flat(2);
+    shouldBe(result.length, 2);
+    shouldBe(result[0], "x");
+    shouldBe(result[1], "y");
+}
+
+{
+    let other = createGlobalObject();
+    let result = [other.Array.prototype, 42].flat();
+    shouldBe(result.length, 1);
+    shouldBe(result[0], 42);
+}
+
+{
+    let other = createGlobalObject();
+    other.Array.prototype.length = 2;
+    other.Array.prototype[0] = "A";
+    other.Array.prototype[1] = "B";
+
+    let result = [other.Array.prototype].flat(0);
+    shouldBe(result.length, 1);
+    shouldBe(result[0], other.Array.prototype);
+}
+
+{
+    let other = createGlobalObject();
+    shouldBe(Array.isArray(other.Array.prototype), true);
+}

JSTests/stress/array-flat-depth-zero-revoked-proxy.js
@@ -1,0 +1,63 @@
+function shouldBe(actual, expected) {
+    if (actual !== expected)
+        throw new Error(`bad value: ${String(actual)}`);
+}
+
+{
+    let { proxy, revoke } = Proxy.revocable([], {});
+    revoke();
+
+    let result = [proxy].flat(0);
+    shouldBe(result.length, 1);
+    shouldBe(result[0], proxy);
+}
+
+{
+    let { proxy, revoke } = Proxy.revocable([1, 2, 3], {});
+    revoke();
+
+    let result = [proxy].flat(-1);
+    shouldBe(result.length, 1);
+    shouldBe(result[0], proxy);
+}
+
+{
+    let lengthAccessed = false;
+    let proxy = new Proxy({}, {
+        get(t, k) {
+            if (k === "length")
+                lengthAccessed = true;
+            return Reflect.get(t, k);
+        }
+    });
+    let result = [proxy, 42].flat(0);
+    shouldBe(result.length, 2);
+    shouldBe(result[0], proxy);
+    shouldBe(result[1], 42);
+    shouldBe(lengthAccessed, false);
+}
+
+{
+    let { proxy, revoke } = Proxy.revocable([], {});
+    revoke();
+
+    let didThrow = false;
+    try {
+        [proxy].flat(1);
+    } catch (e) {
+        didThrow = true;
+        shouldBe(e instanceof TypeError, true);
+    }
+    shouldBe(didThrow, true);
+}
+
+{
+    let { proxy, revoke } = Proxy.revocable([], {});
+    revoke();
+
+    let arrayLike = { length: 2, 0: proxy, 1: "x" };
+    let result = Array.prototype.flat.call(arrayLike, 0);
+    shouldBe(result.length, 2);
+    shouldBe(result[0], proxy);
+    shouldBe(result[1], "x");
+}

Source/JavaScriptCore/runtime/ArrayPrototype.cpp
@@ -2130,9 +2130,9 @@ static uint64_t flatIntoArray(JSGlobalObject* globalObject, JSObject* target, JS
         if (!element)
             continue;
 
-        bool elementIsArray = isArray(globalObject, element);
+        bool elementIsArray = depth > 0 && isArray(globalObject, element);
         RETURN_IF_EXCEPTION(scope, { });
-        if (depth > 0 && elementIsArray) {
+        if (elementIsArray) {
             uint64_t newDepth = depth - 1;
             if (depth == std::numeric_limits<uint64_t>::max()) [[unlikely]]
                 newDepth = depth;

Source/JavaScriptCore/runtime/JSArray.cpp
@@ -2277,8 +2277,8 @@ static uint64_t calculateFlattenedLength(JSGlobalObject* globalObject, JSArray* 
                 }
             } else {
                 if (element.isObject()) {
-                    auto elementObject = asObject(element);
-                    if (elementObject->isProxy()) [[unlikely]]
+                    JSType type = asObject(element)->type();
+                    if (type == ProxyObjectType || type == DerivedArrayType) [[unlikely]]
                         return std::numeric_limits<uint64_t>::max();
                 }
                 resultLength++;