487ec443bd23024a0677533a9991380a55c3f64b
Aditya Keerthi <akeerthi@apple.com>
2026-03-10 10:03:51 -0700

    REGRESSION (260067@main): Tabbing position lost when button is disabled https://bugs.webkit.org/show_bug.cgi?id=309544 rdar://120676409
    Reviewed by Ryosuke Niwa and Abrar Rahman Protyasha.
    
    260067@main implemented the focus fixup rule to clear focus from an element
    that is no longer focusable (e.g. a button that became disabled). This change
    also had the effect of resetting the sequential focus navigation starting point.
    Consequently, the next Tab key press started focus navigation from the
    beginning of the document instead of advancing from where the disabled element
    was.
    
    Fix by preserving the sequential focus navigation starting point by resetting
    it to the now-unfocusable element after clearing focus, the same way the
    element-removal code path already does.
    
    * LayoutTests/imported/w3c/web-platform-tests/html/interaction/focus/sequential-focus-navigation-after-disabled-expected.txt: Added.
    * LayoutTests/imported/w3c/web-platform-tests/html/interaction/focus/sequential-focus-navigation-after-disabled.html: Added.
    * LayoutTests/platform/ios/TestExpectations:
    * Source/WebCore/page/Page.cpp:
    (WebCore::Page::updateRendering):
    
    Canonical link: https://commits.webkit.org/308991@main

--- Source/WebCore/page/Page.cpp
+++ Source/WebCore/page/Page.cpp
@@ -2310,8 +2310,10 @@ void Page::updateRendering()
 
     runProcessingStep(RenderingUpdateStep::FocusFixup, [&] (Document& document) {
         if (RefPtr focusedElement = document.focusedElement()) {
-            if (!focusedElement->isFocusable())
+            if (!focusedElement->isFocusable()) {
                 document.setFocusedElement(nullptr);
+                document.setFocusNavigationStartingNode(focusedElement.get());
+            }
         }
     });
 
