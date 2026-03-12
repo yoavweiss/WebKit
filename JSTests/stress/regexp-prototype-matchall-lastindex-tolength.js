yoavweiss/WebKit

commit 4b4cd078895ca415b8eeba467d60b56c42c60bcc

[JSC] Implement `RegExp#@@matchAll` in C++ https://bugs.webkit.org/show_bug.cgi?id=305096
Reviewed by Yusuke Suzuki.

This patch changes to implement `RegExp#@@matchAll` in C++.

                        TipOfTree                  Patched

matchall-bench       41.0811+-0.2157     ^     22.2720+-0.9164        ^ definitely 1.8445x faster

* JSTests/microbenchmarks/matchall-bench.js: Added.
* Source/JavaScriptCore/builtins/RegExpPrototype.js:
Remove the matchAll JavaScript builtin function.
(overriddenName.string_appeared_here.matchAll): Deleted.
* Source/JavaScriptCore/runtime/JSRegExpStringIterator.h:
* Source/JavaScriptCore/runtime/RegExpPrototype.cpp:
(JSC::RegExpPrototype::finishCreation): Register native matchAll function
instead of builtin.
(JSC::JSC_DEFINE_HOST_FUNCTION):
* Source/JavaScriptCore/runtime/RegExpPrototypeInlines.h:
(JSC::regExpMatchAllWathpointIsValid):

Canonical link: https://commits.webkit.org/305270@main

Source/JavaScriptCore/runtime/RegExpPrototype.cpp
@@ -28,8 +28,10 @@
 #include "JSCBuiltins.h"
 #include "JSCJSValue.h"
 #include "JSGlobalObject.h"
+#include "JSRegExpStringIterator.h"
 #include "JSStringInlines.h"
 #include "ObjectConstructor.h"
+#include "RegExpConstructor.h"
 #include "StringPrototypeInlines.h"
 #include "VMEntryScopeInlines.h"
 #include "RegExpObject.h"
@@ -60,6 +62,7 @@ static JSC_DECLARE_HOST_FUNCTION(regExpProtoGetterFlags);
 static JSC_DECLARE_HOST_FUNCTION(regExpProtoFuncTest);
 static JSC_DECLARE_HOST_FUNCTION(regExpProtoFuncSearch);
 static JSC_DECLARE_HOST_FUNCTION(regExpProtoFuncReplace);
+static JSC_DECLARE_HOST_FUNCTION(regExpProtoFuncMatchAll);
 
 const ClassInfo RegExpPrototype::s_info = { "Object"_s, &Base::s_info, nullptr, nullptr, CREATE_METHOD_TABLE(RegExpPrototype) };
 
@@ -86,7 +89,8 @@ void RegExpPrototype::finishCreation(VM& vm, JSGlobalObject* globalObject)
     JSC_NATIVE_GETTER_WITHOUT_TRANSITION(vm.propertyNames->source, regExpProtoGetterSource, PropertyAttribute::DontEnum | PropertyAttribute::Accessor);
     JSC_NATIVE_GETTER_WITHOUT_TRANSITION(vm.propertyNames->flags, regExpProtoGetterFlags, PropertyAttribute::DontEnum | PropertyAttribute::Accessor);
     JSC_BUILTIN_FUNCTION_WITHOUT_TRANSITION(vm.propertyNames->matchSymbol, regExpPrototypeMatchCodeGenerator, static_cast<unsigned>(PropertyAttribute::DontEnum));
-    JSC_BUILTIN_FUNCTION_WITHOUT_TRANSITION(vm.propertyNames->matchAllSymbol, regExpPrototypeMatchAllCodeGenerator, static_cast<unsigned>(PropertyAttribute::DontEnum));
+    JSFunction* matchAllFunction = JSFunction::create(vm, globalObject, 1, "[Symbol.matchAll]"_s, regExpProtoFuncMatchAll, ImplementationVisibility::Public);
+    putDirectWithoutTransition(vm, vm.propertyNames->matchAllSymbol, matchAllFunction, static_cast<unsigned>(PropertyAttribute::DontEnum));
     JSFunction* replaceFunction = JSFunction::create(vm, globalObject, 2, "[Symbol.replace]"_s, regExpProtoFuncReplace, ImplementationVisibility::Public);
     putDirectWithoutTransition(vm, vm.propertyNames->replaceSymbol, replaceFunction, static_cast<unsigned>(PropertyAttribute::DontEnum));
     JSFunction* searchFunction = JSFunction::create(vm, globalObject, 1, "[Symbol.search]"_s, regExpProtoFuncSearch, ImplementationVisibility::Public, RegExpSearchIntrinsic);
@@ -1184,6 +1188,103 @@ JSC_DEFINE_HOST_FUNCTION(regExpProtoFuncReplace, (JSGlobalObject* globalObject, 
         return { };
     }
     return JSValue::encode(jsString(vm, accumulatedResult.toString()));
+}
+
+// https://tc39.es/ecma262/#sec-regexp.prototype-%symbol.matchall%
+JSC_DEFINE_HOST_FUNCTION(regExpProtoFuncMatchAll, (JSGlobalObject* globalObject, CallFrame* callFrame))
+{
+    VM& vm = globalObject->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSValue thisValue = callFrame->thisValue();
+
+    if (!thisValue.isObject()) [[unlikely]]
+        return throwVMTypeError(globalObject, scope, "RegExp.prototype.@@matchAll requires that |this| be an Object"_s);
+    JSObject* thisObject = asObject(thisValue);
+
+    JSString* string = callFrame->argument(0).toString(globalObject);
+    RETURN_IF_EXCEPTION(scope, { });
+
+    auto* regExpObject = jsDynamicCast<RegExpObject*>(thisObject);
+    if (regExpObject && regExpMatchAllWathpointIsValid(regExpObject)) [[likely]] {
+        RegExp* regExp = regExpObject->regExp();
+
+        bool global = regExp->global();
+        bool fullUnicode = regExp->eitherUnicode();
+
+        double lastIndexDouble = regExpObject->getLastIndex().asNumber();
+        size_t lastIndex = (lastIndexDouble >= 0 && lastIndexDouble <= maxSafeInteger()) ? static_cast<size_t>(lastIndexDouble) : 0;
+
+        Structure* structure = globalObject->regExpStructure();
+        RegExpObject* matcher = RegExpObject::create(vm, structure, regExp);
+        matcher->setLastIndex(globalObject, lastIndex);
+        RETURN_IF_EXCEPTION(scope, { });
+
+        auto* iterator = JSRegExpStringIterator::createWithInitialValues(vm, globalObject->regExpStringIteratorStructure());
+        iterator->setRegExp(vm, matcher);
+        iterator->setString(vm, string);
+        iterator->setGlobal(vm, jsBoolean(global));
+        iterator->setFullUnicode(vm, jsBoolean(fullUnicode));
+
+        return JSValue::encode(iterator);
+    }
+
+    JSValue constructorValue = thisObject->get(globalObject, vm.propertyNames->constructor);
+    RETURN_IF_EXCEPTION(scope, { });
+
+    JSObject* constructor;
+    if (constructorValue.isUndefined())
+        constructor = globalObject->regExpConstructor();
+    else {
+        if (!constructorValue.isObject()) [[unlikely]]
+            return throwVMTypeError(globalObject, scope, "|this|.constructor is not an Object or undefined"_s);
+
+        JSValue speciesValue = asObject(constructorValue)->get(globalObject, vm.propertyNames->speciesSymbol);
+        RETURN_IF_EXCEPTION(scope, { });
+
+        if (speciesValue.isUndefinedOrNull())
+            constructor = globalObject->regExpConstructor();
+        else {
+            if (!speciesValue.isConstructor()) [[unlikely]]
+                return throwVMTypeError(globalObject, scope, "|this|.constructor[Symbol.species] is not a constructor"_s);
+            constructor = asObject(speciesValue);
+        }
+    }
+
+    JSValue flagsValue = thisObject->get(globalObject, vm.propertyNames->flags);
+    RETURN_IF_EXCEPTION(scope, { });
+    String flags = flagsValue.toWTFString(globalObject);
+    RETURN_IF_EXCEPTION(scope, { });
+
+    MarkedArgumentBuffer constructorArgs;
+    constructorArgs.append(thisValue);
+    constructorArgs.append(jsString(vm, flags));
+    ASSERT(!constructorArgs.hasOverflowed());
+
+    // FIXME: Introduce getConstructDataInline
+    auto constructData = JSC::getConstructData(constructor);
+    JSObject* matcher = construct(globalObject, constructor, constructData, constructorArgs);
+    RETURN_IF_EXCEPTION(scope, { });
+
+    JSValue lastIndexValue = thisObject->get(globalObject, vm.propertyNames->lastIndex);
+    RETURN_IF_EXCEPTION(scope, { });
+    uint64_t lastIndex = lastIndexValue.toLength(globalObject);
+    RETURN_IF_EXCEPTION(scope, { });
+
+    PutPropertySlot slot(matcher, true);
+    matcher->methodTable()->put(matcher, globalObject, vm.propertyNames->lastIndex, jsNumber(lastIndex), slot);
+    RETURN_IF_EXCEPTION(scope, { });
+
+    bool global = flags.contains('g');
+    bool fullUnicode = flags.contains('u') || flags.contains('v');
+    auto* regExpStringIterator = JSRegExpStringIterator::createWithInitialValues(vm, globalObject->regExpStringIteratorStructure());
+
+    regExpStringIterator->setRegExp(vm, matcher);
+    regExpStringIterator->setString(vm, string);
+    regExpStringIterator->setGlobal(vm, jsBoolean(global));
+    regExpStringIterator->setFullUnicode(vm, jsBoolean(fullUnicode));
+
+    return JSValue::encode(regExpStringIterator);
 }
 
 } // namespace JSC

JSTests/microbenchmarks/matchall-bench.js
@@ -1,0 +1,6 @@
+const re1 = /test/g;
+const str1 = "test1test2test3";
+for (let i = 0; i < 1e5; i++) {
+    const iter = str1.matchAll(re1);
+    iter.next();
+}

Source/JavaScriptCore/builtins/RegExpPrototype.js
@@ -165,28 +165,6 @@ function match(strArg)
     return @matchSlow(this, str);
 }
 
-@overriddenName="[Symbol.matchAll]"
-function matchAll(strArg)
-{
-    "use strict";
-
-    var regExp = this;
-    if (!@isObject(regExp))
-        @throwTypeError("RegExp.prototype.@@matchAll requires |this| to be an Object");
-
-    var string = @toString(strArg);
-    var Matcher = @speciesConstructor(regExp, @RegExp);
-
-    var flags = @toString(regExp.flags);
-    var matcher = new Matcher(regExp, flags);
-    matcher.lastIndex = @toLength(regExp.lastIndex);
-
-    var global = @stringIncludesInternal.@call(flags, "g");
-    var fullUnicode = @stringIncludesInternal.@call(flags, "u") || @stringIncludesInternal.@call(flags, "v");
-
-    return @regExpStringIteratorCreate(matcher, string, global, fullUnicode);
-}
-
 @linkTimeConstant
 function hasObservableSideEffectsForRegExpSplit(regexp)
 {

Source/JavaScriptCore/runtime/JSRegExpStringIterator.h
@@ -42,6 +42,7 @@ public:
     enum class Field : uint8_t {
         RegExp = 0,
         String,
+        // FIXME: Global and FullUnicode should be one bitset field
         Global,
         FullUnicode,
         Done,

Source/JavaScriptCore/runtime/RegExpPrototypeInlines.h
@@ -21,6 +21,7 @@
 #pragma once
 
 #include "JSGlobalObject.h"
+#include "RegExpObject.h"
 #include "RegExpPrototype.h"
 
 namespace JSC {
@@ -41,6 +42,30 @@ ALWAYS_INLINE bool regExpExecWatchpointIsValid(VM& vm, JSObject* thisObject)
         return true;
 
     return thisObject->getDirectOffset(vm, vm.propertyNames->exec) == invalidOffset;
+}
+
+ALWAYS_INLINE bool regExpMatchAllWathpointIsValid(RegExpObject* regExpObject)
+{
+    JSGlobalObject* globalObject = regExpObject->globalObject();
+    RegExpPrototype* regExpPrototype = globalObject->regExpPrototype();
+
+    if (regExpPrototype != regExpObject->getPrototypeDirect())
+        return false;
+
+    if (globalObject->regExpPrimordialPropertiesWatchpointSet().state() != IsWatched)
+        return false;
+
+    if (regExpObject->hasCustomProperties())
+        return false;
+
+    if (!regExpObject->lastIndexIsWritable())
+        return false;
+
+    JSValue lastIndex = regExpObject->getLastIndex();
+    if (!lastIndex.isNumber())
+        return false;
+
+    return true;
 }
 
 inline Structure* RegExpPrototype::createStructure(VM& vm, JSGlobalObject* globalObject, JSValue prototype)

commit 8cfdd1d58057ff6fa49d2918b1d853368fdc7c25

[JSC] `RegExp#@@matchAll` fast path should clamp lastIndex > 2^53-1 to 2^53-1 https://bugs.webkit.org/show_bug.cgi?id=309470
Reviewed by Yusuke Suzuki.

The fast path in regExpProtoFuncMatchAll incorrectly treated lastIndex values
exceeding Number.MAX_SAFE_INTEGER as 0. Per spec, ToLength clamps such values
to 2^53-1[1][2]. Regression from 4b4cd07889.

[1]: https://tc39.es/ecma262/#sec-string.prototype.matchall
[2]: https://tc39.es/ecma262/#sec-tolength

Test: JSTests/stress/regexp-prototype-matchall-lastindex-tolength.js

* JSTests/stress/regexp-prototype-matchall-lastindex-tolength.js: Added.
(shouldBe):
(testSlowPath.MyRegExp):
(testSlowPath):
* Source/JavaScriptCore/runtime/RegExpPrototype.cpp:
(JSC::JSC_DEFINE_HOST_FUNCTION):

Canonical link: https://commits.webkit.org/309129@main

JSTests/stress/regexp-prototype-matchall-lastindex-tolength.js
@@ -1,0 +1,93 @@
+function shouldBe(actual, expected, msg) {
+    if (actual !== expected)
+        throw new Error(`${msg}: expected ${expected} but got ${actual}`);
+}
+
+function testFastPath() {
+    {
+        let r = /a/g;
+        r.lastIndex = Number.MAX_SAFE_INTEGER + 1;
+        let results = [...r[Symbol.matchAll]("aaa")];
+        shouldBe(results.length, 0, "fast: MAX_SAFE_INTEGER + 1");
+    }
+
+    {
+        let r = /a/g;
+        r.lastIndex = Infinity;
+        let results = [...r[Symbol.matchAll]("aaa")];
+        shouldBe(results.length, 0, "fast: Infinity");
+    }
+
+    {
+        let r = /a/g;
+        r.lastIndex = 1e100;
+        let results = [...r[Symbol.matchAll]("aaa")];
+        shouldBe(results.length, 0, "fast: 1e100");
+    }
+
+    {
+        let r = /a/g;
+        r.lastIndex = 2.5;
+        let results = [...r[Symbol.matchAll]("aaa")];
+        shouldBe(results.length, 1, "fast: 2.5 -> 2");
+        shouldBe(results[0].index, 2, "fast: 2.5 index");
+    }
+
+    {
+        let r = /a/g;
+        r.lastIndex = NaN;
+        let results = [...r[Symbol.matchAll]("aaa")];
+        shouldBe(results.length, 3, "fast: NaN -> 0");
+    }
+
+    {
+        let r = /a/g;
+        r.lastIndex = -5;
+        let results = [...r[Symbol.matchAll]("aaa")];
+        shouldBe(results.length, 3, "fast: -5 -> 0");
+    }
+
+    {
+        let r = /a/g;
+        r.lastIndex = -Infinity;
+        let results = [...r[Symbol.matchAll]("aaa")];
+        shouldBe(results.length, 3, "fast: -Infinity -> 0");
+    }
+
+    {
+        let r = /a/g;
+        r.lastIndex = Number.MAX_SAFE_INTEGER;
+        let results = [...r[Symbol.matchAll]("aaa")];
+        shouldBe(results.length, 0, "fast: MAX_SAFE_INTEGER");
+    }
+}
+
+function testSlowPath() {
+    class MyRegExp extends RegExp {}
+
+    {
+        let r = new MyRegExp("a", "g");
+        r.lastIndex = Number.MAX_SAFE_INTEGER + 1;
+        let results = [...r[Symbol.matchAll]("aaa")];
+        shouldBe(results.length, 0, "slow: MAX_SAFE_INTEGER + 1");
+    }
+
+    {
+        let r = new MyRegExp("a", "g");
+        r.lastIndex = Infinity;
+        let results = [...r[Symbol.matchAll]("aaa")];
+        shouldBe(results.length, 0, "slow: Infinity");
+    }
+
+    {
+        let r = new MyRegExp("a", "g");
+        r.lastIndex = 2.5;
+        let results = [...r[Symbol.matchAll]("aaa")];
+        shouldBe(results.length, 1, "slow: 2.5 -> 2");
+    }
+}
+
+for (let i = 0; i < testLoopCount; i++) {
+    testFastPath();
+    testSlowPath();
+}

Source/JavaScriptCore/runtime/RegExpPrototype.cpp
@@ -1216,7 +1216,7 @@ JSC_DEFINE_HOST_FUNCTION(regExpProtoFuncMatchAll, (JSGlobalObject* globalObject,
         bool fullUnicode = regExp->eitherUnicode();
 
         double lastIndexDouble = regExpObject->getLastIndex().asNumber();
-        size_t lastIndex = (lastIndexDouble >= 0 && lastIndexDouble <= maxSafeInteger()) ? static_cast<size_t>(lastIndexDouble) : 0;
+        size_t lastIndex = lastIndexDouble > 0 ? static_cast<size_t>(std::min(lastIndexDouble, maxSafeInteger())) : 0;
 
         Structure* structure = globalObject->regExpStructure();
         RegExpObject* matcher = RegExpObject::create(vm, structure, regExp);