1cd968cdd081a251c7266f03c80670176eab1119
Abrar Rahman Protyasha <a_protyasha@apple.com>
2026-05-23 18:45:01 -0700

    Apply swift-format over SwiftBrowser/BrowserViewModel.swift https://bugs.webkit.org/show_bug.cgi?id=315401 rdar://177752918
    Reviewed by Richard Robinson.
    
    Just a mechanical application of `swift format --in-place`.
    
    * Tools/SwiftBrowser/Source/ViewModel/BrowserViewModel.swift:
    (BrowserViewModel.didExportPDF(_:any:)):
    (BrowserViewModel.didImportFiles(_:any:)):
    (BrowserViewModel.decideSensorAuthorization(_:frame:origin:)): Deleted.
    
    Canonical link: https://commits.webkit.org/313798@main

--- Tools/SwiftBrowser/Source/ViewModel/BrowserViewModel.swift
+++ Tools/SwiftBrowser/Source/ViewModel/BrowserViewModel.swift
@@ -49,9 +49,25 @@ struct OpenRequest: Equatable {
 final class BrowserViewModel {
     private static let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: String(describing: BrowserViewModel.self))
 
-    private static func decideSensorAuthorization(permission: WebPage.DeviceSensorAuthorization.Permission, frame: WebPage.FrameInfo, origin: WKSecurityOrigin) async -> WKPermissionDecision {
-        let mediaCaptureAuthorization = WKPermissionDecision(rawValue: UserDefaults.standard.integer(forKey: AppStorageKeys.mediaCaptureAuthorization))!
-        let orientationAndMotionAuthorization = WKPermissionDecision(rawValue: UserDefaults.standard.integer(forKey: AppStorageKeys.orientationAndMotionAuthorization))!
+    private static func decideSensorAuthorization(
+        permission: WebPage.DeviceSensorAuthorization.Permission,
+        frame: WebPage.FrameInfo,
+        origin: WKSecurityOrigin
+    ) async -> WKPermissionDecision {
+        guard
+            let mediaCaptureAuthorization = WKPermissionDecision(
+                rawValue: UserDefaults.standard.integer(forKey: AppStorageKeys.mediaCaptureAuthorization)
+            )
+        else {
+            preconditionFailure()
+        }
+        guard
+            let orientationAndMotionAuthorization = WKPermissionDecision(
+                rawValue: UserDefaults.standard.integer(forKey: AppStorageKeys.orientationAndMotionAuthorization)
+            )
+        else {
+            preconditionFailure()
+        }
 
         return switch permission {
         case .deviceOrientationAndMotion: orientationAndMotionAuthorization
@@ -63,7 +79,9 @@ final class BrowserViewModel {
 
     init() {
         var configuration = WebPage.Configuration()
-        configuration.deviceSensorAuthorization = WebPage.DeviceSensorAuthorization(decisionHandler: Self.decideSensorAuthorization(permission:frame:origin:))
+        configuration.deviceSensorAuthorization = WebPage.DeviceSensorAuthorization(
+            decisionHandler: Self.decideSensorAuthorization(permission:frame:origin:)
+        )
 
         self.page = WebPage(configuration: configuration, navigationDecider: self.navigationDecider, dialogPresenter: self.dialogPresenter)
         self.page.isInspectable = true
@@ -162,26 +180,28 @@ final class BrowserViewModel {
 
     func didExportPDF(result: Result<URL, any Error>) {
         switch result {
-        case let .success(url):
+        case .success(let url):
             Self.logger.info("Exported PDF to \(url)")
 
-        case let .failure(error):
+        case .failure(let error):
             Self.logger.error("Failed to export PDF: \(error)")
         }
     }
 
     func didImportFiles(result: Result<[URL], any Error>) {
-        precondition(currentFilePicker != nil)
-
-        switch result {
-        case let .success(urls):
-            currentFilePicker!.completion(.selected(urls))
-
-        case .failure:
-            currentFilePicker!.completion(.cancel)
+        guard let currentFilePicker else {
+            preconditionFailure()
         }
 
-        currentFilePicker = nil
+        switch result {
+        case .success(let urls):
+            currentFilePicker.completion(.selected(urls))
+
+        case .failure:
+            currentFilePicker.completion(.cancel)
+        }
+
+        self.currentFilePicker = nil
     }
 
     func setCameraCaptureState(_ state: WKMediaCaptureState) {
