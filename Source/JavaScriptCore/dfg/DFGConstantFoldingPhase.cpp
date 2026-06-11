0f5acb567be089fa9dab007127f06c7bd1fdd6e5
Shu-yu Guo <syg@apple.com>
2026-06-10 21:58:05 -0700

    [JSC] Promise.resolve should not folded to identity on Promise subclasses https://bugs.webkit.org/show_bug.cgi?id=309612 rdar://172195606
    Reviewed by Yijia Huang.
    
    Per spec, Promise.resolve(x) acts as identity on x iff x.constructor is Promise
    (more pedantically, if it is the same object as the `this` value that the
    resolve function was called with, which, when called on the global Promise, is
    Promise). DFG currently incorrectly folds all builtin Promise subclasses to
    identity as well.
    
    This PR fixes the constant folding and abstract interpreter by restricting the
    folding to arguments that have the builtin promise structure, i.e. is not a
    subclass.
    
    Test: JSTests/stress/promise-resolve-subclass-not-identity.js
    * JSTests/stress/promise-resolve-subclass-not-identity.js: Added.
    (MyPromise):
    * Source/JavaScriptCore/dfg/DFGAbstractInterpreterInlines.h:
    (JSC::DFG::AbstractInterpreter<AbstractStateType>::executeEffects):
    * Source/JavaScriptCore/dfg/DFGConstantFoldingPhase.cpp:
    (JSC::DFG::ConstantFoldingPhase::foldConstants):
    
    Originally-landed-as: 305413.447@rapid/safari-7624.2.5.110-branch (8fb39d0d2bd3). rdar://176065429
    Canonical link: https://commits.webkit.org/314995@main

--- Source/JavaScriptCore/dfg/DFGConstantFoldingPhase.cpp
+++ Source/JavaScriptCore/dfg/DFGConstantFoldingPhase.cpp
@@ -2021,11 +2021,15 @@ private:
 
                         if (argument.isType(SpecPromiseObject)) {
                             if (m_graph.isWatchingPromiseSpeciesWatchpoint(node)) {
-                                m_interpreter.execute(indexInBlock); // Push CFA over this node after we get the state before.
-                                alreadyHandled = true; // Don't allow the default constant folder to do things to this.
-                                node->convertToIdentityOn(node->child2().node());
-                                changed = true;
-                                break;
+                                if (auto structure = argument.m_structure.onlyStructure()) {
+                                    if (structure.get() == globalObject->promiseStructure()) {
+                                        m_interpreter.execute(indexInBlock); // Push CFA over this node after we get the state before.
+                                        alreadyHandled = true; // Don't allow the default constant folder to do things to this.
+                                        node->convertToIdentityOn(node->child2().node());
+                                        changed = true;
+                                        break;
+                                    }
+                                }
                             }
                         }
 
