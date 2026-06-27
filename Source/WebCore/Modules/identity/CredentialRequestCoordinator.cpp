6d96204ed552eafdd74565a8813450ab74ecc0c4
Marcos Caceres <marcosc@apple.com>
2026-06-27 00:32:01 -0700

    Digital Credentials: prepare credential requests rejects with wrong error code https://bugs.webkit.org/show_bug.cgi?id=311790 rdar://174895437
    Reviewed by Anne van Kesteren.
    
    Reject with NotAllowedError (not InvalidStateError) when a credential request is already in progress, per spec 'prepare credential requests' step 2.
    
    * Source/WebCore/Modules/identity/CredentialRequestCoordinator.cpp:
    (WebCore::CredentialRequestCoordinator::prepareCredentialRequests):
    
    Canonical link: https://commits.webkit.org/315957@main

--- Source/WebCore/Modules/identity/CredentialRequestCoordinator.cpp
+++ Source/WebCore/Modules/identity/CredentialRequestCoordinator.cpp
@@ -129,7 +129,7 @@ CredentialPromise* CredentialRequestCoordinator::currentPromise()
 void CredentialRequestCoordinator::prepareCredentialRequests(const Document& document, CredentialPromise&& promise, Vector<UnvalidatedDigitalCredentialRequest>&& unvalidatedRequests, RefPtr<AbortSignal> signal)
 {
     if (m_interactionState != InteractionState::Idle)
-        return promise.reject(ExceptionCode::InvalidStateError, "A credential request is already in progress."_s);
+        return promise.reject(ExceptionCode::NotAllowedError, "A credential request is already in progress."_s);
 
     ASSERT(!m_currentPromise);
     setInteractionState(InteractionState::Requesting);
