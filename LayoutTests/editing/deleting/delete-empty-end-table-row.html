yoavweiss/WebKit

commit f20d920da025ef028f9cc686e32a38e291a03633

Reduce incorrect usage of protectedX() in dom and editing https://bugs.webkit.org/show_bug.cgi?id=293959 rdar://152502777
Reviewed by Chris Dumez.

Apply https://github.com/WebKit/WebKit/wiki/Safer-CPP-Guidelines more
strictly to existing code so bad patterns have less of a chance of
getting copied.

This is not exhaustive and will be followed by more patches.

Canonical link: https://commits.webkit.org/295798@main

Source/WebCore/editing/DeleteSelectionCommand.cpp
@@ -425,7 +425,7 @@ void DeleteSelectionCommand::saveTypingStyleState()
         return;
     }
 
-    auto startNode = m_selectionToDelete.start().protectedDeprecatedNode();
+    RefPtr startNode = m_selectionToDelete.start().deprecatedNode();
     if (!startNode->isTextNode() && !startNode->hasTagName(imgTag) && !startNode->hasTagName(brTag))
         return;
 
@@ -628,7 +628,7 @@ void DeleteSelectionCommand::handleGeneralDelete()
         return;
 
     int startOffset = m_upstreamStart.deprecatedEditingOffset();
-    auto startNode = m_upstreamStart.protectedDeprecatedNode();
+    RefPtr startNode = m_upstreamStart.deprecatedNode();
 
     makeStylingElementsDirectChildrenOfEditableRootToPreventStyleLoss();
 
@@ -739,7 +739,7 @@ void DeleteSelectionCommand::handleGeneralDelete()
                 } else if (!(startNodeWasDescendantOfEndNode && !m_upstreamStart.anchorNode()->isConnected())) {
                     unsigned offset = 0;
                     if (m_upstreamStart.deprecatedNode()->isDescendantOf(m_downstreamEnd.deprecatedNode())) {
-                        auto n = m_upstreamStart.protectedDeprecatedNode();
+                        RefPtr n = m_upstreamStart.deprecatedNode();
                         while (n && n->parentNode() != m_downstreamEnd.deprecatedNode())
                             n = n->parentNode();
                         if (n)
@@ -829,7 +829,7 @@ void DeleteSelectionCommand::mergeParagraphs()
     // FIXME: Consider RTL.
     if (!m_startsAtEmptyLine && isStartOfParagraph(mergeDestination) && startOfParagraphToMove.absoluteCaretBounds().x() > mergeDestination.absoluteCaretBounds().x()) {
         if (mergeDestination.deepEquivalent().downstream().deprecatedNode()->hasTagName(brTag)) {
-            auto nodeToRemove = mergeDestination.deepEquivalent().downstream().protectedDeprecatedNode();
+            RefPtr nodeToRemove = mergeDestination.deepEquivalent().downstream().deprecatedNode();
             removeNodeAndPruneAncestors(*nodeToRemove);
             m_endingPosition = startOfParagraphToMove.deepEquivalent();
             return;
@@ -891,7 +891,7 @@ void DeleteSelectionCommand::removePreviouslySelectedEmptyTableRows()
         }
     }
 
-    auto endTableRow = protectedEndTableRow();
+    RefPtr endTableRow = m_startTableRow;
     if (endTableRow && endTableRow->isConnected() && endTableRow != m_startTableRow) {
         if (isTableRowEmpty(*endTableRow)) {
             // Don't remove m_endTableRow if it's where we're putting the ending selection.

Source/WebCore/SaferCPPExpectations/UncheckedCallArgsCheckerExpectations
@@ -212,7 +212,6 @@ dom/ViewTransition.cpp
 dom/WindowEventLoop.cpp
 dom/mac/ImageControlsMac.cpp
 editing/AlternativeTextController.cpp
-editing/AppendNodeCommand.cpp
 editing/ApplyBlockElementCommand.cpp
 editing/ApplyStyleCommand.cpp
 editing/BreakBlockquoteCommand.cpp

Source/WebCore/SaferCPPExpectations/UncountedCallArgsCheckerExpectations
@@ -544,7 +544,6 @@ dom/VisitedLinkState.cpp
 dom/WindowOrWorkerGlobalScopeTrustedTypes.cpp
 dom/mac/ImageControlsMac.cpp
 editing/AlternativeTextController.cpp
-editing/AppendNodeCommand.cpp
 editing/ApplyBlockElementCommand.cpp
 editing/ApplyStyleCommand.cpp
 editing/BreakBlockquoteCommand.cpp

Source/WebCore/dom/DocumentMarkerController.cpp
@@ -3,7 +3,7 @@
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  *           (C) 2001 Dirk Mueller (mueller@kde.org)
  *           (C) 2006 Alexey Proskuryakov (ap@webkit.org)
- * Copyright (C) 2004-2020 Apple Inc. All rights reserved.
+ * Copyright (C) 2004-2025 Apple Inc. All rights reserved.
  * Copyright (C) 2008, 2009 Torch Mobile Inc. All rights reserved. (http://www.torchmobile.com/)
  * Copyright (C) Research In Motion Limited 2010. All rights reserved.
  *
@@ -114,8 +114,8 @@ void DocumentMarkerController::addTransparentContentMarker(const SimpleRange& ra
         //
         // Otherwise, this information would be lost if the original range is collapsed, so this prevents that from happening.
 
-        DocumentMarker::TransparentContentData markerData { { range.protectedStartContainer().ptr() }, uuid };
-        Ref node = range.protectedStartContainer();
+        Ref node = range.startContainer();
+        DocumentMarker::TransparentContentData markerData { { node.ptr() }, uuid };
         addMarker(node.get(), { DocumentMarkerType::TransparentContent, { range.startOffset(), range.endOffset() }, WTFMove(markerData) });
 
         return;
@@ -543,7 +543,7 @@ void DocumentMarkerController::applyToCollapsedRangeMarker(const SimpleRange& ra
     if (!range.collapsed())
         return;
 
-    Ref node = range.protectedStartContainer();
+    Ref node = range.startContainer();
     if (auto list = m_markers.get(node.ptr())) {
         auto offsetRange = characterDataOffsetRange(range, node.get());
         for (auto& marker : *list) {

Source/WebCore/dom/Element.cpp
@@ -4,7 +4,7 @@
  *           (C) 2001 Peter Kelly (pmk@post.com)
  *           (C) 2001 Dirk Mueller (mueller@kde.org)
  *           (C) 2007 David Smith (catfish.man@gmail.com)
- * Copyright (C) 2004-2024 Apple Inc. All rights reserved.
+ * Copyright (C) 2004-2025 Apple Inc. All rights reserved.
  *           (C) 2007 Eric Seidel (eric@webkit.org)
  *
  * This library is free software; you can redistribute it and/or
@@ -237,7 +237,7 @@ static Attr* findAttrNodeInList(Vector<RefPtr<Attr>>& attrNodeList, const Qualif
 static bool shouldAutofocus(const Element& element)
 {
     Ref document = element.document();
-    RefPtr page = document->protectedPage();
+    RefPtr page = document->page();
     if (!page || page->autofocusProcessed())
         return false;
 

Source/WebCore/dom/Position.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2022 Apple Inc. All rights reserved.
+ * Copyright (C) 2004-2025 Apple Inc. All rights reserved.
  * Copyright (C) 2015-2018 Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -351,7 +351,7 @@ RefPtr<Element> Position::anchorElementAncestor() const
 
 Position Position::previous(PositionMoveType moveType) const
 {
-    auto node = protectedDeprecatedNode();
+    RefPtr node = deprecatedNode();
     if (!node)
         return *this;
 
@@ -408,7 +408,7 @@ Position Position::next(PositionMoveType moveType) const
 {
     ASSERT(moveType != BackwardDeletion);
 
-    auto node = protectedDeprecatedNode();
+    RefPtr node = deprecatedNode();
     if (!node)
         return *this;
 
@@ -1203,7 +1203,7 @@ InlineBoxAndOffset Position::inlineBoxAndOffset(Affinity affinity, TextDirection
 {
     auto caretOffset = static_cast<unsigned>(deprecatedEditingOffset());
 
-    auto node = protectedDeprecatedNode();
+    RefPtr node = deprecatedNode();
     if (!node)
         return { { }, caretOffset };
     auto renderer = node->renderer();

Source/WebCore/dom/PositionIterator.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2007-2025 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -50,7 +50,7 @@ PositionIterator::PositionIterator(const Position& pos)
 
 PositionIterator::operator Position() const
 {
-    auto anchorNode = protectedNode();
+    RefPtr anchorNode = node();
     if (m_nodeAfterPositionInAnchor) {
         ASSERT(m_nodeAfterPositionInAnchor->parentNode() == anchorNode.get());
         // FIXME: This check is inadaquete because any ancestor could be ignored by editing
@@ -77,7 +77,7 @@ void PositionIterator::increment()
         return;
     }
 
-    auto anchorNode = protectedNode();
+    RefPtr anchorNode = node();
     if (anchorNode->renderer() && !anchorNode->hasChildNodes() && m_offsetInAnchor < lastOffsetForEditing(*anchorNode))
         m_offsetInAnchor = Position::uncheckedNextOffset(anchorNode.get(), m_offsetInAnchor);
     else {
@@ -130,7 +130,7 @@ bool PositionIterator::atStart() const
 
 bool PositionIterator::atEnd() const
 {
-    auto anchorNode = protectedNode();
+    RefPtr anchorNode = node();
     if (!anchorNode)
         return true;
     if (m_nodeAfterPositionInAnchor)
@@ -149,7 +149,7 @@ bool PositionIterator::atStartOfNode() const
 
 bool PositionIterator::atEndOfNode() const
 {
-    auto anchorNode = protectedNode();
+    RefPtr anchorNode = node();
     if (!anchorNode)
         return true;
     if (m_nodeAfterPositionInAnchor)
@@ -160,7 +160,7 @@ bool PositionIterator::atEndOfNode() const
 // This function should be kept in sync with Position::isCandidate().
 bool PositionIterator::isCandidate() const
 {
-    auto anchorNode = protectedNode();
+    RefPtr anchorNode = node();
     if (!anchorNode)
         return false;
 

Source/WebCore/dom/ScriptElement.cpp
@@ -2,7 +2,7 @@
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  *           (C) 2001 Dirk Mueller (mueller@kde.org)
- * Copyright (C) 2003-2023 Apple Inc. All rights reserved.
+ * Copyright (C) 2003-2025 Apple Inc. All rights reserved.
  * Copyright (C) 2008 Nikolas Zimmermann <zimmermann@kde.org>
  *
  * This library is free software; you can redistribute it and/or
@@ -326,7 +326,7 @@ void ScriptElement::updateTaintedOriginFromSourceURL()
 
 bool ScriptElement::requestClassicScript(const String& sourceURL)
 {
-    auto element = protectedElement();
+    Ref element = this->element();
     ASSERT(element->isConnected());
     ASSERT(!m_loadableScript);
     Ref document = element->document();

commit 105fc9b52b9ae74666096a6289b9717abde65177

DeleteSelectionCommand leaves an empty trailing table row behind after deletion https://bugs.webkit.org/show_bug.cgi?id=318002
Reviewed by Anne van Kesteren and Darin Adler.

DeleteSelectionCommand::removePreviouslySelectedEmptyTableRows() has a final
block whose job is to remove the *end* table row itself when the deleted
selection left it empty (and the caret is not landing inside it). The block was
guarded by `endTableRow != m_startTableRow`, but a refactoring typo initialized
the local `endTableRow` from `m_startTableRow` instead of `m_endTableRow`. That
made the guard always false, so the block became dead code and an empty trailing
row was never removed.

This regressed in 295798@main ("Reduce incorrect usage of protectedX() in dom
and editing"), which mechanically rewrote:
```
      auto endTableRow = protectedEndTableRow();   // returned m_endTableRow
```
into:
```
      RefPtr endTableRow = m_startTableRow;         // wrong member
```
silently substituting the start row for the end row.

Fix the local to be initialized from m_endTableRow, restoring the intended
behavior. Note this is only observable when the end row is already empty and
falls inside the selection (e.g. a trailing empty row), because table-spanning
deletes preserve the content of the boundary rows.

Test: editing/deleting/delete-empty-end-table-row.html

* LayoutTests/editing/deleting/delete-empty-end-table-row-expected.txt: Added.
* LayoutTests/editing/deleting/delete-empty-end-table-row.html: Added.
* Source/WebCore/editing/DeleteSelectionCommand.cpp:
(WebCore::DeleteSelectionCommand::removePreviouslySelectedEmptyTableRows):

Canonical link: https://commits.webkit.org/315990@main

Source/WebCore/editing/DeleteSelectionCommand.cpp
@@ -894,7 +894,7 @@ void DeleteSelectionCommand::removePreviouslySelectedEmptyTableRows()
         }
     }
 
-    RefPtr endTableRow = m_startTableRow;
+    RefPtr endTableRow = m_endTableRow;
     if (endTableRow && endTableRow->isConnected() && endTableRow != m_startTableRow) {
         if (isTableRowEmpty(*endTableRow)) {
             // Don't remove m_endTableRow if it's where we're putting the ending selection.

LayoutTests/editing/deleting/delete-empty-end-table-row-expected.txt
@@ -1,0 +1,5 @@
+Tests that deleting a selection running from inside a table row through a trailing empty row removes the now-fully-selected empty row. This is a regression test for a typo (introduced in 295798@main) that made DeleteSelectionCommand compare the end row against itself (using m_startTableRow in place of m_endTableRow), leaving the empty trailing row behind. The test passes if the table ends up with a single row containing "A".
+
+A
+PASS: empty trailing table row is removed (row count is 1, was 2)
+PASS: the surviving row keeps its remaining text "A"

LayoutTests/editing/deleting/delete-empty-end-table-row.html
@@ -1,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>Tests that deleting a selection running from inside a table row through a
+trailing empty row removes the now-fully-selected empty row. This is a
+regression test for a typo (introduced in 295798@main) that made
+DeleteSelectionCommand compare the end row against itself (using m_startTableRow
+in place of m_endTableRow), leaving the empty trailing row behind. The test
+passes if the table ends up with a single row containing "A".</p>
+<div id="edit" contenteditable="true">
+<table id="table" border="1"><tbody>
+<tr><td id="start">AAA</td></tr>
+<tr><td id="end"><br></td></tr>
+</tbody></table>
+</div>
+<pre id="result"></pre>
+<script>
+if (window.testRunner)
+    testRunner.dumpAsText();
+
+var table = document.getElementById("table");
+var start = document.getElementById("start").firstChild;
+var end = document.getElementById("end");
+
+// Select from the middle of the first row through the empty trailing row.
+window.getSelection().setBaseAndExtent(start, 1, end, 0);
+document.execCommand("Delete");
+
+var rowCount = table.rows.length;
+var firstCellText = table.rows.length ? table.rows[0].textContent : "";
+
+var lines = [];
+function check(description, condition) {
+    lines.push((condition ? "PASS" : "FAIL") + ": " + description);
+}
+
+check("empty trailing table row is removed (row count is 1, was 2)", rowCount === 1);
+check("the surviving row keeps its remaining text \"A\"", firstCellText === "A");
+
+document.getElementById("result").textContent = lines.join("\n");
+</script>
+</body>
+</html>