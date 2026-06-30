d8576e6cceeb0595321ad30fb85d0b861377ce9d
Charlie Wolfe <charliew@apple.com>
2026-06-30 03:39:33 -0700

    Reject requestStorageAccess() without gesture should not synthesize user activation https://bugs.webkit.org/show_bug.cgi?id=313478 rdar://174964081
    Reviewed by Matthew Finkel.
    
    The completion handler for requestStorageAccess() preserved the user gesture whenever the prompt was
    not shown, including on the no-gesture fast-reject path. This synthesized a UserGestureIndicator
    from nothing, allowing the .catch handler to call gesture-gated APIs from a cross-site iframe with
    zero user interaction.
    
    Gate gesture preservation on a real gesture having existed at call time.
    
    Test: http/tests/storageAccess/request-storage-access-rejected-without-gesture-should-not-activate.html
    
    * LayoutTests/http/tests/storageAccess/request-storage-access-rejected-without-gesture-should-not-activate-expected.txt: Added.
    * LayoutTests/http/tests/storageAccess/request-storage-access-rejected-without-gesture-should-not-activate.html: Added.
    * LayoutTests/http/tests/storageAccess/resources/request-storage-access-without-gesture-check-activation-iframe.html: Added.
    * Source/WebCore/dom/DocumentStorageAccess.cpp:
    (WebCore::DocumentStorageAccess::requestStorageAccess):
    (WebCore::DocumentStorageAccess::requestStorageAccessQuirk):
    * Source/WebCore/dom/DocumentStorageAccess.h:
    * Source/WebCore/page/ChromeClient.h:
    (WebCore::ChromeClient::requestStorageAccess):
    * Source/WebKit/NetworkProcess/Classifier/WebResourceLoadStatisticsStore.cpp:
    (WebKit::WebResourceLoadStatisticsStore::requestStorageAccess):
    * Source/WebKit/NetworkProcess/Classifier/WebResourceLoadStatisticsStore.h:
    * Source/WebKit/NetworkProcess/NetworkConnectionToWebProcess.cpp:
    (WebKit::NetworkConnectionToWebProcess::requestStorageAccess):
    * Source/WebKit/NetworkProcess/NetworkConnectionToWebProcess.h:
    * Source/WebKit/NetworkProcess/NetworkConnectionToWebProcess.messages.in:
    * Source/WebKit/Scripts/webkit/messages.py:
    (headers_for_type):
    * Source/WebKit/Shared/WebCoreArgumentCoders.serialization.in:
    * Source/WebKit/WebProcess/WebCoreSupport/WebChromeClient.cpp:
    (WebKit::WebChromeClient::requestStorageAccess):
    * Source/WebKit/WebProcess/WebCoreSupport/WebChromeClient.h:
    * Source/WebKit/WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::requestStorageAccess):
    * Source/WebKit/WebProcess/WebPage/WebPage.h:
    
    Originally-landed-as: 305413.854@safari-7624-branch (6fae18f756d2). rdar://180436613
    Canonical link: https://commits.webkit.org/316120@main

--- Source/WebKit/WebProcess/WebPage/WebPage.h
+++ Source/WebKit/WebProcess/WebPage/WebPage.h
@@ -254,7 +254,7 @@ enum class EventHandling : uint8_t;
 enum class EventMakesGamepadsVisible : bool;
 enum class ExceptionCode : uint8_t;
 enum class FinalizeRenderingUpdateFlags : uint8_t;
-enum class HasOrShouldIgnoreUserGesture : bool;
+enum class HasUserGestureOrNoUserGestureRequired : bool;
 enum class HighlightRequestOriginatedInApp : bool;
 enum class IFrameUnloadReason : bool;
 enum class ImageDecodingError : uint8_t;
@@ -1787,7 +1787,7 @@ public:
 #endif
 
     void hasStorageAccess(WebCore::RegistrableDomain&& subFrameDomain, WebCore::RegistrableDomain&& topFrameDomain, WebFrame&, CompletionHandler<void(bool)>&&);
-    void requestStorageAccess(WebCore::RegistrableDomain&& subFrameDomain, WebCore::RegistrableDomain&& topFrameDomain, WebFrame&, WebCore::StorageAccessScope, WebCore::HasOrShouldIgnoreUserGesture, CompletionHandler<void(WebCore::RequestStorageAccessResult)>&&);
+    void requestStorageAccess(WebCore::RegistrableDomain&& subFrameDomain, WebCore::RegistrableDomain&& topFrameDomain, WebFrame&, WebCore::StorageAccessScope, WebCore::HasUserGestureOrNoUserGestureRequired, CompletionHandler<void(WebCore::RequestStorageAccessResult)>&&);
     void setLoginStatus(WebCore::RegistrableDomain&&, WebCore::IsLoggedIn, CompletionHandler<void()>&&);
     void isLoggedIn(WebCore::RegistrableDomain&&, CompletionHandler<void(bool)>&&);
     bool hasPageLevelStorageAccess(const WebCore::RegistrableDomain& topLevelDomain, const WebCore::RegistrableDomain& resourceDomain) const;
